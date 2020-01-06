18c3f9b943ab622b7081a997b03dcb384db9bac8
Doug Moore <dougm@rice.edu>
2020-01-06 16:37:13 -0600

    Split the DoFFT class into three classes, an FFTBase class for data and     initialization, a DoFFT class that just offers the FFT method, and a     DoIFFT class that just offers the IFFT method.  Why?  Because I seek     one work task function per Doer class.  Soon they'll all have the     same name and be derived from a Doer base class.
    Former-commit-id: 02c60c5f59af81d49e5f6f84f8d709f4686ee7fb

--- src/millipede/dofft.hpp
+++ src/millipede/dofft.hpp
@@ -21,76 +21,16 @@
 #include "stats.hpp"
 class Consumer;
 
-class DoFFT
-{
+class FFTBase{
 public:
-    DoFFT(Config *cfg, int in_tid, Consumer &in_consumer,
-	Table<char> &in_socket_buffer, Table<int> &in_socket_buffer_status, Table<complex_float> &in_data_buffer, Table<complex_float> &in_csi_buffer,
+    FFTBase(Config *cfg, int in_tid, Consumer &in_consumer,
+	Table<char> &in_socket_buffer, Table<int> &in_socket_buffer_status,
+	Table<complex_float> &in_data_buffer, Table<complex_float> &in_csi_buffer,
         Table<complex_float> &in_dl_ifft_buffer, char *in_dl_socket_buffer, 
         Stats *in_stats_manager);
-    ~DoFFT();
+    ~FFTBase();
 
-    /**
-     * Do FFT task for one OFDM symbol 
-     * @param tid: task thread index, used for selecting muplans and task ptok
-     * @param offset: offset of the OFDM symbol in socket_buffer_
-     * Buffers: socket_buffer_, fft_buffer_, csi_buffer_, data_buffer_ 
-     *     Input buffer: socket_buffer_
-     *     Output buffer: csi_buffer_ if subframe is pilot
-     *                    data_buffer_ if subframe is data
-     *     Intermediate buffer: fft_buffer_ (FFT_inputs, FFT_outputs)
-     * Offsets: 
-     *     socket_buffer_: 
-     *         dim1: socket thread index: (offset / # of OFDM symbols per thread)
-     *         dim2: OFDM symbol index in this socket thread (offset - # of subframes in previous threads)
-     *     FFT_inputs, FFT_outputs: 
-     *         dim1: frame index * # of OFDM symbols per frame + subframe index * # of atennas + antenna index
-     *         dim2: subcarrier index
-     *     csi_buffer_: 
-     *         dim1: frame index * FFT size + subcarrier index in the current frame
-     *         dim2: user index * # of antennas + antenna index
-     *     data_buffer_: 
-     *         dim1: frame index * # of data subframes per frame + data subframe index
-     *         dim2: transpose block index * block size * # of antennas + antenna index * block size
-     * Event offset: frame index * # of subframe per frame + subframe index
-     * Description: 
-     *     1. copy received data (one OFDM symbol) from socket_buffer to fft_buffer_.FFT_inputs (remove CP)
-     *     2. perform FFT on fft_buffer_.FFT_inputs and store results in fft_buffer_.FFT_outputs
-     *     3. if subframe is pilot, do channel estimation from fft_buffer_.FFT_outputs to csi_buffer_
-     *        if subframe is data, copy data from fft_buffer_.FFT_outputs to data_buffer_ and do block transpose     
-     *     4. add an event to the message queue to infrom main thread the completion of this task
-     */
-    void FFT(int offset);
-
-
-    /**
-     * Do modulation and ifft tasks for one OFDM symbol
-     * @param tid: task thread index, used for selecting task ptok
-     * @param offset: offset of the OFDM symbol in dl_modulated_buffer_
-     * Buffers: dl_IQ_data_long, dl_modulated_buffer_
-     *     Input buffer: dl_IQ_data_long
-     *     Output buffer: dl_iffted_data_buffer_
-     *     Intermediate buffer: dl_ifft_buffer_
-     * Offsets: 
-     *     dl_IQ_data_long_: 
-     *         dim1: data subframe index in the current frame * # of users + user index
-     *         dim2: subcarrier index
-     *     dl_ifft_buffer_: 
-     *         dim1: frame index * # of data subframes per frame * # of users + data subframe index * # of users + user index
-     *         dim2: subcarrier index 
-     *     dl_iffted_data_buffer_: 
-     *         dim1: frame index * # of data subframes per frame + data subframe index
-     *         dim2: transpose block index * block size * # of UEs + user index * block size
-     * Event offset: offset
-     * Description: 
-     *     1. for each OFDM symbol, perform modulation and then ifft
-     *     2. perform block-wise transpose on IFFT outputs and store results in dl_iffted_data_buffer_
-     *     2. add an event to the message queue to infrom main thread the completion of this task
-     */
-    void IFFT(int offset);
-
-    
-private:
+protected:
     Config *config_;
     int BS_ANT_NUM, PILOT_NUM;
     int OFDM_CA_NUM;
@@ -129,12 +69,95 @@ private:
 
     DFTI_DESCRIPTOR_HANDLE mkl_handle_dl;
     MKL_LONG mkl_status_dl;
-
     
-
     // mufft_plan_1d *muplans_;
+};
 
+class DoFFT: public FFTBase
+{
+public:
+    DoFFT(Config *cfg, int in_tid, Consumer &in_consumer,
+	Table<char> &in_socket_buffer, Table<int> &in_socket_buffer_status,
+	Table<complex_float> &in_data_buffer, Table<complex_float> &in_csi_buffer,
+        Table<complex_float> &in_dl_ifft_buffer, char *in_dl_socket_buffer, 
+        Stats *in_stats_manager)
+      :FFTBase(cfg, in_tid, in_consumer, in_socket_buffer, in_socket_buffer_status,
+	       in_data_buffer, in_csi_buffer, in_dl_ifft_buffer, in_dl_socket_buffer,
+	       in_stats_manager)
+    {}
+    ~DoFFT() {}
 
+    /**
+     * Do FFT task for one OFDM symbol 
+     * @param tid: task thread index, used for selecting muplans and task ptok
+     * @param offset: offset of the OFDM symbol in socket_buffer_
+     * Buffers: socket_buffer_, fft_buffer_, csi_buffer_, data_buffer_ 
+     *     Input buffer: socket_buffer_
+     *     Output buffer: csi_buffer_ if subframe is pilot
+     *                    data_buffer_ if subframe is data
+     *     Intermediate buffer: fft_buffer_ (FFT_inputs, FFT_outputs)
+     * Offsets: 
+     *     socket_buffer_: 
+     *         dim1: socket thread index: (offset / # of OFDM symbols per thread)
+     *         dim2: OFDM symbol index in this socket thread (offset - # of subframes in previous threads)
+     *     FFT_inputs, FFT_outputs: 
+     *         dim1: frame index * # of OFDM symbols per frame + subframe index * # of atennas + antenna index
+     *         dim2: subcarrier index
+     *     csi_buffer_: 
+     *         dim1: frame index * FFT size + subcarrier index in the current frame
+     *         dim2: user index * # of antennas + antenna index
+     *     data_buffer_: 
+     *         dim1: frame index * # of data subframes per frame + data subframe index
+     *         dim2: transpose block index * block size * # of antennas + antenna index * block size
+     * Event offset: frame index * # of subframe per frame + subframe index
+     * Description: 
+     *     1. copy received data (one OFDM symbol) from socket_buffer to fft_buffer_.FFT_inputs (remove CP)
+     *     2. perform FFT on fft_buffer_.FFT_inputs and store results in fft_buffer_.FFT_outputs
+     *     3. if subframe is pilot, do channel estimation from fft_buffer_.FFT_outputs to csi_buffer_
+     *        if subframe is data, copy data from fft_buffer_.FFT_outputs to data_buffer_ and do block transpose     
+     *     4. add an event to the message queue to infrom main thread the completion of this task
+     */
+    void FFT(int offset);
+};
+
+class DoIFFT: public FFTBase {
+public:
+    DoIFFT(Config *cfg, int in_tid, Consumer &in_consumer,
+	Table<char> &in_socket_buffer, Table<int> &in_socket_buffer_status,
+	Table<complex_float> &in_data_buffer, Table<complex_float> &in_csi_buffer,
+        Table<complex_float> &in_dl_ifft_buffer, char *in_dl_socket_buffer, 
+        Stats *in_stats_manager)
+      :FFTBase(cfg, in_tid, in_consumer, in_socket_buffer, in_socket_buffer_status,
+	       in_data_buffer, in_csi_buffer, in_dl_ifft_buffer, in_dl_socket_buffer,
+	       in_stats_manager)
+    {}
+    ~DoIFFT() {}
+
+    /**
+     * Do modulation and ifft tasks for one OFDM symbol
+     * @param tid: task thread index, used for selecting task ptok
+     * @param offset: offset of the OFDM symbol in dl_modulated_buffer_
+     * Buffers: dl_IQ_data_long, dl_modulated_buffer_
+     *     Input buffer: dl_IQ_data_long
+     *     Output buffer: dl_iffted_data_buffer_
+     *     Intermediate buffer: dl_ifft_buffer_
+     * Offsets: 
+     *     dl_IQ_data_long_: 
+     *         dim1: data subframe index in the current frame * # of users + user index
+     *         dim2: subcarrier index
+     *     dl_ifft_buffer_: 
+     *         dim1: frame index * # of data subframes per frame * # of users + data subframe index * # of users + user index
+     *         dim2: subcarrier index 
+     *     dl_iffted_data_buffer_: 
+     *         dim1: frame index * # of data subframes per frame + data subframe index
+     *         dim2: transpose block index * block size * # of UEs + user index * block size
+     * Event offset: offset
+     * Description: 
+     *     1. for each OFDM symbol, perform modulation and then ifft
+     *     2. perform block-wise transpose on IFFT outputs and store results in dl_iffted_data_buffer_
+     *     2. add an event to the message queue to infrom main thread the completion of this task
+     */
+    void IFFT(int offset);
 };
 
 
