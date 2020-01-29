8c3d811e511c24f4d59e1a7ef892a4a4b706eba3
rdoost <doost@rice.edu>
2020-01-29 13:39:07 -0600

    fixing a new compile error in code enabled by DEBUG_RECV macro
    Former-commit-id: 77a80dfbcf43906ac789274677a0aa46e1efcfbc

--- src/millipede/txrx/txrx_argos.cpp
+++ src/millipede/txrx/txrx_argos.cpp
@@ -215,7 +215,7 @@ void* PacketTXRX::loopRecv_Argos(int tid)
                 cursor %= buffer_frame_num_;
             }
 #if DEBUG_RECV
-            printf("PacketTXRX %d: receive frame_id %d, symbol_id %d, ant_id %d, offset %d\n", tid, frame_id, symbol_id, ant_id, offset + tid * buffer_frame_num);
+            printf("PacketTXRX %d: receive frame_id %d, symbol_id %d, ant_id %d, offset %d\n", tid, frame_id, symbol_id, ant_id, cursor);
 #endif
 #if DEBUG_DOWNLINK && !SEPARATE_TX_RX
             if (rx_symbol_id > 0)
