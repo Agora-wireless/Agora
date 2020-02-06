2e072556b39a723cc3a6e2ef8611bb9b346ce43e
Doug Moore <dougm@rice.edu>
2020-02-06 09:53:32 -0600

    Update prev_frame_counter after schedule_fft_task, not in it.
    Former-commit-id: 11816f516b9cdb6e4b6b0cfad235b9602d9713fd

--- src/millipede/millipede.cpp
+++ src/millipede/millipede.cpp
@@ -215,6 +215,7 @@ void Millipede::start()
                 /* if this is the first frame or the previous frame is all processed, schedule FFT for this packet */
                 if ((frame_count == 0 && fft_stats_.frame_count < 100) || (fft_stats_.frame_count > 0 && previous_frame_done)) {
                     schedule_fft_task(offset, frame_count, frame_id, subframe_id, ant_id, consumer_fft);
+                    prev_frame_counter[(frame_id + TASK_BUFFER_FRAME_NUM - 1) % TASK_BUFFER_FRAME_NUM] = 0;
                 } else {
                     /* if the previous frame is not finished, store offset in queue */
                     delay_fft_queue[frame_id][delay_fft_queue_cnt[frame_id]] = offset;
@@ -706,9 +707,6 @@ void Millipede::schedule_fft_task(int offset, int frame_count,
         printf("Main thread: created FFT tasks for all packets in frame: %d, frame buffer: %d in %.5f us\n",
             frame_count, frame_id, get_time() - stats_manager_->get_pilot_received(frame_count));
 #endif
-#if !BIGSTATION
-        prev_frame_counter[(frame_id + TASK_BUFFER_FRAME_NUM - 1) % TASK_BUFFER_FRAME_NUM] = 0;
-#endif
     }
 }
 
@@ -721,6 +719,7 @@ void Millipede::schedule_delayed_fft_tasks(int frame_count, int frame_id, int da
         for (int i = 0; i < delay_fft_queue_cnt[frame_id]; i++) {
             int offset_rx = delay_fft_queue[frame_id][i];
             schedule_fft_task(offset_rx, frame_count + 1, frame_id, data_subframe_id, 0, consumer);
+            prev_frame_counter[(frame_id + TASK_BUFFER_FRAME_NUM - 1) % TASK_BUFFER_FRAME_NUM] = 0;
         }
         delay_fft_queue_cnt[frame_id] = 0;
 #if DEBUG_PRINT_PER_FRAME_ENTER_QUEUE
