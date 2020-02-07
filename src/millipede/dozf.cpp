e7f5f10cc194e2d056f1d56e7d75ce33c720d318
Doug Moore <dougm@rice.edu>
2020-02-07 16:22:55 -0600

    Isolate the part of ZF that differs between downlink and uplink mode in a 'precoder' function.

--- src/millipede/dozf.cpp
+++ src/millipede/dozf.cpp
@@ -43,14 +43,37 @@ void DoZF::launch(int offset)
         ZF_freq_orthogonal(offset);
     else
         ZF_time_orthogonal(offset);
+
+    // inform main thread
+    Event_data ZF_finish_event;
+    ZF_finish_event.event_type = EVENT_ZF;
+    ZF_finish_event.data = offset;
+    consumer_.handle(ZF_finish_event);
+}
+
+void* DoZF::precoder(void* mat_input_ptr, int frame_id, int sc_id, int offset)
+{
+    cx_fmat& mat_input = *(cx_fmat*)mat_input_ptr;
+    cx_float* ptr_out;
+    int BS_ANT_NUM = config_->BS_ANT_NUM;
+    if (config_->downlink_mode) {
+        if (config_->recipCalEn) {
+            cx_float* calib = (cx_float*)(&recip_buffer_[frame_id][sc_id * BS_ANT_NUM]);
+            cx_fvec vec_calib(calib, BS_ANT_NUM, false);
+            cx_fmat mat_calib(BS_ANT_NUM, BS_ANT_NUM);
+            mat_calib = diagmat(vec_calib);
+            mat_input = mat_calib * mat_input;
+        }
+        ptr_out = (cx_float*)dl_precoder_buffer_[offset];
+    } else
+        ptr_out = (cx_float*)precoder_buffer_[offset];
+    return (ptr_out);
 }
 
 void DoZF::ZF_time_orthogonal(int offset)
 {
     int OFDM_DATA_NUM = config_->OFDM_DATA_NUM;
     int zf_block_size = config_->zf_block_size;
-    bool downlink_mode = config_->downlink_mode;
-    bool recipCalEn = config_->recipCalEn;
 
     int frame_id = offset / config_->zf_block_num;
     int sc_id = offset % config_->zf_block_num * zf_block_size;
@@ -144,19 +167,7 @@ void DoZF::ZF_time_orthogonal(int offset)
         cx_fmat mat_input(ptr_in, BS_ANT_NUM, UE_NUM, false);
         // cout<<"CSI matrix"<<endl;
         // cout<<mat_input.st()<<endl;
-        cx_float* ptr_out;
-        if (downlink_mode) {
-            cx_fmat mat_calib(BS_ANT_NUM, BS_ANT_NUM);
-            if (recipCalEn) {
-                cx_float* calib = (cx_float*)(&recip_buffer_[frame_id][cur_sc_id * BS_ANT_NUM]);
-                cx_fvec vec_calib(calib, BS_ANT_NUM, false);
-                mat_calib = diagmat(vec_calib);
-            } else
-                mat_calib.eye();
-            mat_input = mat_calib * mat_input;
-            ptr_out = (cx_float*)dl_precoder_buffer_[cur_offset];
-        } else
-            ptr_out = (cx_float*)precoder_buffer_[cur_offset];
+        cx_float* ptr_out = (cx_float*)precoder(&mat_input, frame_id, cur_sc_id, cur_offset);
         cx_fmat mat_output(ptr_out, UE_NUM, BS_ANT_NUM, false);
 
 #if DEBUG_UPDATE_STATS_DETAILED
@@ -198,12 +209,6 @@ void DoZF::ZF_time_orthogonal(int offset)
         }
 #endif
     }
-
-    // inform main thread
-    Event_data ZF_finish_event;
-    ZF_finish_event.event_type = EVENT_ZF;
-    ZF_finish_event.data = offset;
-    consumer_.handle(ZF_finish_event);
 }
 
 void DoZF::ZF_freq_orthogonal(int offset)
@@ -280,11 +285,6 @@ void DoZF::ZF_freq_orthogonal(int offset)
         printf("Thread %d ZF takes %.2f\n", tid, duration);
     }
 #endif
-    // inform main thread
-    Event_data ZF_finish_event;
-    ZF_finish_event.event_type = EVENT_ZF;
-    ZF_finish_event.data = offset;
-    consumer_.handle(ZF_finish_event);
 }
 
 void DoZF::Predict(int offset)
