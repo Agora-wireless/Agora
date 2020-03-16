c7f27d57c6f41988eef44dde442629be081d9942
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-03-16 12:12:28 -0700

    Allow running for multiple iterations in test_millipede.sh
    Former-commit-id: 443c393934256e7f706fa7e8180d2a1c3c781d24

--- test/test_millipede/test_millipede.sh
+++ test/test_millipede/test_millipede.sh
@@ -1,17 +1,28 @@
-for i in `seq 1 100`; do
-  echo "generate data for correctness test......"
+#!/bin/bash
+num_iters=1
+
+# Check if the user supplied a number-of-iterations argument
+if [ "$#" -eq 2 ]; then
+  num_iters=$1
+fi
+
+echo "Running tests for $num_iters iterations"
+
+for i in `seq 1 $num_iters`; do
+  echo "Generating data for correctness test"
   ./data_generator data/tddconfig-correctness-test-ul.json
   echo -e "-------------------------------------------------------\n\n\n"
   echo "==================================="
-  echo "run uplink correctness test......"
+  echo "Running uplink correctness test......"
   echo -e "===================================\n"
   ./millipede data/tddconfig-correctness-test-ul.json &
   ./sender 4 10 5000 data/tddconfig-correctness-test-ul.json
 
   echo -e "-------------------------------------------------------\n\n\n"
   echo "==================================="
-  echo "run downlink correctness test......"
+  echo "Running downlink correctness test......"
   echo -e "===================================\n"
   ./millipede data/tddconfig-correctness-test-dl.json &
   ./sender 4 10 5000 data/tddconfig-correctness-test-dl.json
+
 done
