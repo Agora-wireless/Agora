59eb0f692146178b96a57027df7072a62bb0d5c8
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-04-30 17:29:27 -0500

    Bug fix in computing total_data_symbol_idx in dodemul.cpp

--- src/millipede/dodemul.cpp
+++ src/millipede/dodemul.cpp
@@ -45,15 +45,16 @@ DoDemul::~DoDemul()
 Event_data DoDemul::launch(size_t tag)
 {
     size_t frame_id = gen_tag_t(tag).frame_id;
-    size_t total_data_symbol_idx = (frame_id * cfg->ul_data_symbol_num_perframe)
-        + gen_tag_t(tag).symbol_id;
+    size_t symbol_id = gen_tag_t(tag).symbol_id;
+    size_t total_data_symbol_idx
+        = cfg->get_total_data_symbol_idx_ul(frame_id, symbol_id);
     size_t base_sc_id = gen_tag_t(tag).sc_id;
 
     size_t start_tsc = worker_rdtsc();
 
     if (kDebugPrintInTask) {
         printf("In doDemul tid %d: frame: %zu, symbol: %zu, subcarrier: %zu \n",
-            tid, frame_id, gen_tag_t(tag).symbol_id, base_sc_id);
+            tid, frame_id, symbol_id, base_sc_id);
     }
 
     int transpose_block_size = cfg->transpose_block_size;
