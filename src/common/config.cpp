a67cc425e580bc5562dcb8db95cd313e27b35bd1
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-05-03 19:29:23 -0500

    Rename SFIndex functions-> symbol_idx

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -462,25 +462,25 @@ int Config::getDownlinkPilotId(size_t frame_id, size_t symbol_id)
     return -1;
 }
 
-int Config::getDlSFIndex(size_t frame_id, size_t symbol_id)
+size_t Config::get_dl_symbol_idx(size_t frame_id, size_t symbol_id) const
 {
-    std::vector<size_t>::iterator it;
     size_t fid = frame_id % frames.size();
-    it = find(DLSymbols[fid].begin(), DLSymbols[fid].end(), symbol_id);
+    const auto it
+        = find(DLSymbols[fid].begin(), DLSymbols[fid].end(), symbol_id);
     if (it != DLSymbols[fid].end())
         return it - DLSymbols[fid].begin();
     else
         return -1;
 }
 
-int Config::getPilotSFIndex(size_t frame_id, size_t symbol_id)
+size_t Config::get_pilot_symbol_idx(size_t frame_id, size_t symbol_id) const
 {
-    std::vector<size_t>::iterator it;
     size_t fid = frame_id % frames.size();
-    it = find(pilotSymbols[fid].begin(), pilotSymbols[fid].end(), symbol_id);
+    const auto it
+        = find(pilotSymbols[fid].begin(), pilotSymbols[fid].end(), symbol_id);
     if (it != pilotSymbols[fid].end()) {
 #ifdef DEBUG3
-        printf("getPilotSFIndex(%zu, %zu) = %zu\n", frame_id, symbol_id,
+        printf("get_pilot_symbol_idx(%zu, %zu) = %zu\n", frame_id, symbol_id,
             it - pilotSymbols[fid].begin());
 #endif
         return it - pilotSymbols[fid].begin();
@@ -488,14 +488,14 @@ int Config::getPilotSFIndex(size_t frame_id, size_t symbol_id)
         return -1;
 }
 
-int Config::getUlSFIndex(size_t frame_id, size_t symbol_id)
+size_t Config::get_ul_symbol_idx(size_t frame_id, size_t symbol_id) const
 {
-    std::vector<size_t>::iterator it;
     size_t fid = frame_id % frames.size();
-    it = find(ULSymbols[fid].begin(), ULSymbols[fid].end(), symbol_id);
+    const auto it
+        = find(ULSymbols[fid].begin(), ULSymbols[fid].end(), symbol_id);
     if (it != ULSymbols[fid].end()) {
 #ifdef DEBUG3
-        printf("getUlSFIndexId(%zu, %zu) = %zu\n", frame_id, symbol_id,
+        printf("get_ul_symbol_idx(%zu, %zu) = %zu\n", frame_id, symbol_id,
             it - ULSymbols[fid].begin());
 #endif
         return it - ULSymbols[fid].begin();
