de20a9c264185a5efb268cd292a1a2387d8de63b
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-05-05 09:50:20 -0700

    Working zf performance test

--- src/common/utils.h
+++ src/common/utils.h
@@ -28,6 +28,7 @@
 #include <iostream>
 #include <mutex>
 #include <pthread.h>
+#include <random>
 #include <sstream>
 #include <stdlib.h>
 #include <string>
@@ -119,5 +120,38 @@ static inline void rt_assert(bool condition, std::string throw_str, char* s)
         throw std::runtime_error(throw_str + std::string(s));
     }
 }
+
+class SlowRand {
+    std::random_device rand_dev; // Non-pseudorandom seed for twister
+    std::mt19937_64 mt;
+    std::uniform_int_distribution<uint64_t> dist;
+
+public:
+    SlowRand()
+        : mt(rand_dev())
+        , dist(0, UINT64_MAX)
+    {
+    }
+
+    inline uint64_t next_u64() { return dist(mt); }
+};
+
+class FastRand {
+public:
+    uint64_t seed;
+
+    /// Create a FastRand using a seed from SlowRand
+    FastRand()
+    {
+        SlowRand slow_rand;
+        seed = slow_rand.next_u64();
+    }
+
+    inline uint32_t next_u32()
+    {
+        seed = seed * 1103515245 + 12345;
+        return static_cast<uint32_t>(seed >> 32);
+    }
+};
 
 #endif
