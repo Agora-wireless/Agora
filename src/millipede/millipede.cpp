f217268001275e6a7dfcb31449f286f54f2d9fce
rdoost <doost@rice.edu>
2020-05-06 16:14:25 -0500

    faster version of pilot tracking

--- src/millipede/millipede.cpp
+++ src/millipede/millipede.cpp
@@ -1011,8 +1011,8 @@ void Millipede::initialize_uplink_buffers()
 
     equal_buffer_.malloc(
         task_buffer_symbol_num_ul, cfg->OFDM_DATA_NUM * cfg->UE_NUM, 64);
-    ue_spec_pilot_buffer_.calloc(TASK_BUFFER_FRAME_NUM * cfg->UL_PILOT_SYMS,
-        cfg->OFDM_DATA_NUM * cfg->UE_NUM, 64);
+    ue_spec_pilot_buffer_.calloc(
+        TASK_BUFFER_FRAME_NUM, cfg->UL_PILOT_SYMS * cfg->UE_NUM, 64);
     demod_hard_buffer_.malloc(
         task_buffer_symbol_num_ul, cfg->OFDM_DATA_NUM * cfg->UE_NUM, 64);
     size_t mod_type = config_->mod_type;
