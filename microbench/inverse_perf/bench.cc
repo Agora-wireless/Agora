b7a2764712bf31b35183d5752d58d5c5b9d0f9c8
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-05-18 22:33:31 +0000

    Streamline inverse perf benchmark

--- microbench/inverse_perf/bench.cc
+++ microbench/inverse_perf/bench.cc
@@ -15,13 +15,17 @@ DEFINE_uint64(n_rows, 64, "Number of matrix rows");
 DEFINE_uint64(n_cols, 32, "Number of matrix columns");
 DEFINE_uint64(check_condition, 0, "Check the square matrix's condition number");
 
-enum class PinvMode { kFormula, kSVD };
+enum class PinvMode {
+  kFormula,
+  kSVD
+};
 
 arma::cx_float test_arma(const arma::cx_float* _in_mat_arr, PinvMode mode) {
   const size_t tot_size = FLAGS_n_rows * FLAGS_n_cols;
   auto* in_mat_arr = new arma::cx_float[tot_size];
   auto* out_mat_arr = new arma::cx_float[tot_size];
-  for (size_t i = 0; i < tot_size; i++) in_mat_arr[i] = _in_mat_arr[i];
+  for (size_t i = 0; i < tot_size; i++)
+    in_mat_arr[i] = _in_mat_arr[i];
 
   arma::cx_fmat input(in_mat_arr, FLAGS_n_rows, FLAGS_n_cols, false);
   arma::cx_fmat output(out_mat_arr, FLAGS_n_cols, FLAGS_n_rows, false);
@@ -30,27 +34,27 @@ arma::cx_float test_arma(const arma::cx_float* _in_mat_arr, PinvMode mode) {
   arma::cx_float ret(0.0, 0.0);
   for (size_t iter = 0; iter < FLAGS_n_iters; iter++) {
     const bool take_measurement = (iter >= FLAGS_n_iters * warmup_fraction);
-    if (take_measurement) timer.start();
+    if (take_measurement)
+      timer.start();
 
     if (mode == PinvMode::kFormula) {
       arma::cx_fmat A = input.t() * input;
-      if (FLAGS_check_condition == 1) ret += rcond(A);
+      if (FLAGS_check_condition == 1)
+        ret += rcond(A);
       output = A.i() * input.t();
     } else {
       output = pinv(input);
     }
 
-    if (take_measurement) timer.stop();
+    if (take_measurement)
+      timer.stop();
 
     ret += arma::accu(output);
     in_mat_arr[0] = out_mat_arr[0];
   }
 
-  printf(
-      "Armadillo: Average time for %s-based pseudo-inverse of "
-      "%zux%zu matrix = {avg %.3f ms, stddev %.3f ms}\n",
-      mode == PinvMode::kFormula ? "formula" : "SVD", FLAGS_n_rows,
-      FLAGS_n_cols, timer.avg_msec(), timer.stddev_msec());
+  printf("%s: %.3f ms\n", mode == PinvMode::kFormula ? "Formula" : "SVD",
+         timer.avg_msec());
   return ret;
 }
 
@@ -59,38 +63,40 @@ std::complex<float> test_eigen(const std::complex<float>* _in_mat_arr,
   const size_t tot_size = FLAGS_n_rows * FLAGS_n_cols;
   auto* in_mat_arr = new std::complex<float>[tot_size];
   auto* out_mat_arr = new std::complex<float>[tot_size];
-  for (size_t i = 0; i < tot_size; i++) in_mat_arr[i] = _in_mat_arr[i];
+  for (size_t i = 0; i < tot_size; i++)
+    in_mat_arr[i] = _in_mat_arr[i];
 
   Eigen::Map<Eigen::Matrix<std::complex<float>, Eigen::Dynamic, Eigen::Dynamic,
-                           Eigen::ColMajor>>
-      input(in_mat_arr, FLAGS_n_rows, FLAGS_n_cols);
+                           Eigen::ColMajor>> input(in_mat_arr, FLAGS_n_rows,
+                                                   FLAGS_n_cols);
 
   Eigen::Map<Eigen::Matrix<std::complex<float>, Eigen::Dynamic, Eigen::Dynamic,
-                           Eigen::ColMajor>>
-      output(out_mat_arr, FLAGS_n_cols, FLAGS_n_rows);
+                           Eigen::ColMajor>> output(out_mat_arr, FLAGS_n_cols,
+                                                    FLAGS_n_rows);
 
   std::complex<float> ret(0.0, 0.0);
   TscTimer timer(FLAGS_n_iters, freq_ghz);
   for (size_t iter = 0; iter < FLAGS_n_iters; iter++) {
     const bool take_measurement = (iter >= FLAGS_n_iters * warmup_fraction);
-    if (take_measurement) timer.start();
+    if (take_measurement)
+      timer.start();
 
     if (mode == PinvMode::kFormula) {
       output = (input.adjoint() * input).inverse() * input.adjoint();
     } else {
       output = input.completeOrthogonalDecomposition().pseudoInverse();
     }
-    if (take_measurement) timer.stop();
+    if (take_measurement)
+      timer.stop();
 
     ret += output.sum();
     in_mat_arr[0] = out_mat_arr[0];
   }
 
-  printf(
-      "Eigen: Average time for %s-based pseudo-inverse of "
-      "%zux%zu matrix = {avg %.3f ms, stddev %.3f ms}\n",
-      mode == PinvMode::kFormula ? "formula" : "SVD", FLAGS_n_rows,
-      FLAGS_n_cols, timer.avg_msec(), timer.stddev_msec());
+  printf("Eigen: Average time for %s-based pseudo-inverse of "
+         "%zux%zu matrix = {avg %.3f ms, stddev %.3f ms}\n",
+         mode == PinvMode::kFormula ? "formula" : "SVD", FLAGS_n_rows,
+         FLAGS_n_cols, timer.avg_msec(), timer.stddev_msec());
   return ret;
 }
 
@@ -107,17 +113,8 @@ int main(int argc, char** argv) {
     in_base_mat_arr[i] = arma::cx_float(re, im);
   }
 
-  {
-    arma::cx_float ret_1 = test_arma(in_base_mat_arr, PinvMode::kFormula);
-    arma::cx_float ret_2 = test_arma(in_base_mat_arr, PinvMode::kSVD);
-    printf("Arma: Results = {%.5f, %.5f}, {%.5f, %.5f}\n", ret_1.real(),
-           ret_1.imag(), ret_2.real(), ret_2.imag());
-  }
-
-  {
-    std::complex<float> ret_1 = test_eigen(in_base_mat_arr, PinvMode::kFormula);
-    std::complex<float> ret_2 = test_eigen(in_base_mat_arr, PinvMode::kSVD);
-    printf("Eigen: Results = {%.5f, %.5f}, {%.5f, %.5f}\n", ret_1.real(),
-           ret_1.imag(), ret_2.real(), ret_2.imag());
-  }
+  arma::cx_float ret_1 = test_arma(in_base_mat_arr, PinvMode::kFormula);
+  arma::cx_float ret_2 = test_arma(in_base_mat_arr, PinvMode::kSVD);
+  printf("Arma: Results = {%.5f, %.5f}, {%.5f, %.5f}\n", ret_1.real(),
+         ret_1.imag(), ret_2.real(), ret_2.imag());
 }
