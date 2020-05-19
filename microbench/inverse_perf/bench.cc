6c24543253f6b06b418af5638107775801d36b29
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-05-19 01:22:33 -0500

    Save work on inverse microbenchmarks

--- microbench/inverse_perf/bench.cc
+++ microbench/inverse_perf/bench.cc
@@ -1,9 +1,9 @@
 #include <gflags/gflags.h>
 #include <mkl.h>
 #include <armadillo>
-#include <eigen3/Eigen/Dense>
 #include <iostream>
 #include "timer.h"
+//#include <eigen3/Eigen/Dense>
 
 double freq_ghz = -1.0;
 
@@ -17,38 +17,55 @@ DEFINE_uint64(check_condition, 0, "Check the square matrix's condition number");
 
 enum class PinvMode { kFormula, kSVD };
 
-std::pair<arma::cx_float, double> test_arma(const arma::cx_float* _in_mat_arr,
-                                            PinvMode mode) {
-  const size_t tot_size = FLAGS_n_rows * FLAGS_n_cols;
-  auto* in_mat_arr = new arma::cx_float[tot_size];
-  auto* out_mat_arr = new arma::cx_float[tot_size];
-  for (size_t i = 0; i < tot_size; i++) in_mat_arr[i] = _in_mat_arr[i];
-
-  arma::cx_fmat input(in_mat_arr, FLAGS_n_rows, FLAGS_n_cols, false);
-  arma::cx_fmat output(out_mat_arr, FLAGS_n_cols, FLAGS_n_rows, false);
-
+std::pair<std::vector<arma::cx_fmat>, double> arma_inverses(
+    const std::vector<arma::cx_fmat>& test_matrices, PinvMode mode) {
   TscTimer timer(FLAGS_n_iters, freq_ghz);
-  arma::cx_float ret(0.0, 0.0);
+  std::vector<arma::cx_fmat> ret;
+
   for (size_t iter = 0; iter < FLAGS_n_iters; iter++) {
+    const arma::cx_fmat& input = test_matrices[iter];
+    arma::cx_fmat output;
+
     const bool take_measurement = (iter >= FLAGS_n_iters * warmup_fraction);
     if (take_measurement) timer.start();
 
     if (mode == PinvMode::kFormula) {
       arma::cx_fmat A = input.t() * input;
-      if (FLAGS_check_condition == 1) ret += rcond(A);
       output = A.i() * input.t();
     } else {
       output = pinv(input);
     }
 
-    if (take_measurement) timer.stop();
+    /*
+    if (FLAGS_check_condition == 1) {
+      // Condition number(X) = norm(X) * norm(X's inverse)
+      ret += arma::norm(input) * arma::norm(output);
+    }
+    */
 
-    ret += arma::accu(output);
-    in_mat_arr[0] = out_mat_arr[0];
+    if (take_measurement) timer.stop();
+    ret.push_back(output);
+    printf("Iter %zu\n", iter);
   }
-  return std::pair<arma::cx_float, double>(ret, timer.avg_msec());
+  return std::pair<std::vector<arma::cx_fmat>, double>(ret, timer.avg_msec());
 }
 
+std::pair<double, double> arma_norm(
+    const std::vector<arma::cx_fmat>& test_matrices) {
+  TscTimer timer(FLAGS_n_iters, freq_ghz);
+  double norm_sum = 0.0;
+
+  for (size_t iter = 0; iter < FLAGS_n_iters; iter++) {
+    const bool take_measurement = (iter >= FLAGS_n_iters * warmup_fraction);
+    if (take_measurement) timer.start();
+    norm_sum += arma::norm(test_matrices[iter], 1);
+    if (take_measurement) timer.stop();
+  }
+
+  return std::pair<double, double>(norm_sum, timer.avg_msec());
+}
+
+/*
 std::complex<float> test_eigen(const std::complex<float>* _in_mat_arr,
                                PinvMode mode) {
   const size_t tot_size = FLAGS_n_rows * FLAGS_n_cols;
@@ -56,19 +73,19 @@ std::complex<float> test_eigen(const std::complex<float>* _in_mat_arr,
   auto* out_mat_arr = new std::complex<float>[tot_size];
   for (size_t i = 0; i < tot_size; i++) in_mat_arr[i] = _in_mat_arr[i];
 
-  Eigen::Map<Eigen::Matrix<std::complex<float>, Eigen::Dynamic, Eigen::Dynamic,
-                           Eigen::ColMajor>>
-      input(in_mat_arr, FLAGS_n_rows, FLAGS_n_cols);
+  Eigen::Map<Eigen::Matrix<std::complex<float>, Eigen::Dynamic,
+Eigen::Dynamic, Eigen::ColMajor>> input(in_mat_arr, FLAGS_n_rows,
+FLAGS_n_cols);
 
-  Eigen::Map<Eigen::Matrix<std::complex<float>, Eigen::Dynamic, Eigen::Dynamic,
-                           Eigen::ColMajor>>
-      output(out_mat_arr, FLAGS_n_cols, FLAGS_n_rows);
+  Eigen::Map<Eigen::Matrix<std::complex<float>, Eigen::Dynamic,
+Eigen::Dynamic, Eigen::ColMajor>> output(out_mat_arr, FLAGS_n_cols,
+FLAGS_n_rows);
 
   std::complex<float> ret(0.0, 0.0);
   TscTimer timer(FLAGS_n_iters, freq_ghz);
   for (size_t iter = 0; iter < FLAGS_n_iters; iter++) {
-    const bool take_measurement = (iter >= FLAGS_n_iters * warmup_fraction);
-    if (take_measurement) timer.start();
+    const bool take_measurement = (iter >= FLAGS_n_iters *
+warmup_fraction); if (take_measurement) timer.start();
 
     if (mode == PinvMode::kFormula) {
       output = (input.adjoint() * input).inverse() * input.adjoint();
@@ -88,29 +105,41 @@ std::complex<float> test_eigen(const std::complex<float>* _in_mat_arr,
       FLAGS_n_cols, timer.avg_msec(), timer.stddev_msec());
   return ret;
 }
+*/
 
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   freq_ghz = measure_rdtsc_freq();
-  nano_sleep(100 * 1000 * 1000, freq_ghz);  // Spin 100 ms to trigger turbo
+  nano_sleep(100 * 1000 * 1000,
+             freq_ghz);  // Spin 100 ms to trigger turbo
   mkl_set_num_threads(1);
 
-  auto* in_base_mat_arr = new arma::cx_float[FLAGS_n_rows * FLAGS_n_cols];
-  for (size_t i = 0; i < FLAGS_n_rows * FLAGS_n_cols; i++) {
-    auto re = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-    auto im = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-    in_base_mat_arr[i] = arma::cx_float(re, im);
+  std::vector<arma::cx_fmat> test_matrices;
+  for (size_t i = 0; i < FLAGS_n_iters; i++) {
+    test_matrices.push_back(
+        arma::randu<arma::cx_fmat>(FLAGS_n_rows, FLAGS_n_cols));
   }
 
-  std::pair<arma::cx_float, double> ret_1 =
-      test_arma(in_base_mat_arr, PinvMode::kFormula);
-  std::pair<arma::cx_float, double> ret_2 =
-      test_arma(in_base_mat_arr, PinvMode::kSVD);
+  // Part 1: Test inverse speed
+  std::pair<std::vector<arma::cx_fmat>, double> ret_1 =
+      arma_inverses(test_matrices, PinvMode::kSVD);
+  std::pair<std::vector<arma::cx_fmat>, double> ret_2 =
+      arma_inverses(test_matrices, PinvMode::kSVD);
 
   // Header: "Matrix_size Formula SVD SVD/Formula"
-  printf("%zux%zu %.3f %.3f ms %.2f\n", FLAGS_n_rows, FLAGS_n_cols,
-         ret_1.second, ret_2.second, ret_2.second / ret_1.second);
-  fprintf(stderr, "Arma: Results = {%.5f, %.5f}, {%.5f, %.5f}\n",
-          ret_1.first.real(), ret_1.first.imag(), ret_2.first.real(),
-          ret_2.first.imag());
+  printf("%zux%zu %.3f %.3f %.2f\n", FLAGS_n_rows, FLAGS_n_cols, ret_1.second,
+         ret_2.second, ret_2.second / ret_1.second);
+
+  double norm_sum = 0.0;
+  for (size_t i = 0; i < FLAGS_n_iters; i++) {
+    norm_sum += arma::norm(ret_1.first[i] - ret_2.first[i]);
+  }
+  fprintf(stderr, "Computation proof = %.2f\n", norm_sum);
+
+  /*
+  // Part 2: Test norm speed
+  std::pair<double, double> ret_3 = arma_norm(test_matrices);
+  printf("Norm of %zux%zu: %.3f ms (computation proof = %.3f)\n", FLAGS_n_rows,
+         FLAGS_n_cols, ret_3.second, ret_3.first);
+         */
 }
--- microbench/inverse_perf/run.sh
+++ microbench/inverse_perf/run.sh
@@ -2,6 +2,6 @@
 echo "Matrix_size Formula SVD SVD/Formula"
 for n_rows in `seq 8 8 64`; do
   for n_cols in `seq 8 8 ${n_rows}`; do 
-    ./bench --n_rows ${n_rows} --n_cols ${n_cols} --n_iters 1000 2>/dev/null
+    ./bench --n_rows ${n_rows} --n_cols ${n_cols} --n_iters 1000  2>/dev/null
   done
 done
