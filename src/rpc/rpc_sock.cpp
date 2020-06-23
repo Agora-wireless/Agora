69043d69a6a6b57150317bdb2978ae1e53b441fc
Gavindeed <jug257@g.harvard.edu>
2020-06-22 18:12:40 -0700

    Define worker_rpc in Millipede class
    Former-commit-id: 5aea2450fddbbffe0a95c284cce3a6333f5615c6

--- src/rpc/rpc_sock.cpp
+++ src/rpc/rpc_sock.cpp
@@ -22,6 +22,7 @@ RPCServer::RPCServer() {
     nexus->register_req_func(kReqType, server_req_handler);
 
     server_rpc = new erpc::Rpc<erpc::CTransport>(nexus, nullptr, 0, nullptr);
+    server_rpc->run_event_loop(100000);
 }
 
 void *RPCServer::Launch(int tid) {
@@ -38,6 +39,17 @@ RPCClient::RPCClient() {
     nexus = new erpc::Nexus(client_uri, 0, 0);
 
     client_rpc = new erpc::Rpc<erpc::CTransport>(nexus, nullptr, 0, sm_handler);
+
+    std::string server_uri = kServerHostname + ":" + std::to_string(kUDPPort);
+    int session_num = client_rpc->create_session(server_uri, 0);
+
+    while (!client_rpc->is_connected(session_num)) client_rpc->run_event_loop_once();
+
+    req = client_rpc->alloc_msg_buffer_or_die(kMsgSize);
+    resp = client_rpc->alloc_msg_buffer_or_die(kMsgSize);
+
+    client_rpc->enqueue_request(session_num, kReqType, &req, &resp, cont_func, nullptr);
+    client_rpc->run_event_loop(100);
 }
 
 void *RPCClient::Launch(int tid) {
