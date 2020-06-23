ac1d3d33f0d5e6d2b9adafc217da89f8d3196eea
rdoost <doost@rice.edu>
2020-06-22 21:20:45 -0500

    gracefully shut down when millipede assert condition is not met

--- src/millipede/millipede.cpp
+++ src/millipede/millipede.cpp
@@ -238,10 +238,15 @@ void Millipede::start()
 
                 auto* pkt = (Packet*)(socket_buffer_[socket_thread_id]
                     + (sock_buf_offset * cfg->packet_length));
-                rt_assert(pkt->frame_id < cur_frame_id + TASK_BUFFER_FRAME_NUM,
-                    "Error: Received packet for future frame beyond frame "
-                    "window. This can happen if Millipede is running "
-                    "slowly, e.g., in debug mode");
+                if (pkt->frame_id >= cur_frame_id + TASK_BUFFER_FRAME_NUM) {
+                    std::cout
+                        << "Error: Received packet for future frame beyond "
+                           "frame "
+                        << "window. This can happen if Millipede is running "
+                        << "slowly, e.g., in debug mode\n";
+                    cfg->running = false;
+                    break;
+                }
 
                 update_rx_counters(pkt->frame_id, pkt->symbol_id);
                 if (config_->bigstation_mode) {
