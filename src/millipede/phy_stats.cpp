dcdcb6cb81c8c730297d4f9eb4e58d90a7cdbc93
rdoost <doost@rice.edu>
2020-06-23 13:48:56 -0500

    New class PhyStats to collect statistics on physical layer performance

---
+++ src/millipede/phy_stats.cpp
@@ -0,0 +1,51 @@
+#include "phy_stats.hpp"
+
+PhyStats::PhyStats(Config *cfg)
+	: config_(cfg)
+{
+    const size_t task_buffer_symbol_num_ul
+        = cfg->ul_data_symbol_num_perframe * TASK_BUFFER_FRAME_NUM;
+    decoded_bits_count_.calloc(cfg->UE_NUM, task_buffer_symbol_num_ul, 64);
+    bit_error_count_.calloc(cfg->UE_NUM, task_buffer_symbol_num_ul, 64);
+}
+
+void PhyStats::print_phy_stats()
+{
+    auto& cfg = config_;
+    const size_t task_buffer_symbol_num_ul
+        = cfg->ul_data_symbol_num_perframe * TASK_BUFFER_FRAME_NUM;
+    for (size_t ue_id = 0; ue_id < cfg->UE_NUM; ue_id++) {
+        size_t total_decoded_bits(0);
+        size_t total_bit_errors(0);
+        for (size_t i = 0; i < task_buffer_symbol_num_ul; i++) {
+            total_decoded_bits += decoded_bits_count_[ue_id][i];
+            total_bit_errors += bit_error_count_[ue_id][i];
+        }
+        std::cout << "UE " << ue_id << " bit errors " << total_bit_errors
+                  << "(" << total_decoded_bits << "), BER "
+                  << 1.0 * total_bit_errors / total_decoded_bits
+                  << std::endl;
+    }
+}
+
+void PhyStats::update_bit_errors(size_t ue_id, size_t offset, uint8_t tx_byte, uint8_t rx_byte)
+{
+    uint8_t xor_byte(tx_byte ^ rx_byte);
+    size_t bit_errors = 0;
+    for (size_t j = 0; j < 8; j++) {
+        bit_errors += xor_byte & 1;
+        xor_byte >>= 1;
+    }
+    bit_error_count_[ue_id][offset] += bit_errors;
+}
+
+void PhyStats::update_decoded_bits(size_t ue_id, size_t offset, size_t new_bits_num)
+{
+    decoded_bits_count_[ue_id][offset] += new_bits_num;
+}
+
+PhyStats::~PhyStats() {
+    decoded_bits_count_.free();
+    bit_error_count_.free();
+}
+
