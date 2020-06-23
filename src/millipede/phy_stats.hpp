2746bf59c8b79398cce8d18e804d7a32e69f6aa7
rdoost <doost@rice.edu>
2020-06-23 18:35:13 -0500

    moving EVM updates and prints from DeDemul to PhyStats

--- src/millipede/phy_stats.hpp
+++ src/millipede/phy_stats.hpp
@@ -5,7 +5,9 @@
 #include "Symbols.hpp"
 #include "config.hpp"
 #include "memory_manage.h"
+#include <armadillo>
 
+using namespace arma;
 class PhyStats {
 public:
     PhyStats(Config* cfg);
@@ -15,6 +17,8 @@ public:
     void update_decoded_bits(size_t, size_t, size_t);
     void update_block_errors(size_t, size_t, size_t);
     void increment_decoded_blocks(size_t, size_t);
+    void update_evm_stats(size_t, size_t, cx_fmat);
+    void print_evm_stats(size_t);
 
 private:
     Config* config_;
@@ -22,6 +26,9 @@ private:
     Table<size_t> bit_error_count_;
     Table<size_t> decoded_blocks_count_;
     Table<size_t> block_error_count_;
+    Table<float> evm_buffer_;
+
+    cx_fmat ul_gt_mat;
 };
 
 #endif
