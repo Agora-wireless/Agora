9c8785d4becf348faea638cfc224993feee1c518
rdoost <doost@rice.edu>
2020-06-28 15:48:15 -0500

    add support for running multiple instances of the Client baseband processing on multiple machines
    Former-commit-id: f4bff6c0a6b8d309184e21fad50a11deb8b3e57d

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -96,6 +96,8 @@ Config::Config(std::string jsonfile)
     DL_PILOT_SYMS = tddConf.value("client_dl_pilot_syms", 0);
     UL_PILOT_SYMS = tddConf.value("client_ul_pilot_syms", 0);
     cl_tx_advance = tddConf.value("tx_advance", 100);
+    ue_ant_offset = tddConf.value("ue_ant_offset", 0);
+    total_ue_ant_num = tddConf.value("total_ue_ant_num", UE_ANT_NUM);
     hw_framer = tddConf.value("hw_framer", true);
     if (tddConf.find("frames") == tddConf.end()) {
         symbol_num_perframe = tddConf.value("symbol_num_perframe", 70);
@@ -341,7 +343,7 @@ void Config::genData()
     auto zc_ue_pilot = Utils::double_to_cfloat(zc_ue_pilot_double);
     for (size_t i = 0; i < UE_ANT_NUM; i++) {
         auto zc_ue_pilot_i = CommsLib::seqCyclicShift(
-            zc_ue_pilot, i * (float)M_PI / 6); // LTE DMRS
+            zc_ue_pilot, (i + ue_ant_offset) * (float)M_PI / 6); // LTE DMRS
         for (size_t j = 0; j < OFDM_DATA_NUM; j++) {
             ue_specific_pilot[i][j]
                 = { zc_ue_pilot_i[j].real(), zc_ue_pilot_i[j].imag() };
@@ -380,7 +382,7 @@ void Config::genData()
     std::string cur_directory1 = TOSTRING(PROJECT_DIRECTORY);
     std::string filename1 = cur_directory1
         + (kUseLDPC ? "/data/LDPC_orig_data_" : "/data/orig_data_")
-        + std::to_string(OFDM_CA_NUM) + "_ant" + std::to_string(UE_ANT_NUM)
+        + std::to_string(OFDM_CA_NUM) + "_ant" + std::to_string(total_ue_ant_num)
         + ".bin";
     FILE* fd = fopen(filename1.c_str(), "rb");
     if (fd == NULL) {
@@ -388,11 +390,15 @@ void Config::genData()
         std::cerr << "Error: " << strerror(errno) << std::endl;
     }
     for (size_t i = 0; i < ul_data_symbol_num_perframe; i++) {
+	if (fseek(fd, num_bytes_per_ue * ue_ant_offset, SEEK_SET) != 0)
+            return;
         size_t r = fread(
             ul_bits[i], sizeof(int8_t), num_bytes_per_ue * UE_ANT_NUM, fd);
         if (r < num_bytes_per_ue * UE_ANT_NUM)
             printf(
                 "bad read from file %s (batch %zu) \n", filename1.c_str(), i);
+	if (fseek(fd, num_bytes_per_ue * (total_ue_ant_num - ue_ant_offset - UE_ANT_NUM), SEEK_SET) != 0)
+            return;
     }
     for (size_t i = 0; i < dl_data_symbol_num_perframe; i++) {
         size_t r = fread(
