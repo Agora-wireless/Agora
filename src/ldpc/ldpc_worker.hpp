b43c4454077e72ee4df1acebec79181ad3db2f15
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-07-01 12:24:00 -0500

    Add some code suggestions

--- src/ldpc/ldpc_worker.hpp
+++ src/ldpc/ldpc_worker.hpp
@@ -1,30 +1,36 @@
+// XXX Some documentation about the file
+
 #ifndef LDPC_WORKER
 #define LDPC_WORKER
 
-#include "rpc_context.hpp"
+#include "config.hpp"
 #include "encoder.hpp"
 #include "phy_ldpc_decoder_5gnr.h"
-#include "config.hpp"
+#include "rpc_context.hpp"
 #include <cstdint>
 
+// XXX: Need a comment for global variables
+// static constexpr kLPDCMaxBufferSize = 65536;
 #define LDPC_MAX_BUFFER_SIZE (65536)
 
-void ldpc_req_handler(erpc::ReqHandle *req_handle, void * _context);
+void ldpc_req_handler(erpc::ReqHandle* req_handle, void* _context);
 
+// XXX doc for the class & public members
 class LDPCWorker {
 public:
-    LDPCWorker(Config *config, int tid, erpc::Nexus *nexus);
+    LDPCWorker(Config* config, int tid, erpc::Nexus* nexus);
     ~LDPCWorker();
 
     void serve();
-    bool decode(int8_t *in_buffer, uint8_t *out_buffer);
+    bool decode(int8_t* in_buffer, uint8_t* out_buffer);
     size_t get_decoded_bits();
 
 private:
-    Config *cfg;
-    int tid;
-    int16_t *resp_var_nodes;
-    RPCContext *ctx;
+    // XXX: Short doc if needed for private variables
+    Config* cfg;
+    int tid; // Thread ID as defined by eRPC
+    int16_t* resp_var_nodes;
+    RPCContext* ctx;
     size_t decoded_bits;
 };
 
