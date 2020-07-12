2d9005d9ec2398ab110a4f4d2df3af5903dee6cb
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-07-12 15:48:34 -0700

    Compile encoder test with FlexRAN encoder with gcc

--- src/encoder/compile_encoder_test.sh
+++ src/encoder/compile_encoder_test.sh
@@ -1,27 +1,25 @@
 # Compile with local libraries and avx2
 
-FLEXRAN_SDK="${HOME}/FlexRAN-FEC-SDK-19-04/sdk"
+FLEXRAN_FEC_SDK_DIR="${HOME}/FlexRAN-FEC-SDK-19-04/sdk"
 
 compile_with_millipede_encoder() {
   g++ -std=c++11 -mavx2 -Wall \
     -I. \
-    -isystem ${FLEXRAN_SDK}/source/phy/lib_ldpc_encoder_5gnr \
-    -isystem ${FLEXRAN_SDK}/source/phy/lib_common \
+    -isystem ${FLEXRAN_FEC_SDK_DIR}/source/phy/lib_ldpc_encoder_5gnr \
+    -isystem ${FLEXRAN_FEC_SDK_DIR}/source/phy/lib_common \
     encoder_test.cpp encoder.cpp cyclic_shift.cpp iobuffer.cpp -o test_avx2
 }
 
 compile_with_flexran_encoder() {
-  icpc -g -debug all -std=c++11 -march=native -Wall -check-pointers=write \
+  FLEXRAN_FEC_LIB_DIR=${FLEXRAN_FEC_SDK_DIR}/build-avx512-icc
+  g++ -g -std=c++11 -march=native -Wall -no-pie \
     -DUSE_LDPC=on  -D_BBLIB_AVX512_ \
     -I. \
-    -isystem ${FLEXRAN_SDK}/source/phy/lib_ldpc_encoder_5gnr \
-    -isystem ${FLEXRAN_SDK}/source/phy/lib_common \
+    -isystem ${FLEXRAN_FEC_SDK_DIR}/source/phy/lib_ldpc_encoder_5gnr \
+    -isystem ${FLEXRAN_FEC_SDK_DIR}/source/phy/lib_common \
     encoder_test.cpp \
-    ${FLEXRAN_SDK}/source/phy/lib_ldpc_encoder_5gnr/phy_ldpc_encoder_5gnr_avx512.cpp \
-    ${FLEXRAN_SDK}/source/phy/lib_ldpc_encoder_5gnr/phy_ldpc_encoder_5gnr.cpp \
-    ${FLEXRAN_SDK}/source/phy/lib_ldpc_encoder_5gnr/ldpc_encoder_cycshift.cpp \
-    -L ${FLEXRAN_SDK}/build-avx512-icc/source/phy/lib_common \
-    -lcommon \
+    ${FLEXRAN_FEC_LIB_DIR}/source/phy/lib_ldpc_encoder_5gnr/libldpc_encoder_5gnr.a \
+    ${FLEXRAN_FEC_LIB_DIR}/source/phy/lib_common/libcommon.a \
     -o test_avx512
 }
 
