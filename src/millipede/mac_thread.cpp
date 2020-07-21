aa5b6c1407556cf3efa0112fbf2c7ec3a5c8839e
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-07-20 22:23:13 -0700

    Use UDPClient class in mac_thread
    Former-commit-id: 68a5dec4bbb195f18b78278e2d090e1e4984aff2

--- src/millipede/mac_thread.cpp
+++ src/millipede/mac_thread.cpp
@@ -17,30 +17,12 @@ MacThread::MacThread(Config* cfg, size_t core_offset,
     , tx_ptok_(tx_ptok)
     , rx_ptok_(rx_ptok)
 {
-    // Set up sockets to push MAC data to applications
-    app_sockets_.resize(cfg_->UE_NUM);
-    app_sockaddr_.resize(cfg_->UE_NUM);
-
     // Set up MAC log file
     if (log_filename != "") {
-        // Use a non-default log filename
-        log_filename_ = log_filename;
+        log_filename_ = log_filename; // Use a non-default log filename
     }
     log_file_ = fopen("/tmp/millipede_mac_log", "w");
     rt_assert(log_file_ != nullptr, "Failed to open MAC log file");
-
-    for (size_t i = 0; i < cfg_->UE_NUM; i++) {
-        size_t local_port = kBaseLocalPort + i;
-        size_t remote_port = kBaseRemotePort + i;
-        std::string remote_addr = "127.0.0.1";
-        size_t sock_buf_size = 1024 * 1024 * 64 * 8 - 1;
-        app_sockets_[i] = setup_socket_ipv4(local_port, true, sock_buf_size);
-        setup_sockaddr_remote_ipv4(
-            &app_sockaddr_[i], remote_port, remote_addr.c_str());
-        printf("MAC thread: Sending data to app at %s:%zu from port %zu. "
-               "app_sockets_[i] = %d\n",
-            remote_addr.c_str(), remote_port, local_port, app_sockets_[i]);
-    }
 }
 
 MacThread::~MacThread()
@@ -55,7 +37,10 @@ void MacThread::run_event_loop()
     pin_to_core_with_offset(
         ThreadType::kWorkerMacTXRX, core_offset_, 0 /* thread ID */);
 
-    std::vector<uint8_t> frame_data(cfg_->mac_data_bytes_num_perframe);
+    // Staging buffers to accumulate data for each UE
+    std::vector<uint8_t> frame_data[kMaxUEs];
+    for (auto& v : frame_data)
+        v.resize(cfg_->mac_data_bytes_num_perframe);
 
     const size_t cbLenBytes = (cfg_->LDPC_config.cbLen + 7) >> 3;
     const size_t ul_data_syms
@@ -63,7 +48,7 @@ void MacThread::run_event_loop()
     std::stringstream ss;
     std::stringstream ss1[ul_data_syms];
 
-    size_t num_filled_bytes_in_frame = 0;
+    size_t num_filled_bytes_in_frame[kMaxUEs] = { 0 };
     while (cfg_->running) {
         Event_data event;
         if (!rx_queue_->try_dequeue_from_producer(*tx_ptok_, event))
@@ -88,9 +73,9 @@ void MacThread::run_event_loop()
             const size_t frame_data_offset
                 = (symbol_idx_ul - cfg_->UL_PILOT_SYMS)
                 * cfg_->data_bytes_num_persymbol;
-            memcpy(&frame_data[frame_data_offset], ul_data_ptr,
+            memcpy(&frame_data[ue_id][frame_data_offset], ul_data_ptr,
                 cfg_->data_bytes_num_persymbol);
-            num_filled_bytes_in_frame += cfg_->data_bytes_num_persymbol;
+            num_filled_bytes_in_frame[ue_id] += cfg_->data_bytes_num_persymbol;
 
             // Print information about the received symbol
             if (kDebugBSReceiver) {
@@ -108,24 +93,18 @@ void MacThread::run_event_loop()
             }
         }
 
-        // When the frame is full, send it to the application.
-        if (num_filled_bytes_in_frame == cfg_->data_bytes_num_perframe) {
-            num_filled_bytes_in_frame = 0;
+        // When the frame is full, send it to the application
+        if (num_filled_bytes_in_frame[ue_id] == cfg_->data_bytes_num_perframe) {
+            num_filled_bytes_in_frame[ue_id] = 0;
 
-            int ret = sendto(app_sockets_[ue_id], &frame_data[0],
-                cfg_->mac_data_bytes_num_perframe, 0,
-                (struct sockaddr*)&app_sockaddr_[ue_id], sizeof(sockaddr_in));
-            if (ret == -1) {
-                fprintf(stderr, "MAC thread: sendto() failed with error: %s\n",
-                    strerror(errno));
-                exit(-1);
-            }
+            udp_client.send(kRemoteHostname, kBaseRemotePort,
+                &frame_data[ue_id][0], cfg_->mac_data_bytes_num_perframe);
 
             fprintf(log_file_,
                 "MAC thread: Sent data for frame %zu, ue %zu, size %zu\n",
                 frame_id, ue_id, cfg_->mac_data_bytes_num_perframe);
-            for (size_t i = 0; i < cfg_->data_bytes_num_persymbol; i++) {
-                ss << std::to_string(frame_data[i]) << " ";
+            for (size_t i = 0; i < cfg_->mac_data_bytes_num_perframe; i++) {
+                ss << std::to_string(frame_data[ue_id][i]) << " ";
             }
             fprintf(log_file_, "%s\n", ss.str().c_str());
             ss.str("");
