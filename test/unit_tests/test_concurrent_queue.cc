d421f0cb53f8029d941415b1ed65c17fbb512ab0
Gavindeed <jug257@g.harvard.edu>
2020-07-27 14:21:52 -0700

    Basic concurrent queue test
    Former-commit-id: 31e5e96016f63702221203296a2e6c7e3691d1a2

---
+++ test/unit_tests/test_concurrent_queue.cc
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+// For some reason, gtest include order matters
+#include "concurrentqueue.h"
+#include <thread>
+
+static constexpr size_t kNumWorker = 2;
+static constexpr size_t kMaxTestNum = (1 << 24);
+
+void run_master(moodycamel::ConcurrentQueue<size_t>** queues,
+    moodycamel::ProducerToken** ptoks)
+{
+    for (size_t i = 0; i < kMaxTestNum; i++) {
+        queues[i % kNumWorker]->enqueue(*ptoks[i % kNumWorker], i);
+    }
+}
+
+void run_worker(size_t worker_id, moodycamel::ConcurrentQueue<size_t>* queue,
+    moodycamel::ConsumerToken* ctok)
+{
+    size_t next_expected = worker_id;
+    while (next_expected < kMaxTestNum) {
+        size_t item;
+        if (queue->try_dequeue(*ctok, item)) {
+            ASSERT_EQ(item, next_expected);
+            next_expected += kNumWorker;
+        }
+    }
+}
+
+TEST(TestConcurrentQueue, Correctness)
+{
+    auto** queues = new moodycamel::ConcurrentQueue<size_t>*[kNumWorker];
+    auto** ptoks = new moodycamel::ProducerToken*[kNumWorker];
+    auto** ctoks = new moodycamel::ConsumerToken*[kNumWorker];
+    for (size_t i = 0; i < kNumWorker; i++) {
+        queues[i] = new moodycamel::ConcurrentQueue<size_t>;
+        ptoks[i] = new moodycamel::ProducerToken(*queues[i]);
+        ctoks[i] = new moodycamel::ConsumerToken(*queues[i]);
+    }
+    auto* master = new std::thread(run_master, queues, ptoks);
+    auto** workers = new std::thread*[kNumWorker];
+    for (size_t i = 0; i < kNumWorker; i++) {
+        workers[i] = new std::thread(run_worker, i, queues[i], ctoks[i]);
+    }
+    master->join();
+    for (size_t i = 0; i < kNumWorker; i++) {
+        workers[i]->join();
+    }
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
