46a894f188979bf8608da85cf98b0a3c141a6dd0
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-08-19 15:45:36 -0700

    Add more documentation to adapt_bits_for_mod, and a stress test in the unit test

--- test/unit_tests/test_datatype_conversion.cc
+++ test/unit_tests/test_datatype_conversion.cc
@@ -12,7 +12,7 @@
 
 static constexpr size_t kSIMDTestNum = 1024;
 
-TEST(Modulation, adapt_bits_for_mod)
+TEST(Modulation, adapt_bits_for_mod_one)
 {
     static constexpr auto kModOrder = CommsLib::ModulationOrder::QAM64;
     static constexpr size_t kInputBytes = 8;
@@ -44,6 +44,48 @@ TEST(Modulation, adapt_bits_for_mod)
     printf("\n");
 
     ASSERT_EQ(input, regen_input);
+}
+
+TEST(Modulation, adapt_bits_for_mod_stress)
+{
+    std::vector<size_t> modulations = { CommsLib::ModulationOrder::QAM16,
+        CommsLib::ModulationOrder::QAM64 };
+
+    for (size_t mod_type : modulations) {
+        for (size_t iter = 0; iter < 1000; iter++) {
+            const size_t num_input_bytes = rand() % 10000;
+            std::vector<uint8_t> input(num_input_bytes);
+            std::vector<uint8_t> regen_input(num_input_bytes);
+            std::vector<uint8_t> output(
+                std::ceil(num_input_bytes * 8.0 / mod_type));
+            for (size_t i = 0; i < num_input_bytes; i++) {
+                input[i] = rand();
+                output[i] = 0;
+            }
+
+            adapt_bits_for_mod(
+                &input[0], &output[0], num_input_bytes, mod_type);
+
+            // Sanity check: Input and output must have same number of set bits
+            size_t set_bits_in_input = 0;
+            size_t set_bits_in_output = 0;
+            for (uint8_t& i : input)
+                set_bits_in_input += __builtin_popcount(i);
+            for (uint8_t& o : output)
+                set_bits_in_output += __builtin_popcount(o);
+            ASSERT_EQ(set_bits_in_input, set_bits_in_output);
+
+            // Sanity check: Output bytes must have at most mod_type bits set
+            for (uint8_t& o : output) {
+                ASSERT_LE(o, ((1 << mod_type) - 1));
+            }
+
+            adapt_bits_from_mod(
+                &output[0], &regen_input[0], output.size(), mod_type);
+
+            ASSERT_EQ(input, regen_input);
+        }
+    }
 }
 
 TEST(SIMD, float_32_to_16)
