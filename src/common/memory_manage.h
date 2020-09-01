f499fb0b20a3ca2bcee8c4460e836fbd203b90e5
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-08-31 17:18:56 -0700

    Save work on static dimensions for tables

--- src/common/memory_manage.h
+++ src/common/memory_manage.h
@@ -5,8 +5,10 @@
  */
 #ifndef MEMORY_MANAGE
 #define MEMORY_MANAGE
+#include <array>
 #include <cstdlib>
 #include <cstring>
+#include <malloc.h>
 #include <random>
 #include <stdio.h>
 
@@ -81,5 +83,72 @@ static void alloc_buffer_1d(T** buffer, U dim, int aligned_bytes, int init_zero)
 };
 
 template <typename T> static void free_buffer_1d(T** buffer) { free(*buffer); };
+
+// PMat2D is a 2D pointer matrix with [ROWS] rows and [COLS] columns. Each entry
+// of the matrix is a pointer to an array of [T].
+template <size_t ROWS, size_t COLS, class T> class PMat2D {
+public:
+    PMat2D() {}
+
+    /// Create a pointer matrix with [n_entries] entries per matrix cell
+    PMat2D(size_t n_entries) { alloc(n_entries); }
+
+    ~PMat2D()
+    {
+        if (allocated) {
+            for (auto& row : mat) {
+                for (auto& entry : row) {
+                    free(entry);
+                }
+            }
+        }
+    }
+
+    /// Allocate [n_entries] entries per pointer cell in the pointer matrix
+    void alloc(size_t n_entries)
+    {
+        for (auto& row : mat) {
+            for (auto& entry : row) {
+                entry
+                    = reinterpret_cast<T*>(memalign(64, n_entries * sizeof(T)));
+                memset(reinterpret_cast<uint8_t*>(entry), 0,
+                    n_entries * sizeof(T));
+            }
+        }
+    }
+
+    /// Allocate [n_entries] entries per pointer cell in the pointer matrix.
+    /// Each entry is a random float between -1.0 and 1.0.
+    void rand_alloc_cx_float(size_t n_entries)
+    {
+        static_assert(
+            sizeof(T) == 2 * sizeof(float), "T must be complex_float");
+        alloc(n_entries);
+
+        std::default_random_engine generator;
+        std::uniform_real_distribution<float> distribution(-1.0, 1.0);
+
+        for (auto& row : mat) {
+            for (auto& entry : row) {
+                auto* base = reinterpret_cast<float*>(entry);
+                for (size_t i = 0; i < n_entries * 2; i++) {
+                    base[i] = distribution(generator);
+                }
+            }
+        }
+    }
+
+    /// Free the memory used by the pointer cells
+    T** operator[](size_t row_idx)
+    {
+        return reinterpret_cast<T**>(&mat[row_idx][0]);
+    }
+
+private:
+    std::array<std::array<T*, COLS>, ROWS> mat; /// The pointer cells
+
+    /// True iff we've allocated memory for the pointer cells
+    bool allocated = false;
+};
 
 #endif
--- test/unit_tests/test_zf_threaded.cc
+++ test/unit_tests/test_zf_threaded.cc
@@ -51,7 +51,8 @@ void MasterToWorkerDynamic_master(Config* cfg,
 void MasterToWorkerDynamic_worker(Config* cfg, size_t worker_id,
     double freq_ghz, moodycamel::ConcurrentQueue<Event_data>& event_queue,
     moodycamel::ConcurrentQueue<Event_data>& complete_task_queue,
-    moodycamel::ProducerToken* ptok, Table<complex_float>& csi_buffer,
+    moodycamel::ProducerToken* ptok,
+    PMat2D<TASK_BUFFER_FRAME_NUM, kMaxUEs, complex_float> csi_buffers,
     Table<complex_float>& recip_buffer, Table<complex_float>& ul_zf_buffer,
     Table<complex_float>& dl_zf_buffer, Stats* stats)
 {
@@ -65,7 +66,7 @@ void MasterToWorkerDynamic_worker(Config* cfg, size_t worker_id,
     }
     auto computeZF
         = new DoZF(cfg, worker_id, freq_ghz, event_queue, complete_task_queue,
-            ptok, csi_buffer, recip_buffer, ul_zf_buffer, dl_zf_buffer, stats);
+            ptok, csi_buffers, recip_buffer, ul_zf_buffer, dl_zf_buffer, stats);
 
     size_t start_tsc = rdtsc();
     size_t num_tasks = 0;
@@ -113,10 +114,11 @@ TEST(TestZF, VaryingConfig)
         ptoks[i] = new moodycamel::ProducerToken(complete_task_queue);
     }
 
-    Table<complex_float> csi_buffer, ul_zf_buffer, dl_zf_buffer, recip_buffer;
-    csi_buffer.rand_alloc_cx_float(
-        cfg->pilot_symbol_num_perframe * TASK_BUFFER_FRAME_NUM,
-        kMaxAntennas * k5GMaxSubcarriers, 64);
+    Table<complex_float> ul_zf_buffer, dl_zf_buffer, recip_buffer;
+
+    PMat2D<TASK_BUFFER_FRAME_NUM, kMaxUEs, complex_float> csi_buffers;
+    csi_buffers.rand_alloc_cx_float(cfg->BS_ANT_NUM * cfg->OFDM_DATA_NUM);
+
     ul_zf_buffer.calloc(
         k5GMaxSubcarriers * TASK_BUFFER_FRAME_NUM, kMaxUEs * kMaxAntennas, 64);
     dl_zf_buffer.calloc(
@@ -132,7 +134,7 @@ TEST(TestZF, VaryingConfig)
     for (size_t i = 0; i < kNumWorkers; i++) {
         workers[i] = std::thread(MasterToWorkerDynamic_worker, cfg, i, freq_ghz,
             std::ref(event_queue), std::ref(complete_task_queue), ptoks[i],
-            std::ref(csi_buffer), std::ref(recip_buffer),
+            std::ref(csi_buffers), std::ref(recip_buffer),
             std::ref(ul_zf_buffer), std::ref(dl_zf_buffer), stats);
     }
     master.join();
