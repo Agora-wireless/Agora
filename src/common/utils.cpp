2b886c485bb05512ed5a4ac40035f3a171148449
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-09-09 18:37:18 -0700

    Reduce print verbosity
    Former-commit-id: e28a33c4b5f709055f716fa0e7eb3b63522f5211

--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -25,7 +25,7 @@ int pin_to_core(int core_id)
 }
 
 void pin_to_core_with_offset(
-    ThreadType thread_type, int core_offset, int thread_id)
+    ThreadType thread_type, int core_offset, int thread_id, bool verbose)
 {
     if (!kEnableThreadPinning)
         return;
@@ -47,8 +47,11 @@ void pin_to_core_with_offset(
             thread_type_str(thread_type).c_str(), thread_id, actual_core_id);
         exit(0);
     } else {
-        printf("%s thread %d: pinned to core %d\n",
-            thread_type_str(thread_type).c_str(), thread_id, actual_core_id);
+        if (verbose) {
+            printf("%s thread %d: pinned to core %d\n",
+                thread_type_str(thread_type).c_str(), thread_id,
+                actual_core_id);
+        }
     }
 }
 
