a6ad034143be709949242f15b6e3263a79242e1c
Jian Ding <jianding17@gmail.com>
2020-10-19 12:19:52 -0400

    Use pthread for creating dpdk threads
    This change is to let threads be pinned to cores with correct layout
    
    
    Former-commit-id: fa43b41ebd1ea65bcb014418edd5a3648f2b15fa

--- src/agora/txrx/txrx_DPDK.cpp
+++ src/agora/txrx/txrx_DPDK.cpp
@@ -70,22 +70,14 @@ bool PacketTXRX::startTXRX(Table<char>& buffer, Table<int>& buffer_status,
     packet_num_in_buffer_ = packet_num_in_buffer;
     tx_buffer_ = tx_buffer;
 
-    unsigned int lcore_id;
-    size_t worker_id = 0;
-    // Launch specific task to cores
-    RTE_LCORE_FOREACH_SLAVE(lcore_id)
-    {
-        // launch communication and task thread onto specific core
-        if (worker_id < socket_thread_num) {
-            auto context = new EventHandlerContext<PacketTXRX>;
-            context->obj_ptr = this;
-            context->id = worker_id;
-            rte_eal_remote_launch(
-                (lcore_function_t*)
-                    pthread_fun_wrapper<PacketTXRX, &PacketTXRX::loop_tx_rx>,
-                context, lcore_id);
-        }
-        worker_id++;
+    for (size_t i = 0; i < socket_thread_num; i++) {
+        pthread_t txrx_thread;
+        auto context = new EventHandlerContext<PacketTXRX>;
+        context->obj_ptr = this;
+        context->id = i;
+        int ret = pthread_create(&txrx_thread, NULL,
+            pthread_fun_wrapper<PacketTXRX, &PacketTXRX::loop_tx_rx>, context);
+        rt_assert(ret == 0, "Failed to create threads");
     }
     return true;
 }
@@ -99,6 +91,8 @@ void PacketTXRX::send_beacon(int tid, size_t frame_id)
 
 void* PacketTXRX::loop_tx_rx(int tid)
 {
+    pin_to_core_with_offset(
+        ThreadType::kWorkerTXRX, core_offset, tid, false /* quiet */);
     size_t rx_offset = 0;
     size_t prev_frame_id = SIZE_MAX;
     const uint16_t port_id = tid % cfg->dpdk_num_ports;
