7253b4c654bad5aa08105869eefc702347502cd8
Andrew Sedlmayr <Andrew.Sedlmayr@rice.edu>
2020-12-02 16:28:10 -0600

    Testing looking good so far.
    Former-commit-id: c9b345c81e1c1289a97b5d363af5781cbb38724b

--- src/common/memory_manage.h
+++ src/common/memory_manage.h
@@ -6,88 +6,98 @@
 #include <cstring>
 #include <random>
 #include <cstdio>
+#include <complex>
+
+namespace Agora_memory {
+    enum class Alignment_t : size_t
+    {
+        k32Align = 32,
+        k64Align = 64,
+        k4096Align = 4096
+    };
+
+    void *padded_aligned_alloc(Alignment_t alignment, size_t size);
+}
 
 template <typename T> class Table {
 private:
-    size_t dimension;
-    void* data;
+    size_t dim2_;
+    size_t dim1_;
+    T*     data_;
 
 public:
-    Table(void)
-        : dimension(0)
-        , data(NULL)
+    Table(void) : dim2_(0), dim1_(0), data_(nullptr) { }
+
+    void malloc(size_t dim1, size_t dim2, Agora_memory::Alignment_t alignment)
     {
+        this->dim2_ = dim2;
+        this->dim1_ = dim1;
+        size_t alloc_size = (this->dim1_ * this->dim2_ * sizeof(T));
+        this->data_ = static_cast<T*>(Agora_memory::padded_aligned_alloc(alignment, alloc_size));
     }
-    void malloc(size_t dim1, size_t dim2, size_t aligned_bytes)
-    {
-        aligned_bytes = (aligned_bytes) / 32 * 32;
-        dimension = (dim2 * sizeof(T) + aligned_bytes - 1) & -aligned_bytes;
-        size_t alloc_size = (dim1 * dimension);
-        data = std::aligned_alloc(aligned_bytes, alloc_size);
-    }
-    void calloc(size_t dim1, size_t dim2, size_t aligned_bytes)
+    void calloc(size_t dim1, size_t dim2, Agora_memory::Alignment_t alignment)
     {
         //assert(!((dim1 == 0) || (dim2 == 0)));
-        this->malloc(dim1, dim2, aligned_bytes);
-        std::memset(data, 0, dim1 * dimension);
+        this->malloc(dim1, dim2, alignment);
+        std::memset(this->data_, 0, (this->dim1_ * this->dim2_ * sizeof(T)));
     }
 
     // Allocate the table and fill it with random floating point values between
     // -1.0 and 1.0
-    void rand_alloc_float(size_t dim1, size_t dim2, size_t aligned_bytes)
+    void rand_alloc_float(size_t dim1, size_t dim2, Agora_memory::Alignment_t alignment)
     {
         std::default_random_engine generator;
         std::uniform_real_distribution<float> distribution(-1.0, 1.0);
 
-        malloc(dim1, dim2, aligned_bytes);
-        auto* base = reinterpret_cast<float*>(data);
-        for (size_t i = 0; i < dim1 * dim2; i++) {
+        assert(sizeof(T) >= sizeof(float));
+        this->malloc(dim1, dim2, alignment);
+        auto* base = reinterpret_cast<float*>(this->data_);
+        for (size_t i = 0; i < (dim1 * dim2); i++) {
             base[i] = distribution(generator);
         }
     }
 
     // Allocate the table and fill it with random complex floating point values
     // between -1.0 and 1.0
-    void rand_alloc_cx_float(size_t dim1, size_t dim2, size_t aligned_bytes)
+    void rand_alloc_cx_float(size_t dim1, size_t dim2, Agora_memory::Alignment_t alignment)
     {
         std::default_random_engine generator;
         std::uniform_real_distribution<float> distribution(-1.0, 1.0);
 
-        malloc(dim1, dim2, aligned_bytes);
-        auto* base = reinterpret_cast<float*>(data);
-        for (size_t i = 0; i < dim1 * dim2 * 2; i++) {
-            base[i] = distribution(generator);
+        assert(sizeof(T) >= sizeof(std::complex<float>));
+        this->malloc(dim1, dim2, alignment);
+        auto* base = reinterpret_cast<std::complex<float> *>(this->data_);
+        for (size_t i = 0; i < (dim1 * dim2); i++) {
+            base[i] = { distribution(generator), distribution(generator) };
         }
     }
 
-    bool is_allocated() { return dimension > 0; }
+    bool is_allocated( void ) { return (this->data_ != nullptr); }
 
     void free(void)
     {
-        std::free(data);
-        dimension = 0;
-        data = NULL;
+        if (this->data_ != nullptr)
+        {
+            std::free(this->data_);
+        }
+        this->dim2_ = 0;
+        this->dim1_ = 0;
+        this->data_ = nullptr;
     }
 
-    T* operator[](int dim1) { return (T*)((char*)data + dim1 * dimension); }
+    T* operator[](size_t dim1) {
+        assert(this->dim1_ > dim1);
+        return (this->data_ + (dim1 * this->dim2_)); 
+    }
 };
 
 template <typename T, typename U>
-static void alloc_buffer_1d(T** buffer, U dim, size_t aligned_bytes, int init_zero)
+static void alloc_buffer_1d(T** buffer, U dim, Agora_memory::Alignment_t alignment, int init_zero)
 {
-    //aligned_bytes = (aligned_bytes) / 32 * 32;
     size_t size = dim * sizeof(T);
-    size_t padded_size = size;
-    //Check for power of 2 alignment
-    assert((aligned_bytes & (aligned_bytes - 1)) == 0);
-    size_t padding = aligned_bytes - (size % aligned_bytes);
-
-    if (padding < aligned_bytes) {
-        padded_size += padding;
-    }
-    *buffer = static_cast<T*>(std::aligned_alloc(aligned_bytes, padded_size));
+    *buffer = static_cast<T*>(Agora_memory::padded_aligned_alloc(alignment, size));
     if (init_zero) {
-        std::memset(*buffer, 0, padded_size);
+        std::memset(*buffer, 0u, size);
     }
 };
 
@@ -97,11 +107,11 @@ template <typename T> static void free_buffer_1d(T** buffer) { std::free(*buffer
 // entry of the grid is a pointer to an array of [T].
 template <size_t ROWS, size_t COLS, class T> class PtrGrid {
 public:
-    PtrGrid() {}
+    PtrGrid(void): backing_buf_(nullptr) {}
 
     /// Create a grid of pointers where each grid cell points to an array of
     /// [n_entries]
-    PtrGrid(size_t n_entries) { alloc(ROWS, COLS, n_entries); }
+    PtrGrid(size_t n_entries) { this->alloc(ROWS, COLS, n_entries); }
 
     /// Create a grid of pointers with dimensions [ROWS, COLS], where
     /// only the grid with dimensions [n_rows, n_cols] has cells pointing to an
@@ -110,13 +120,14 @@ public:
     PtrGrid(size_t n_rows, size_t n_cols, size_t n_entries)
     {
         assert(n_rows <= ROWS && n_cols <= COLS);
-        alloc(n_rows, n_cols, n_entries);
+        this->alloc(n_rows, n_cols, n_entries);
     }
 
     ~PtrGrid(void)
     {
-        if (is_allocated) {
-            std::free(backing_buf);
+        if (this->backing_buf_ != nullptr) {
+            std::free(this->backing_buf_);
+            this->backing_buf_ = nullptr;
         }
     }
 
@@ -124,15 +135,14 @@ public:
     void alloc(size_t n_rows, size_t n_cols, size_t n_entries)
     {
         const size_t alloc_sz = n_rows * n_cols * n_entries * sizeof(T);
-        backing_buf = reinterpret_cast<T*>(std::aligned_alloc(64, alloc_sz));
-        std::memset(reinterpret_cast<uint8_t*>(backing_buf), 0, alloc_sz);
-        is_allocated = true;
+        this->backing_buf_ = reinterpret_cast<T*>(Agora_memory::padded_aligned_alloc(Agora_memory::Alignment_t::k64Align, alloc_sz));
+        std::memset(reinterpret_cast<uint8_t*>(this->backing_buf_), 0, alloc_sz);
 
         // Fill-in the grid with pointers into backing_buf
         size_t offset = 0;
         for (size_t i = 0; i < n_rows; i++) {
             for (size_t j = 0; j < n_cols; j++) {
-                mat[i][j] = &backing_buf[offset];
+                mat_[i][j] = &this->backing_buf_[offset];
                 offset += n_entries;
             }
         }
@@ -149,7 +159,7 @@ public:
         std::default_random_engine generator;
         std::uniform_real_distribution<float> distribution(-1.0, 1.0);
 
-        for (auto& row : mat) {
+        for (auto& row : mat_) {
             for (auto& entry : row) {
                 auto* base = reinterpret_cast<float*>(entry);
                 for (size_t i = 0; i < n_entries * 2; i++) {
@@ -159,32 +169,29 @@ public:
         }
     }
 
-    std::array<T*, COLS>& operator[](size_t row_idx) { return mat[row_idx]; }
+    std::array<T*, COLS>& operator[](size_t row_idx) { return this->mat_[row_idx]; }
 
     // Delete copy constructor and copy assignment
     PtrGrid(PtrGrid const&) = delete;
     PtrGrid& operator=(PtrGrid const&) = delete;
 
 private:
-    std::array<std::array<T*, COLS>, ROWS> mat; /// The pointer cells
+    std::array<std::array<T*, COLS>, ROWS> mat_; /// The pointer cells
 
     /// The backing buffer for the per-cell arrays. Having a common buffer
     /// reduces the number of memory allocations.
-    T* backing_buf;
-
-    /// True iff we've allocated the backing buffer
-    bool is_allocated = false;
+    T* backing_buf_;
 };
 
 // PtrCube is a 3D cube of pointers. Each entry of the cube is a pointer to an
 // array of [T].
 template <size_t DIM1, size_t DIM2, size_t DIM3, class T> class PtrCube {
 public:
-    PtrCube(void) {}
+    PtrCube(void): backing_buf_(nullptr) { }
 
     /// Create a cube of pointers with dimensions [DIM1, DIM2, DIM3], where each
     /// cube cell points to an array of [n_entries]
-    PtrCube(size_t n_entries) { alloc(DIM1, DIM2, DIM3, n_entries); }
+    PtrCube(size_t n_entries) { this->alloc(DIM1, DIM2, DIM3, n_entries); }
 
     /// Create a cube of pointers with dimensions [DIM1, DIM2, DIM3], where
     /// only the cube with dimensions [dim_1, dim_2, dim_3] has cells
@@ -193,13 +200,14 @@ public:
     PtrCube(size_t dim_1, size_t dim_2, size_t dim_3, size_t n_entries)
     {
         assert(dim_1 <= DIM1 && dim_2 <= DIM2 && dim_3 <= DIM3);
-        alloc(dim_1, dim_2, dim_3, n_entries);
+        this->alloc(dim_1, dim_2, dim_3, n_entries);
     }
 
     ~PtrCube(void)
     {
-        if (is_allocated) {
-            std::free(backing_buf);
+        if (this->backing_buf_ != nullptr) {
+            std::free(this->backing_buf_);
+            this->backing_buf_ = nullptr;
         }
     }
 
@@ -207,12 +215,11 @@ public:
     void alloc(size_t dim_1, size_t dim_2, size_t dim_3, size_t n_entries)
     {
         const size_t alloc_sz = dim_1 * dim_2 * dim_3 * n_entries * sizeof(T);
-        backing_buf = reinterpret_cast<T*>(std::aligned_alloc(64, alloc_sz));
-        std::memset(reinterpret_cast<uint8_t*>(backing_buf), 0, alloc_sz);
-        is_allocated = true;
+        this->backing_buf_ = reinterpret_cast<T*>(Agora_memory::padded_aligned_alloc(Agora_memory::Alignment_t::k64Align, alloc_sz));
+        std::memset(reinterpret_cast<uint8_t*>(this->backing_buf_), 0, alloc_sz);
 
         // Fill-in the grid with pointers into backing_buf
-        for (auto& mat : cube) {
+        for (auto& mat : this->cube_) {
             for (auto& row : mat) {
                 for (auto& entry : row) {
                     entry = nullptr;
@@ -224,7 +231,7 @@ public:
         for (size_t i = 0; i < dim_1; i++) {
             for (size_t j = 0; j < dim_2; j++) {
                 for (size_t k = 0; k < dim_3; k++) {
-                    cube[i][j][k] = &backing_buf[offset];
+                    this->cube_[i][j][k] = &this->backing_buf_[offset];
                     offset += n_entries;
                 }
             }
@@ -233,7 +240,7 @@ public:
 
     std::array<std::array<T*, DIM3>, DIM2>& operator[](size_t row_idx)
     {
-        return cube[row_idx];
+        return this->cube_[row_idx];
     }
 
     // Delete copy constructor and copy assignment
@@ -242,14 +249,11 @@ public:
 
 private:
     /// The pointer cells
-    std::array<std::array<std::array<T*, DIM3>, DIM2>, DIM1> cube;
+    std::array<std::array<std::array<T*, DIM3>, DIM2>, DIM1> cube_;
 
     /// The backing buffer for the per-cell arrays. Having a common buffer
     /// reduces the number of memory allocations.
-    T* backing_buf;
-
-    /// True iff we've allocated the backing buffer
-    bool is_allocated = false;
+    T* backing_buf_;
 };
 
 #endif
