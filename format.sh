a96895b9a1d89aed18592b7f789568e57cbb05aa
Andrew Sedlmayr <Andrew.Sedlmayr@rice.edu>
2021-01-19 18:45:17 -0600

    Clang format baseline

---
+++ format.sh
@@ -0,0 +1,40 @@
+
+clang-format-11 -i src/agora/txrx/*.cpp src/agora/txrx/*.h src/agora/txrx/*.hpp
+clang-format-11 -i src/agora/txrx/*.h
+clang-format-11 -i src/agora/txrx/*.hpp
+
+clang-format-11 -i src/agora/*.cpp
+clang-format-11 -i src/agora/*.h
+clang-format-11 -i src/agora/*.hpp
+
+clang-format-11 -i src/client/*.cpp
+clang-format-11 -i src/client/*.h
+clang-format-11 -i src/client/*.hpp
+
+clang-format-11 -i src/common/*.cpp
+clang-format-11 -i src/common/*.h
+clang-format-11 -i src/common/*.hpp
+
+clang-format-11 -i src/encoder/*.cpp
+clang-format-11 -i src/encoder/*.h
+clang-format-11 -i src/encoder/*.hpp
+
+clang-format-11 -i src/mac/*.cpp
+clang-format-11 -i src/mac/*.hpp
+
+clang-format-11 -i simulator/*.cpp
+clang-format-11 -i simulator/*.h
+clang-format-11 -i simulator/*.hpp
+
+clang-format-11 -i data/data_generator/*.cpp
+clang-format-11 -i data/data_generator/*.h
+
+clang-format-11 -i test/test_agora/*.cpp
+clang-format-11 -i test/unit_tests/*.cc
+clang-format-11 -i test/compute_kernels/*.cpp
+clang-format-11 -i test/compute_kernels/*.c
+clang-format-11 -i test/compute_kernels/*.hpp
+clang-format-11 -i test/compute_kernels/*.cpp
+clang-format-11 -i test/compute_kernels/ldpc/*.cpp
+
+
