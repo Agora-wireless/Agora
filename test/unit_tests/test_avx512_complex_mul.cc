3ad2e6de920065f97f884e84dfc86ed9a7a2935b
Daniel DeGrasse <daniel@degrasse.com>
2021-02-25 14:29:33 -0600

    Switched to rdtsc as timer
    Former-commit-id: 589f3369d2b6e6770d48286319adde8f8037f688

--- test/unit_tests/test_avx512_complex_mul.cc
+++ test/unit_tests/test_avx512_complex_mul.cc
@@ -1,128 +1,118 @@
 #include <gtest/gtest.h>
-#include <time.h>
-#include "comms-lib.h"
 
+#include "comms-lib.h"
+#include "gettime.h"
 
 #ifdef __AVX512F__
-#define NUM_RUNS 1000000 // number of runs, for benchmarking
+#define NUM_RUNS 1000000  // number of runs, for benchmarking
 
-
-TEST(TestComplexMul, Multiply) 
-{
-    float values[32] __attribute((aligned(64)));
-    float out256[16] __attribute((aligned(64)));
-    float out512[16] __attribute((aligned(64)));
-    __m256 result_256_lower __attribute((aligned(64)));
-    __m256 result_256_upper __attribute((aligned(64)));
-    __m512 result_512 __attribute((aligned(64)));
-    clock_t time;
-    for (int i = 0; i < 32; i++) {
-        // Set each float to a random value between -1 and 1
-        values[i] 
-            = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-    }
-    /* Load the generated values */
-    __m256 values0_lower_256 
-        = _mm256_load_ps(reinterpret_cast<const float*>(values));
-    __m256 values0_upper_256 
-        = _mm256_load_ps(reinterpret_cast<const float*>(values + 8));
-    __m256 values1_lower_256
-        = _mm256_load_ps(reinterpret_cast<const float*>(values + 16));
-    __m256 values1_upper_256
-        = _mm256_load_ps(reinterpret_cast<const float*>(values + 24));
-    __m512 values0_512
-        = _mm512_load_ps(reinterpret_cast<const float*>(values));
-    __m512 values1_512
-        = _mm512_load_ps(reinterpret_cast<const float*>(values + 16));
-    /* Do the multiplication */
-    time = clock();
-    for (int i = 0; i < NUM_RUNS; i++) {
-        result_256_lower = CommsLib::M256ComplexCf32Mult(
-            values0_lower_256, values1_lower_256, false);
-        result_256_upper = CommsLib::M256ComplexCf32Mult(
-            values0_upper_256, values1_upper_256, false);
-    }
-    time = clock() - time;
-    std::cout << "AVX256 Multiplication took " << time << "\n";
-    time = clock();
-    for (int i = 0; i < NUM_RUNS; i++) {
-        result_512 
-            = CommsLib::M512ComplexCf32Mult(values0_512, values1_512, false);
-    }
-    time = clock() - time;
-    std::cout << "AVX512 Multiplication took " << time << "\n";
-    /* Extract the results into output buffers */
-    _mm256_stream_ps(reinterpret_cast<float*>(out256), result_256_lower);
-    _mm256_stream_ps(reinterpret_cast<float*>(out256 + 8), result_256_upper);
-    _mm512_stream_ps(reinterpret_cast<float*>(out512), result_512);
-    ASSERT_EQ(memcmp(out512, out256, sizeof(out512)), 0) 
-        << "AVX512 and AVX256 multiplication differ";
+TEST(TestComplexMul, Multiply) {
+  float values[32] __attribute((aligned(64)));
+  float out256[16] __attribute((aligned(64)));
+  float out512[16] __attribute((aligned(64)));
+  __m256 result_256_lower __attribute((aligned(64)));
+  __m256 result_256_upper __attribute((aligned(64)));
+  __m512 result_512 __attribute((aligned(64)));
+  uint64_t ticks;
+  for (int i = 0; i < 32; i++) {
+    // Set each float to a random value between -1 and 1
+    values[i] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+  }
+  /* Load the generated values */
+  __m256 values0_lower_256 =
+      _mm256_load_ps(reinterpret_cast<const float*>(values));
+  __m256 values0_upper_256 =
+      _mm256_load_ps(reinterpret_cast<const float*>(values + 8));
+  __m256 values1_lower_256 =
+      _mm256_load_ps(reinterpret_cast<const float*>(values + 16));
+  __m256 values1_upper_256 =
+      _mm256_load_ps(reinterpret_cast<const float*>(values + 24));
+  __m512 values0_512 = _mm512_load_ps(reinterpret_cast<const float*>(values));
+  __m512 values1_512 =
+      _mm512_load_ps(reinterpret_cast<const float*>(values + 16));
+  /* Do the multiplication */
+  ticks = Rdtsc();
+  for (int i = 0; i < NUM_RUNS; i++) {
+    result_256_lower = CommsLib::M256ComplexCf32Mult(values0_lower_256,
+                                                     values1_lower_256, false);
+    result_256_upper = CommsLib::M256ComplexCf32Mult(values0_upper_256,
+                                                     values1_upper_256, false);
+  }
+  ticks = Rdtsc() - ticks;
+  std::cout << "AVX256 Multiplication took " << ticks << "\n";
+  ticks = Rdtsc();
+  for (int i = 0; i < NUM_RUNS; i++) {
+    result_512 = CommsLib::M512ComplexCf32Mult(values0_512, values1_512, false);
+  }
+  ticks = Rdtsc()- ticks;
+  std::cout << "AVX512 Multiplication took " << ticks << "\n";
+  /* Extract the results into output buffers */
+  _mm256_stream_ps(reinterpret_cast<float*>(out256), result_256_lower);
+  _mm256_stream_ps(reinterpret_cast<float*>(out256 + 8), result_256_upper);
+  _mm512_stream_ps(reinterpret_cast<float*>(out512), result_512);
+  ASSERT_EQ(memcmp(out512, out256, sizeof(out512)), 0)
+      << "AVX512 and AVX256 multiplication differ";
 }
 
-TEST(TestComplexMul, ConjMultiply)
-{
-    float values[32] __attribute((aligned(64)));
-    float out256[16] __attribute((aligned(64)));
-    float out512[16] __attribute((aligned(64)));
-    __m256 result_256_lower __attribute((aligned(64)));
-    __m256 result_256_upper __attribute((aligned(64)));
-    __m512 result_512 __attribute((aligned(64)));
-    clock_t time;
-    for (int i = 0; i < 32; i++) {
-        // Set each float to a random value between -1 and 1
-        values[i] 
-            = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-    }
-    /* Load the generated values */
-    __m256 values0_lower_256 
-        = _mm256_load_ps(reinterpret_cast<const float*>(values));
-    __m256 values0_upper_256 
-        = _mm256_load_ps(reinterpret_cast<const float*>(values + 8));
-    __m256 values1_lower_256
-        = _mm256_load_ps(reinterpret_cast<const float*>(values + 16));
-    __m256 values1_upper_256
-        = _mm256_load_ps(reinterpret_cast<const float*>(values + 24));
-    __m512 values0_512
-        = _mm512_load_ps(reinterpret_cast<const float*>(values));
-    __m512 values1_512
-        = _mm512_load_ps(reinterpret_cast<const float*>(values + 16));
-    /* Do the multiplication */
-    time = clock();
-    for (int i = 0; i < NUM_RUNS; i++) {
-        result_256_lower = CommsLib::M256ComplexCf32Mult(
-            values0_lower_256, values1_lower_256, true);
-        result_256_upper = CommsLib::M256ComplexCf32Mult(
-            values0_upper_256, values1_upper_256, true);
-    }
-    time = clock() - time;
-    std::cout << "AVX256 Conj Multiplication took " << time << "\n";
-    time = clock();
-    for (int i = 0; i < NUM_RUNS; i++) {
-        result_512 
-            = CommsLib::M512ComplexCf32Mult(values0_512, values1_512, true);
-    }
-    time = clock() - time;
-    std::cout << "AVX512 Conj Multiplication took " << time << "\n";
-    /* Extract the results into output buffers */
-    _mm256_stream_ps(reinterpret_cast<float*>(out256), result_256_lower);
-    _mm256_stream_ps(reinterpret_cast<float*>(out256 + 8), result_256_upper);
-    _mm512_stream_ps(reinterpret_cast<float*>(out512), result_512);
-    ASSERT_EQ(memcmp(out512, out256, sizeof(out512)), 0) 
-        << "AVX512 and AVX256 conjugate multiplication differ";
+TEST(TestComplexMul, ConjMultiply) {
+  float values[32] __attribute((aligned(64)));
+  float out256[16] __attribute((aligned(64)));
+  float out512[16] __attribute((aligned(64)));
+  __m256 result_256_lower __attribute((aligned(64)));
+  __m256 result_256_upper __attribute((aligned(64)));
+  __m512 result_512 __attribute((aligned(64)));
+  uint64_t ticks;
+  for (int i = 0; i < 32; i++) {
+    // Set each float to a random value between -1 and 1
+    values[i] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+  }
+  /* Load the generated values */
+  __m256 values0_lower_256 =
+      _mm256_load_ps(reinterpret_cast<const float*>(values));
+  __m256 values0_upper_256 =
+      _mm256_load_ps(reinterpret_cast<const float*>(values + 8));
+  __m256 values1_lower_256 =
+      _mm256_load_ps(reinterpret_cast<const float*>(values + 16));
+  __m256 values1_upper_256 =
+      _mm256_load_ps(reinterpret_cast<const float*>(values + 24));
+  __m512 values0_512 = _mm512_load_ps(reinterpret_cast<const float*>(values));
+  __m512 values1_512 =
+      _mm512_load_ps(reinterpret_cast<const float*>(values + 16));
+  /* Do the multiplication */
+  ticks = Rdtsc();
+  for (int i = 0; i < NUM_RUNS; i++) {
+    result_256_lower = CommsLib::M256ComplexCf32Mult(values0_lower_256,
+                                                     values1_lower_256, true);
+    result_256_upper = CommsLib::M256ComplexCf32Mult(values0_upper_256,
+                                                     values1_upper_256, true);
+  }
+  ticks = Rdtsc() - ticks;
+  std::cout << "AVX256 Conj Multiplication took " << ticks << "\n";
+  ticks = Rdtsc();
+  for (int i = 0; i < NUM_RUNS; i++) {
+    result_512 = CommsLib::M512ComplexCf32Mult(values0_512, values1_512, true);
+  }
+  ticks = Rdtsc() - ticks;
+  std::cout << "AVX512 Conj Multiplication took " << ticks << "\n";
+  /* Extract the results into output buffers */
+  _mm256_stream_ps(reinterpret_cast<float*>(out256), result_256_lower);
+  _mm256_stream_ps(reinterpret_cast<float*>(out256 + 8), result_256_upper);
+  _mm512_stream_ps(reinterpret_cast<float*>(out512), result_512);
+  ASSERT_EQ(memcmp(out512, out256, sizeof(out512)), 0)
+      << "AVX512 and AVX256 conjugate multiplication differ";
 }
 
 #endif
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 #ifdef __AVX512F__
-    testing::InitGoogleTest(&argc, argv);
-    std::cout << "---- CommsLib AVX512 Channel Estimation ----\n";
-    return RUN_ALL_TESTS();
-    #else
-    (void)argc;
-    (void)argv;
-    std::cout << "Platform does not support AVX512!\n";
-    return 0;
-#endif 
+  testing::InitGoogleTest(&argc, argv);
+  std::cout << "---- CommsLib AVX512 Channel Estimation ----\n";
+  return RUN_ALL_TESTS();
+#else
+  (void)argc;
+  (void)argv;
+  std::cout << "Platform does not support AVX512!\n";
+  return 0;
+#endif
 }
