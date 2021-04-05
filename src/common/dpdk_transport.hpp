38e7a8e78e30b17bb11b6c9ae9169cd44252f01a
Gavindeed <jug257@g.harvard.edu>
2021-04-05 14:03:47 -0700

    Dpdk debug
    Former-commit-id: b74d7ba00f7077da1867fd97178effa6d3649c1c

--- src/common/dpdk_transport.hpp
+++ src/common/dpdk_transport.hpp
@@ -17,8 +17,8 @@
 #include <string>
 #include <unistd.h>
 
-#define RX_RING_SIZE 8192
-#define TX_RING_SIZE 8192
+#define RX_RING_SIZE 4096
+#define TX_RING_SIZE 4096
 
 #define NUM_MBUFS ((64 * 1024) - 1)
 #define MBUF_CACHE_SIZE 128
