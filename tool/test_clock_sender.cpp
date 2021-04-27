9837e5fdac3342f608eedadcd887d52cd3ffcfbe
Gavindeed <jug257@g.harvard.edu>
2021-04-27 16:59:49 -0700

    update test clock sender
    Former-commit-id: 1136648ea89ea6ecc54cf4dc897b9389420981b6

--- tool/test_clock_sender.cpp
+++ tool/test_clock_sender.cpp
@@ -2,6 +2,8 @@
 #include <netinet/ether.h>
 #include <arpa/inet.h>
 #include <stdexcept>
+#include "gettime.h"
+#include <sys/time.h>
 
 static inline void rt_assert(bool condition, const char* throw_str) {
   if (!condition) throw std::runtime_error(throw_str);
@@ -9,6 +11,8 @@ static inline void rt_assert(bool condition, const char* throw_str) {
 
 int main(int argc, char **argv) 
 {
+    double freq_ghz = measure_rdtsc_freq();
+
     rte_mempool *mbuf_pool;
     DpdkTransport::dpdk_init(0, 1);
     mbuf_pool = DpdkTransport::create_mempool();
@@ -41,7 +45,35 @@ int main(int argc, char **argv)
     auto* payload = reinterpret_cast<char*>(eth_hdr) + kPayloadOffset;
     *((uint32_t*)payload) = 4244;
     
+    struct timeval start_time;
+    gettimeofday(&start_time, NULL);
     int sent = rte_eth_tx_burst(0, 0, &mbuf, 1);
+
+    rte_mbuf *mbufs[32];
+    
+    while (true) {
+        int recv = rte_eth_rx_burst(0, 0, mbufs, 32);
+        if (recv > 0) {
+            for (size_t i = 0; i < recv; i ++) {
+                auto* eth_hdr = rte_pktmbuf_mtod(mbufs[i], rte_ether_hdr*);
+                auto* ip_hdr = reinterpret_cast<rte_ipv4_hdr*>(
+                    reinterpret_cast<uint8_t*>(eth_hdr) + sizeof(rte_ether_hdr));
+                auto* udp_hdr = reinterpret_cast<rte_udp_hdr*>(
+                    reinterpret_cast<uint8_t*>(ip_hdr) + sizeof(rte_ipv4_hdr));
+                auto* payload = reinterpret_cast<char*>(eth_hdr) + kPayloadOffset;
+                if (*((uint32_t*)payload) == 4244) {
+                    struct timeval end_time;
+                    gettimeofday(&end_time, NULL);
+                    uint64_t start_tc = start_time.tv_sec * 1000000000L + start_time.tv_usec;
+                    uint64_t end_tc = end_time.tv_sec * 1000000000L + end_time.tv_usec;
+                    int64_t latency = (int64_t)end_tc - (int64_t)start_tc;
+                    int64_t middle = *((int64_t*)(payload + 4)) - (int64_t)start_tc;
+                    printf("Received timer %ld %ld %ld!\n", latency, middle, middle-latency/2);
+                    return 0;
+                }
+            }
+        }
+    }
 
     return 0;
 }
