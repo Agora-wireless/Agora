db65c287378907bb1294acc4f21e53af7d890e66
Naman Arora <naman.arora@rice.edu>
2021-06-02 16:18:42 -0500

    Add a new pre-push hook
        Default hook is still post-checkout. The pre-push hooks makes
        changes to the README before the branch is actually pushed to a
        remote.
        TODO: Fix README overwriting during fast-forward merge of two
        remote tracking branches.
    
    
    Former-commit-id: 9f63a2972d4c3c87ccf1113ff7709777cf01ddaa

--- .githooks/templateReadmeCIBadgeHook.sh
+++ .githooks/templateReadmeCIBadgeHook.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-
-echo "Starting post-checkout hook ..."
+CI_BUILD_STATUS_BADGE_OLD=$(grep -i "build status" README.md)
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 CI_BUILD_STATUS_BADGE="[![Build Status](https://falcon.ecg.rice.edu:443/buildStatus/icon?job=github_public_agora%2F${BRANCH_NAME})](https://falcon.ecg.rice.edu:443/job/github_public_agora/job/${BRANCH_NAME}/)"
 echo $BRANCH_NAME
 echo $CI_BUILD_STATUS_BADGE
+
+if [ "${CI_BUILD_STATUS_BADGE_OLD}" == "${CI_BUILD_STATUS_BADGE}" ]; then
+    exit 0
+fi
 
 sed -i -e "/Build Status/ c $CI_BUILD_STATUS_BADGE" README.md
 git add README.md
--- config_ci.sh
+++ config_ci.sh
@@ -9,7 +9,29 @@ if [ $(version $GIT_VER) -lt $(version $GIT_MIN_VER) ]; then
       echo "Your Git version is ${GIT_VER}. Need ${GIT_MIN_VER} or higher."
 fi
 
-# configure git hooks path
-git config core.hooksPath .githooks
-echo configured git hooks path
+HOOKS_PATH=.git/hooks
+TEMPLATE_DIR=.githooks
+HOOK_NAME=""
+
+# Purge old hooks
+echo -e 'Purging old pre-push/post-checkout hooks... Continue? [y/N] \c'
+read consent
+if [ -z "$consent" ] || [ "$consent" == 'n' ]; then
+      echo 'Exiting...'
+      exit 0
+fi
+rm -f ${HOOKS_PATH}/{pre-push,post-checkout}
+
+# Install new hook 
+if [ "$#" == 0 ] || [ "$1" == "post-checkout" ]; then
+      cp ${TEMPLATE_DIR}/templateReadmeCIBadgeHook.sh ${HOOKS_PATH}/post-checkout
+      HOOK_NAME="post-checkout"
+elif [ "$1" == "pre-push" ]; then
+      cp ${TEMPLATE_DIR}/templateReadmeCIBadgeHook.sh ${HOOKS_PATH}/pre-push
+      HOOK_NAME="pre-push"
+fi
+
+sed -i -e "1a echo 'Starting ${HOOK_NAME} hook ...'" ${HOOKS_PATH}/${HOOK_NAME}
+
+echo "configured git ${HOOK_NAME} hook."
 
