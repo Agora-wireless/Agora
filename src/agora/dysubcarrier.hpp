5d94612a51c3c625d4cf40c4e62b5216d12782f9
Junzhi Gong <jug257@g.harvard.edu>
2021-07-28 20:25:59 -0700

    Add more details in time measurement
    Former-commit-id: cc1c5ba64506b7ded464ec71222943f5142d7373

--- src/agora/dysubcarrier.hpp
+++ src/agora/dysubcarrier.hpp
@@ -380,12 +380,12 @@ public:
         size_t whole_duration = rdtsc() - start_tsc;
         size_t idle_duration = whole_duration - work_tsc_duration;
         printf("DySubcarrier Thread %u duration stats: total time used %.2lfms, "
-            "csi %.2lfms (%.2lf\%, %u), zf %.2lfms (%.2lf\%, %u), demod %.2lfms (%.2lf\%, %u, %.2lfus), "
+            "csi %.2lfms (%.2lf\%, %u, %.2lfus), zf %.2lfms (%.2lf\%, %u, %.2lfus), demod %.2lfms (%.2lf\%, %u, %.2lfus), "
             "precode %.2lfms (%.2lf\%), print %.2lfms (%.2lf\%), stating "
             "%.2lfms (%.2lf\%), idle %.2lfms (%.2lf\%), working rate (%u/%u: %.2lf\%)\n", 
             tid, cycles_to_ms(whole_duration, freq_ghz),
-            cycles_to_ms(csi_tsc_duration, freq_ghz), csi_tsc_duration * 100.0f / whole_duration, csi_count,
-            cycles_to_ms(zf_tsc_duration, freq_ghz), zf_tsc_duration * 100.0f / whole_duration, zf_count,
+            cycles_to_ms(csi_tsc_duration, freq_ghz), csi_tsc_duration * 100.0f / whole_duration, csi_count, cycles_to_us(csi_max, freq_ghz),
+            cycles_to_ms(zf_tsc_duration, freq_ghz), zf_tsc_duration * 100.0f / whole_duration, zf_count, cycles_to_us(zf_max, freq_ghz),
             cycles_to_ms(demod_tsc_duration, freq_ghz), demod_tsc_duration * 100.0f / whole_duration, demod_count, cycles_to_us(demod_max, freq_ghz),
             cycles_to_ms(precode_tsc_duration, freq_ghz), precode_tsc_duration * 100.0f / whole_duration,
             cycles_to_ms(print_tsc_duration, freq_ghz), print_tsc_duration * 100.0f / whole_duration,
@@ -397,12 +397,14 @@ public:
         std::string cur_directory = TOSTRING(PROJECT_DIRECTORY);
         std::string filename = cur_directory + "/data/performance_dysubcarrier.txt";
         FILE* fp = fopen(filename.c_str(), "a");
-        fprintf(fp, "%u %u %u %u %u %.2lf %.2lf %.2lf %.2lf %.2lf %.2lf %.2lf\n", cfg->BS_ANT_NUM, cfg->UE_NUM,
+        fprintf(fp, "%u %u %u %u %u %.2lf %.2lf %.2lf %.2lf %.2lf %.2lf %.2lf %.2lf %u %u %u %.2lf %.2lf %.2lf\n", cfg->BS_ANT_NUM, cfg->UE_NUM,
             sc_range_.end - sc_range_.start, cfg->demul_block_size, cfg->mod_order_bits,
             csi_tsc_duration * 100.0f / whole_duration, zf_tsc_duration * 100.0f / whole_duration,
             demod_tsc_duration * 100.0f / whole_duration, precode_tsc_duration * 100.0f / whole_duration,
             print_tsc_duration * 100.0f / whole_duration, state_operation_duration * 100.0f / whole_duration,
-            idle_duration * 100.0f / whole_duration);
+            idle_duration * 100.0f / whole_duration, cycles_to_ms(whole_duration, freq_ghz),
+            csi_count, zf_count, demod_count, 
+            cycles_to_us(csi_max, freq_ghz), cycles_to_us(zf_max, freq_ghz), cycles_to_us(demod_max, freq_ghz));
         fclose(fp);
     }
 
