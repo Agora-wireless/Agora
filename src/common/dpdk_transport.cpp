514a27106f8bdb837ca2c792d123bffc46a14b64
Gavindeed <jug257@g.harvard.edu>
2021-07-28 19:56:27 -0700

    Make dpdk message printed
    Former-commit-id: 8687f43cb8ea9c2485b4fd98007ab1c56cf89c33

--- src/common/dpdk_transport.cpp
+++ src/common/dpdk_transport.cpp
@@ -370,7 +370,7 @@ void DpdkTransport::dpdk_init(uint16_t core_offset, size_t thread_num, std::stri
         + std::to_string(core_offset + thread_num);
     // n: channels, m: maximum memory in megabytes
     const char* rte_argv[]
-        = { "txrx", "-l", core_list.c_str(), "--log-level", "0", "-w", pci_addr.c_str(), nullptr };
+        = { "txrx", "-l", core_list.c_str(), "--log-level", "7", "-w", pci_addr.c_str(), nullptr };
     int rte_argc = static_cast<int>(sizeof(rte_argv) / sizeof(rte_argv[0])) - 1;
 
     // Initialize DPDK environment
