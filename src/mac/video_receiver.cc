905871978b6620a475bc689350fae41a3a4a90a5
Andrew Sedlmayr <andrew.sedlmayr@rice.edu>
2021-08-05 14:35:28 -0500

    looping_fix
    Former-commit-id: 1c1415e960bc5bc965d2d2f0bd6684ce47442053

--- src/mac/video_receiver.cc
+++ src/mac/video_receiver.cc
@@ -10,7 +10,7 @@
 VideoReceiver::VideoReceiver(size_t port)
     : udp_video_receiver_(port, VideoReceiver::kVideoStreamSocketRxBufSize),
       data_availble_(0),
-      data_start_offset_(0) {}
+      data_start_offset_(0) { udp_video_receiver_.MakeBlocking(1); }
 
 void VideoReceiver::Load(char *destination, size_t num_load_bytes) {
   if (num_load_bytes > data_availble_) {
@@ -22,25 +22,29 @@ void VideoReceiver::Load(char *destination, size_t num_load_bytes) {
       data_start_offset_ = 0;
     }
 
-    ssize_t rx_bytes = udp_video_receiver_.Recv(
-        &local_rx_buffer_.at(data_start_offset_ + data_availble_),
-        VideoReceiver::kVideoStreamMaxRxSize);
+    while (data_availble_ < num_load_bytes) {
+      ssize_t rcv_ret = udp_video_receiver_.Recv(
+          &local_rx_buffer_.at(data_start_offset_ + data_availble_),
+          VideoReceiver::kVideoStreamMaxRxSize);
 
-    std::printf("[VideoReceiver] data received: %zd\n", rx_bytes);
-
-    if (rx_bytes < 0) {
-      throw std::runtime_error("[VideoReceiver] Receive error");
-    } else if (static_cast<size_t>(rx_bytes) >
-               VideoReceiver::kVideoStreamMaxRxSize) {
-      throw std::runtime_error(
-          "[VideoReceiver] Received packet larger than max receive size -- "
-          "inspect");
+      if (rcv_ret < 0) {
+        throw std::runtime_error("[VideoReceiver] Receive error");
+      } else if (static_cast<size_t>(rcv_ret) >
+                 VideoReceiver::kVideoStreamMaxRxSize) {
+        throw std::runtime_error(
+            "[VideoReceiver] Received packet larger than max receive size -- "
+            "inspect");
+      }
+      else if (rcv_ret > 0){
+        std::printf("[VideoReceiver] data received: %zd\n", rcv_ret);
+      }
+      data_availble_ += rcv_ret;
     }
-    data_availble_ += rx_bytes;
   }
 
   //Copy data from local buffer to requested memory location
   memcpy(destination, &local_rx_buffer_.at(data_start_offset_), num_load_bytes);
-  std::printf("[Receive Server] Data loaded: %zu\n", num_load_bytes);
+  std::printf("[VideoReceiver] Data loaded: %zu %zu %zu\n", num_load_bytes, data_availble_, data_start_offset_);
   data_start_offset_ += num_load_bytes;
+  data_availble_ -= num_load_bytes;
 }
