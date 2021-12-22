760e78930d77b8932ff720e9874b212b3002482c
Andrew Sedlmayr <as61@localhost>
2021-12-22 08:41:55 -0700

    working uplink and downlink
    Former-commit-id: 2cde53c54d566de9ee12ecec416763e8754b8c05

--- src/common/symbols.h
+++ src/common/symbols.h
@@ -150,7 +150,7 @@ static constexpr bool kUsePartialTrans = true;
 static constexpr bool kExportConstellation = false;
 static constexpr bool kPrintPhyStats = true;
 static constexpr bool kCollectPhyStats = true;
-static constexpr bool kPrintZfStats = false;
+static constexpr bool kPrintZfStats = true;
 
 static constexpr bool kStatsPrintFrameSummary = true;
 static constexpr bool kDebugPrintPerFrameDone = true;
