4e2a06386d65de2b0c0ea671f9a26224820e2eaf
junzhig <junzhig@node-0.junzhig-114255.opensketch-pg0.utah.cloudlab.us>
2022-01-03 20:27:51 -0700

    Update server init script
    Former-commit-id: 3d330f08adc3c01aba4d6c00a41471baaead5f3e

--- scripts/server_init.sh
+++ scripts/server_init.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install byobu
+sudo apt-get install -y byobu
 cd ~
 mkdir project
-sudo mkfs -t ext3 /dev/nvme0n1p4
+sudo mkfs -t ext3 /dev/nvme0n1p4 <<< y
 sudo mount -t auto /dev/nvme0n1p4 ./project
 sudo chown junzhig ./project
 sudo chgrp opensketch-PG0 ./project
