19c92d53b39d5cc57afec96996b7442e4613a85c
Di Mu <di.mu@rice.edu>
2022-01-29 21:09:11 -0600

    update data extraction and plotting

--- log2data-ue.sh
+++ log2data-ue.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-  echo "usage: $0 <input_log_file> <output_data_file>"
+if [ $# -ne 1 ]; then
+  echo 'usage: $0 <input_log_file>'
   exit
 fi
-echo "Frame,Symbol,User,EVM,SNR" > $2
-grep 'Frame.*Symbol.*User.*EVM.*SNR.*' $1 | awk '{print $2 $4 $6 $8 $10}' | sed 's/%//g' >> $2
+echo 'Frame,User,EVM,SNR' > uedata-evmsnr.csv
+grep 'Frame.*Symbol.*User.*EVM.*SNR.*' $1 | awk '{print $2 $6 $8 $10}' \
+| sed 's/%//g' >> uedata-evmsnr.csv
+echo 'Frame,User,SymbolErrors' > uedata-se.csv
+grep 'Frame.*Symbol.*Ue.*symbol errors' $1 | awk '{print $2 "," $6 "," $7}' \
+| sed 's/://g' >> uedata-se.csv
+echo 'Frame,AntId,RFSNR' > uedata-rfsnr.csv
+grep 'UeWorker: Fft Pilot(frame.*symbol 10 ant.*) sig offset.*SNR.*' $1 \
+| awk '{print $4 "," $8 "," $13}' | sed 's/)//g' >> uedata-rfsnr.csv
+python3 plot-ue.py
--- plot-ue.py
+++ plot-ue.py
@@ -1,13 +1,38 @@
 import sys
 import pandas as pd
 from matplotlib import pyplot as plt
-if len(sys.argv) != 2:
-  print('usage: python3', str(sys.argv[0]), '<data_file>')
-  exit()
-columns = ["Frame", "EVM", "SNR"]
-data = pd.read_csv(sys.argv[1], usecols=columns)
-plt.subplot(2, 1, 1)
-plt.plot(data.Frame, data.SNR)
-plt.subplot(2, 1, 2)
-plt.plot(data.Frame, data.EVM)
-plt.show()
+#if len(sys.argv) != 2:
+#  print('usage: python3', str(sys.argv[0]), '<data_file>')
+#  exit()
+columns_evmsnr = ["Frame", "User", "SNR"]
+columns_se = ["Frame", "User", "SymbolErrors"]
+columns_rfsnr = ["Frame", "AntId", "RFSNR"]
+df_evmsnr = pd.read_csv("uedata-evmsnr.csv", usecols=columns_evmsnr)
+df_se = pd.read_csv("uedata-se.csv", usecols=columns_se)
+df_rfsnr = pd.read_csv("uedata-rfsnr.csv", usecols=columns_rfsnr)
+num_users = 1
+range_frame = [0,698000]
+for i in range(num_users):
+	fig, axs = plt.subplots(3, 1, constrained_layout=True)
+	fig.suptitle('Single User Downlink')
+	df_select = df_evmsnr[df_evmsnr.User == i]
+	axs[0].plot(df_select.Frame, df_select.SNR)
+	axs[0].set_xlim(range_frame)
+	axs[0].set_ylim([-30,30])
+	axs[0].set_xlabel('Frame')
+	axs[0].set_ylabel('EVM-SNR (dB)')
+	
+	df_select = df_rfsnr[df_rfsnr.AntId == i]
+	axs[1].plot(df_select.Frame, df_select.RFSNR)
+	axs[1].set_xlim(range_frame)
+	axs[1].set_ylim([0,20])
+	axs[1].set_xlabel('Frame')
+	axs[1].set_ylabel('RF-SNR (dB)')
+	
+	df_select = df_se[df_se.User == i]
+	axs[2].plot(df_select.Frame, df_select.SymbolErrors)
+	axs[2].set_xlim(range_frame)
+	axs[2].set_ylim([0,40])
+	axs[2].set_xlabel('Frame')
+	axs[2].set_ylabel('Number of Symbol Errors')
+	plt.show()
