8d671baf2eede6fdd93ff9e8d47adb22a9ecb70f
Di Mu <di.mu@rice.edu>
2022-02-09 13:22:12 -0600

    Add range selection, BER, and SER; Support multiple log files
    Former-commit-id: 694e2975b5e50901a33a3e2cc98f5725c7fb3706

--- plot-ue.py
+++ plot-ue.py
@@ -1,44 +1,64 @@
 import sys
+import re
 import pandas as pd
 import numpy as np
 import statistics as sts
 from matplotlib import pyplot as plt
+import dataconf as dc
 
-columns = [['Frame', 'User', 'EVM-SNR'],
-	['Frame', 'User', 'RF-SNR'],
-	['Frame', 'User', 'Symbol_Errors']]
-filenames = ['uedata-evmsnr.csv', 'uedata-rfsnr.csv', 'uedata-se.csv']
-figtitle = 'Single User Downlink'
+def array2float(arr):
+	for i in range(len(arr)):
+		if isinstance(arr[i], str):
+			arr[i] = float(re.sub(r"([A-Z]+)|([a-z]+)", '', arr[i]))
+	return arr
+
+if len(sys.argv) != 2:
+	print('usage: python3 ' + sys.argv[0] + ' <number of devices>')
+	exit()
+
+num_dev = int(sys.argv[1])
+num_file = len(dc.files)
+xlabel = dc.cols[0][0]
+ylabels = [dc.cols[0][-1], dc.cols[1][-1], dc.cols[2][-1]]
+
 dfs = []
-for i in range(len(columns)):
-	dfs.append(pd.read_csv(filenames[i], usecols=columns[i]))
-users = np.max(dfs[0].User) + 1
-xlabel = 'Frame'
-ylabels = [columns[0][-1], columns[1][-1], columns[2][-1]]
-ylims = [[-30,30], [0,20], [0,80]]
-for i in range(users):
-	fig, axs = plt.subplots(len(dfs), 1, constrained_layout=True)
-	fig.suptitle(figtitle)
-	for j in range(len(dfs)):
-		df_select = dfs[j][dfs[j].User == i]
-		if j == 0:
-			xlim = [0, np.max(df_select[xlabel])]
-		axs[j].plot(df_select[xlabel], df_select[ylabels[j]])
-		axs[j].set_xlim(xlim)
-		axs[j].set_ylim(ylims[j])
-		axs[j].set_xlabel(xlabel)
-		axs[j].set_ylabel(ylabels[j])
+for i in range(num_file):
+	for j in range(num_dev):
+		dfs.append(pd.read_csv(dc.files[i] + '-' + str(j) + '.csv', usecols=dc.cols[i]))
+
+if dc.enable_trace_plot:
+	fig, axs = plt.subplots(num_file, 1, constrained_layout=True)
+	fig.suptitle(dc.title + ' Traces')
+	for i in range(num_file):
+		for j in range(num_dev):
+			idx = i * num_dev + j
+			axs[i].plot(dfs[idx][xlabel], array2float(dfs[idx][ylabels[i]].copy()))
+		axs[i].set_xlim(dc.xlims[0])
+		if bool(dc.ylims[i]):
+			axs[i].set_ylim(dc.ylims[i])
+		axs[i].set_xlabel(xlabel)
+		axs[i].set_ylabel(ylabels[i])
 	plt.show()
-	
-	fig, axs = plt.subplots(len(dfs), 1, constrained_layout=True)
-	fig.suptitle(figtitle + ' Statistics')
-	for j in range(len(dfs)):
-		data_select = dfs[j][dfs[j].User == i][ylabels[j]]
-		data_select = data_select[~(np.isnan(data_select) | np.isinf(data_select))]
-		axs[j].plot(np.sort(data_select), 
-			1. * np.arange(len(data_select)) / (len(data_select) - 1))
-		axs[j].set_xlabel(ylabels[j])
-		axs[j].set_ylabel('CDF')
-		print(ylabels[j] + ' Mean = ', sts.mean(data_select))
-		print(ylabels[j] + ' Median = ', sts.median(data_select))
+
+if dc.enable_stats_plot:
+	fig, axs = plt.subplots(num_file, 1, constrained_layout=True)
+	fig.suptitle(dc.title + ' Statistics')
+for i in range(num_file):
+	for j in range(num_dev):
+		idx = i * num_dev + j
+		data_select = dfs[idx][ylabels[i]] \
+				[(dc.xlims[j][0] <= dfs[idx][xlabel]) & (dfs[idx][xlabel] <= dc.xlims[j][1])]
+		array2float(data_select)
+		if dc.enable_stats_plot:
+			axs[i].plot(np.sort(data_select), \
+					1. * np.arange(len(data_select)) / (len(data_select) - 1))
+		print(ylabels[i] + ': Mean[' + str(j) + '] = % f' % sts.mean(data_select) \
+				+ ', Median[' + str(j) + '] = %f' % sts.median(data_select))
+		if ylabels[i] == 'Symbol-Errors':
+			print('SER[' + str(j) + '] = %f' % \
+					(sts.mean(data_select) / dc.qam_symbols_per_frame))
+	if dc.enable_stats_plot:
+		axs[i].set_xlabel(ylabels[i])
+		axs[i].set_ylabel('CDF')
+if dc.enable_stats_plot:
 	plt.show()
