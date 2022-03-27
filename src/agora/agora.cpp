5d507953b3fce2f00f175562a7eb6ed3c421e4a3
junzhig <junzhig@node0.junzhig-121766.opensketch-pg0.utah.cloudlab.us>
2022-03-26 18:46:03 -0600

    Isolate demod tx thread
    Former-commit-id: a75417545cab0da55f4b05a7f037d6d30b0c84df

--- src/agora/agora.cpp
+++ src/agora/agora.cpp
@@ -364,8 +364,10 @@ void* Agora::fftWorker(int tid)
 void* Agora::subcarrierWorker(int tid)
 {
     if (config_->use_hyperthreading) {
-        pin_to_core_with_offset(ThreadType::kWorkerSubcarrier, base_worker_core_offset_ - kNumDemodTxThread, 
-            tid + do_fft_threads_.size() + kNumDemodTxThread, true, true, config_->phy_core_num);
+        // pin_to_core_with_offset(ThreadType::kWorkerSubcarrier, base_worker_core_offset_ - kNumDemodTxThread, 
+        //     tid + do_fft_threads_.size() + kNumDemodTxThread, true, true, config_->phy_core_num);
+        pin_to_core_with_offset(ThreadType::kWorkerSubcarrier, base_worker_core_offset_, 
+            tid + do_fft_threads_.size(), true, true, config_->phy_core_num);
     } else {
         pin_to_core_with_offset(
             ThreadType::kWorkerSubcarrier, base_worker_core_offset_, tid + do_fft_threads_.size());
@@ -400,8 +402,11 @@ void* Agora::subcarrierWorker(int tid)
 void* Agora::decodeWorker(int tid)
 {
     if (config_->use_hyperthreading) {
-        pin_to_core_with_offset(ThreadType::kWorkerDecode, base_worker_core_offset_ - kNumDemodTxThread,
-            tid + do_fft_threads_.size() + do_subcarrier_threads_.size() + kNumDemodTxThread, 
+        // pin_to_core_with_offset(ThreadType::kWorkerDecode, base_worker_core_offset_ - kNumDemodTxThread,
+        //     tid + do_fft_threads_.size() + do_subcarrier_threads_.size() + kNumDemodTxThread, 
+        //     true, true, config_->phy_core_num);
+        pin_to_core_with_offset(ThreadType::kWorkerDecode, base_worker_core_offset_,
+            tid + do_fft_threads_.size() + do_subcarrier_threads_.size(), 
             true, true, config_->phy_core_num);
     } else {
         pin_to_core_with_offset(ThreadType::kWorkerDecode, base_worker_core_offset_,
