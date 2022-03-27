e48be8310345b711e19919af0ab604b75301ee23
junzhig <junzhig@node0.junzhig-121766.opensketch-pg0.utah.cloudlab.us>
2022-03-27 00:05:57 -0600

    Add clock diff tester
    Former-commit-id: 7bebe9a8a355ac23d1fefd4585ea94640ea04b07

---
+++ scripts/evaluation/merge_latency.py
@@ -0,0 +1,42 @@
+import sys
+
+n_servers = 3
+parent_dir = "."
+
+if len(sys.argv) == 2:
+    n_servers = int(sys.argv[1])
+elif len(sys.argv) == 3:
+    n_servers = int(sys.argv[1])
+    parent_dir = sys.argv[2]
+
+latency_data = []
+
+for i in range(n_servers):
+    f = open('{0}/frame_latency_{1}.txt'.format(parent_dir, i), 'r')
+    lines = f.readlines()
+    n_lines = len(lines)
+    for j in range(n_lines):
+        words = lines[j].split()
+        if len(latency_data) <= j:
+            latency_data.append([int(words[0]), int(words[1]), int(words[2]), int(words[3]), int(words[4]), int(words[5])])
+        else:
+            tp = latency_data[j]
+            if tp[1] > int(words[1]):
+                tp[1] = int(words[1])
+            if tp[2] < int(words[2]):
+                tp[2] = int(words[2])
+            if tp[3] < int(words[3]):
+                tp[3] = int(words[3])
+            if tp[4] > int(words[4]):
+                tp[4] = int(words[4])
+            if tp[5] < int(words[5]):
+                tp[5] = int(words[5])
+            latency_data[j] = tp
+    f.close()
+
+out_f = open('{0}/frame_latency_all.txt'.format(parent_dir), 'w')
+
+for frame in latency_data:
+    print >>out_f, frame[0], (frame[2]-frame[1])/1000.0, (frame[3]-frame[2])/1000.0, (frame[4]-frame[3])/1000.0, (frame[5]-frame[4])/1000.0, (frame[5]-frame[1])/1000.0
+
+out_f.close()
