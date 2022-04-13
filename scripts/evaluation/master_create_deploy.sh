25f7ea7beb8538123d386a5ad5bc4d3ace9cc212
junzhig <junzhig@node0.junzhig-121704.opensketch-pg0.utah.cloudlab.us>
2022-04-12 19:36:13 -0600

    Prepare evaluation for msr dl
    Former-commit-id: 8065493dbd97fcbea516d715adcff4289656b393

--- scripts/evaluation/master_create_deploy.sh
+++ scripts/evaluation/master_create_deploy.sh
@@ -8,8 +8,8 @@ hydra_root_dir=$( cd ${script_dir}/../.. >/dev/null 2>&1 && pwd )
 source ${hydra_root_dir}/scripts/utils/utils.sh
 source ${hydra_root_dir}/scripts/control/init_platform.sh
 
-ant_list=(64 64 64 128 128 150)
-ue_list=(8 16 32 16 32 32)
+ant_list=(150)
+ue_list=(32)
 
 # for i in ${!ant_list[@]}; do
 #     ant_num=${ant_list[$i]}
@@ -26,7 +26,7 @@ for i in ${!ant_list[@]}; do
     ue_num=${ue_list[$i]}
     for (( c=80; c>=20; c-=10 )) do
         python ${hydra_root_dir}/scripts/evaluation/create_deploy.py ${c} \
-            ${hydra_root_dir}/config/deploy_msr_dl/deploy_msr_${ant_num}_${ue_num}.json \
-            ${hydra_root_dir}/config/template_msr_dl/template_msr_${ant_num}_${ue_num}.json
+            ${hydra_root_dir}/config/deploy_msr/deploy_msr_${ant_num}_${ue_num}.json \
+            ${hydra_root_dir}/config/template_msr/template_msr_${ant_num}_${ue_num}.json
     done
 done
--- scripts/evaluation/master_test_min_slot.sh
+++ scripts/evaluation/master_test_min_slot.sh
@@ -37,6 +37,22 @@ for i in ${!ant_list[@]}; do
     done
 done
 
+ant_list=(150)
+ue_list=(32)
+
+for i in ${!ant_list[@]}; do
+    ant_num=${ant_list[$i]}
+    ue_num=${ue_list[$i]}
+    for (( c=80; c>=20; c-=10 )) do
+        echo "test min slot size for ant ${ant_num} ue ${ue_num} core ${c}"
+        HYDRA_SERVER_DEPLOY_JSON=config/deploy_msr/deploy_msr_${ant_num}_${ue_num}_${c}c.json
+        HYDRA_SYSTEM_CONFIG_JSON=config/template_msr/template_msr_${ant_num}_${ue_num}_${c}c.json
+        cat ${hydra_master_config_json} | jq --arg deploy "${HYDRA_SERVER_DEPLOY_JSON}" '.hydra_server_deploy_json=$deploy' > tmp.json
+        cat tmp.json | jq --arg template "${HYDRA_SYSTEM_CONFIG_JSON}" '.hydra_system_config_json=$template' > ${hydra_master_config_json}
+        bash ${hydra_root_dir}/scripts/evaluation/test_min_slot.sh ${ant_num} ${ue_num} ${c}
+    done
+done
+
 # ant_list=(150 128 128 128 64 64)
 # ue_list=(32 32 32 32 16 8)
 # core_list=(70 80 60 50 50 60)
