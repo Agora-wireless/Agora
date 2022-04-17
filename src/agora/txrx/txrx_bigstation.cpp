4194f249f3e7106d6d10c38e4a76f717d9d8a83c
junzhig <junzhig@node0.junzhig-121704.opensketch-pg0.utah.cloudlab.us>
2022-04-17 16:35:50 -0600

    Debug bigstation dl
    Former-commit-id: 8cede2eff7caadb43b1b4fb39b97b1951c73aa5c

--- src/agora/txrx/txrx_bigstation.cpp
+++ src/agora/txrx/txrx_bigstation.cpp
@@ -932,8 +932,8 @@ void* BigStationTXRX::tx_thread_dl(int tid)
                         single_convert_float32_to_float16((unsigned short*)pkt->data_ + i * 2, (unsigned int*)(src_ptr + i * 2 * sizeof(float)));
                         single_convert_float32_to_float16((unsigned short*)pkt->data_ + i * 2 + 1, (unsigned int*)(src_ptr + (i * 2 + 1) * sizeof(float)));
                     }
-                    printf("Send precode packet to %zu, frame %zu symbol %zu ant %zu\n", server_id, 
-                        precode_pkt_frame, precode_pkt_symbol_dl, precode_pkt_ant);
+                    // printf("Send precode packet to %zu, frame %zu symbol %zu ant %zu\n", server_id, 
+                    //     precode_pkt_frame, precode_pkt_symbol_dl, precode_pkt_ant);
                     size_t nb_tx_new = rte_eth_tx_burst_loop(0, tid, tx_bufs + total_buf_id, 1);
                     if (unlikely(nb_tx_new != 1)) {
                         printf("rte_eth_tx_burst() failed\n");
