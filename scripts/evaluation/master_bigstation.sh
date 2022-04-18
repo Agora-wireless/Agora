65a1a4de9f5c14d5a9589928a09348e6db595a90
junzhig <junzhig@node0.junzhig-121704.opensketch-pg0.utah.cloudlab.us>
2022-04-18 15:00:19 -0600

    Debug script
    Former-commit-id: 94b877dd1d4c5323daddb14fabf5b176391fd97c

--- scripts/evaluation/master_bigstation.sh
+++ scripts/evaluation/master_bigstation.sh
@@ -6,7 +6,7 @@ hydra_root_dir=$( cd ${script_dir}/../.. >/dev/null 2>&1 && pwd )
 source ${hydra_root_dir}/scripts/utils/utils.sh
 source ${hydra_root_dir}/scripts/control/init_platform.sh
 
-for (( i=4; i<=5; i++ )) do
+for (( i=5; i<=5; i++ )) do
     # ${hydra_root_dir}/scripts/evaluation/master_test_min_core_bigstation.sh ${i}
     ${hydra_root_dir}/scripts/evaluation/master_test_min_core_bigstation_dl.sh ${i}
 done
