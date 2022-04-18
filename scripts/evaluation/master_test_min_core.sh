ebbce3b670c5f4942501dc2ae3f2dc68654fa558
junzhig <junzhig@node0.junzhig-121704.opensketch-pg0.utah.cloudlab.us>
2022-04-18 16:00:16 -0600

    Debug script
    Former-commit-id: 7933d5dd6113b9fe6214e15ba77a3dc52cb8ac27

--- scripts/evaluation/master_test_min_core.sh
+++ scripts/evaluation/master_test_min_core.sh
@@ -13,9 +13,11 @@ ue_list=(24 40 48 8 24 40 48 8 16 24)
 for i in ${!ant_list[@]}; do
     ant_num=${ant_list[$i]}
     ue_num=${ue_list[$i]}
+    echo "Test ant ${ant_num} ue ${ue_num}"
     HYDRA_SYSTEM_CONFIG_JSON=config/template_msr/template_msr_${ant_num}_${ue_num}.json
     HYDRA_SERVER_DEPLOY_JSON=config/deploy_msr/deploy_msr_${ant_num}_${ue_num}.json
     cat ${hydra_master_config_json} | jq --arg template ${HYDRA_SYSTEM_CONFIG_JSON} '.hydra_system_config_json=$template' | \
         jq --arg deploy ${HYDRA_SERVER_DEPLOY_JSON} '.hydra_server_deploy_json=$deploy' > tmp.json
+    mv tmp.json ${hydra_master_config_json}
     ${hydra_root_dir}/scripts/evaluation/test_min_core.sh
 done
