da8cd86678c1c3822d7d21a76e26daac4db59bff
Gavindeed <491887485@qq.com>
2022-04-19 16:42:22 -0400

    Create cdf latency script
    Former-commit-id: 33ace33ebd33aad42bbbc8d700ab60902b8739ee

--- data/measurement/fig_cdf_latency.sh
+++ data/measurement/fig_cdf_latency.sh
@@ -1,46 +1,51 @@
 #! /bin/bash
 
-for (( i=0; i<10; i++ ))
+set -e
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+hydra_root_dir=$( cd ${script_dir}/../.. >/dev/null 2>&1 && pwd )
+
+source ${hydra_root_dir}/scripts/utils/utils.sh
+source ${hydra_root_dir}/scripts/control/init_platform.sh
+
+if [ ! -f ${hydra_root_dir}/scripts/evaluation/cdf ]; then
+    g++ -o ${hydra_root_dir}/scripts/evaluation/cdf ${hydra_root_dir}/scripts/evaluation/cdf.cpp
+fi
+
+for (( i=0; i<12; i++ ))
 do
-    cat frame_latency/frame_latency_all_64_8_1ms_70c_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
+    cat ${hydra_root_dir}/latency/msr_hydra_64_16_ul/frame_latency_all_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
 done
-cat tmp0.txt | ./cdf -m 0 -x 2.5 > tmp1.txt
+cat tmp0.txt | ${hydra_root_dir}/scripts/evaluation/cdf -m 0 -x 2.5 > tmp1.txt
 rm tmp0.txt
 
-for (( i=0; i<10; i++ ))
+for (( i=0; i<12; i++ ))
 do
-    cat frame_latency/frame_latency_all_64_16_1ms_70c_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
+    cat ${hydra_root_dir}/latency/msr_hydra_128_32_ul/frame_latency_all_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
 done
-cat tmp0.txt | ./cdf -m 0 -x 2.5 > tmp2.txt
+cat tmp0.txt | ${hydra_root_dir}/scripts/evaluation/cdf -m 0 -x 2.5 > tmp2.txt
 rm tmp0.txt
 
-for (( i=0; i<10; i++ ))
+for (( i=0; i<12; i++ ))
 do
-    cat frame_latency/frame_latency_all_64_32_1ms_70c_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
+    cat ${hydra_root_dir}/latency/msr_hydra_150_32_ul/frame_latency_all_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
 done
-cat tmp0.txt | ./cdf -m 0 -x 2.5 > tmp3.txt
+cat tmp0.txt | ${hydra_root_dir}/scripts/evaluation/cdf -m 0 -x 2.5 > tmp3.txt
 rm tmp0.txt
 
-for (( i=0; i<10; i++ ))
+for (( i=0; i<12; i++ ))
 do
-    cat frame_latency/frame_latency_all_128_16_1ms_70c_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
+    cat ${hydra_root_dir}/latency/msr_hydra_192_32_ul/frame_latency_all_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
 done
-cat tmp0.txt | ./cdf -m 0 -x 2.5 > tmp4.txt
+cat tmp0.txt | ${hydra_root_dir}/scripts/evaluation/cdf -m 0 -x 2.5 > tmp4.txt
 rm tmp0.txt
 
-for (( i=0; i<10; i++ ))
+for (( i=0; i<12; i++ ))
 do
-    cat frame_latency/frame_latency_all_128_32_1ms_70c_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
+    cat ${hydra_root_dir}/latency/msr_hydra_240_32_ul/frame_latency_all_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
 done
-cat tmp0.txt | ./cdf -m 0 -x 2.5 > tmp5.txt
+cat tmp0.txt | ${hydra_root_dir}/scripts/evaluation/cdf -m 0 -x 2.5 > tmp5.txt
 rm tmp0.txt
 
-for (( i=0; i<10; i++ ))
-do
-    cat frame_latency/frame_latency_all_150_32_1ms_70c_$i.txt | awk 'NR>1000 { print $6;  }' >> tmp0.txt
-done
-cat tmp0.txt | ./cdf -m 0 -x 2.5 > tmp6.txt
-rm tmp0.txt
-
-paste tmp1.txt tmp2.txt tmp3.txt tmp4.txt tmp5.txt tmp6.txt -d " " > measurement/fig_cdf_latency.txt
+paste tmp1.txt tmp2.txt tmp3.txt tmp4.txt tmp5.txt -d " " > ${hydra_root_dir}/data/measurement/fig_cdf_latency.txt
 rm tmp*.txt
