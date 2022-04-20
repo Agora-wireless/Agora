055ce1f661296d8a97fc80ca68e1130a946d66b9
junzhig <junzhig@node0.junzhig-124134.opensketch-pg0.utah.cloudlab.us>
2022-04-20 12:50:29 -0600

    Update script
    Former-commit-id: 6223c5e5af7c01b97c66b71c6fbc0341aa5a6f18

--- scripts/evaluation/latency_analysis.sh
+++ scripts/evaluation/latency_analysis.sh
@@ -9,20 +9,25 @@ source ${hydra_root_dir}/scripts/control/init_platform.sh
 rm ${hydra_root_dir}/data/frame_latency_all.txt
 
 valid=1
+j=0
 for (( i=0; i<${hydra_app_num}; i++ )) do
+    if [ "${i}" == "1" ]; then
+        continue
+    fi
     server_name=$(cat ${HYDRA_SERVER_DEPLOY_JSON} | jq --argjson i $i '.hydra_servers[$i]' | tr -d '"')
     eval "scp ${server_name}:${HYDRA_RUNNER_ROOT}/Agora/data/frame_latency.txt ${hydra_root_dir}/data/frame_latency_$i.txt"
-    num_lines=$(cat ${hydra_root_dir}/data/frame_latency_$i.txt | wc -l )
+    num_lines=$(cat ${hydra_root_dir}/data/frame_latency_$j.txt | wc -l )
     if [ "${num_lines}" != "${slots_to_test}" ]; then
         valid=0
     fi
+    j=$(( ${j}+1 ))
 done
 
 if [ "${valid}" == "1" ]; then
-    python ${hydra_root_dir}/scripts/evaluation/merge_latency.py ${hydra_app_num} ${hydra_root_dir}/data
+    python ${hydra_root_dir}/scripts/evaluation/merge_latency.py $(( ${hydra_app_num}-1 )) ${hydra_root_dir}/data
     cp ${hydra_root_dir}/data/frame_latency_all.txt ${hydra_root_dir}/data/frame_latency_all_$1.txt
 else
-    python ${hydra_root_dir}/scripts/evaluation/merge_latency.py ${hydra_app_num} ${hydra_root_dir}/data
+    python ${hydra_root_dir}/scripts/evaluation/merge_latency.py $(( ${hydra_app_num}-1 )) ${hydra_root_dir}/data
     cp ${hydra_root_dir}/data/frame_latency_all.txt ${hydra_root_dir}/data/frame_latency_all_error.txt
 fi
 # cat ${hydra_root_dir}/data/frame_latency_all.txt | awk 'NR > 1000 { print $6 }' >> ${hydra_root_dir}/data/useful_latency_data.txt 
