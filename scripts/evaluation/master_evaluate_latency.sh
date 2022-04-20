007e685ee681d1cc85918ff0f32f36eaf8b844d7
Gavindeed <491887485@qq.com>
2022-04-20 14:02:19 -0400

    Update script
    Former-commit-id: 50569dff08eaeb43e50e54c370ffb4ef52d261d4

--- scripts/evaluation/master_evaluate_latency.sh
+++ scripts/evaluation/master_evaluate_latency.sh
@@ -8,8 +8,8 @@ hydra_root_dir=$( cd ${script_dir}/../.. >/dev/null 2>&1 && pwd )
 source ${hydra_root_dir}/scripts/utils/utils.sh
 source ${hydra_root_dir}/scripts/control/init_platform.sh
 
-ant_list=(64 128 150 192 240)
-ue_list=(16 32 32 32 32)
+ant_list=(150)
+ue_list=(32)
 
 # for i in ${!ant_list[@]}; do
 #     ant_num=${ant_list[$i]}
@@ -91,12 +91,12 @@ for i in ${!ant_list[@]}; do
     ant_num=${ant_list[$i]}
     ue_num=${ue_list[$i]}
     echo "Run tail latency evaluation for ant ${ant_num} ue ${ue_num} core ${c}"
-    HYDRA_SERVER_DEPLOY_JSON=config/deploy_msr/deploy_msr_${ant_num}_${ue_num}.json
-    HYDRA_SYSTEM_CONFIG_JSON=config/template_msr/template_msr_${ant_num}_${ue_num}.json
+    HYDRA_SERVER_DEPLOY_JSON=config/deploy_msr_dl/deploy_msr_${ant_num}_${ue_num}.json
+    HYDRA_SYSTEM_CONFIG_JSON=config/template_msr_dl/template_msr_${ant_num}_${ue_num}.json
     cat ${hydra_master_config_json} | jq --arg deploy "${HYDRA_SERVER_DEPLOY_JSON}" '.hydra_server_deploy_json=$deploy' > tmp.json
     cat tmp.json | jq --arg template "${HYDRA_SYSTEM_CONFIG_JSON}" '.hydra_system_config_json=$template' > ${hydra_master_config_json}
     rm tmp.json
-    mkdir -p ${hydra_root_dir}/latency/msr_hydra_${ant_num}_${ue_num}_ul 
+    mkdir -p ${hydra_root_dir}/latency/msr_hydra_${ant_num}_${ue_num}_dl 
     ${hydra_root_dir}/scripts/evaluation/evaluate_tail_latency.sh
-    mv ${hydra_root_dir}/data/frame_latency_all_*.txt ${hydra_root_dir}/latency/msr_hydra_${ant_num}_${ue_num}_ul/
+    mv ${hydra_root_dir}/data/frame_latency_all_*.txt ${hydra_root_dir}/latency/msr_hydra_${ant_num}_${ue_num}_dl/
 done
