afbdcc8f1e93231ed4e0d825473a740a7d250171
Di Mu <di.mu@rice.edu>
2022-05-05 18:38:33 -0600

    update csv plot script for dual-user

--- plot-ue.py
+++ plot-ue.py
@@ -16,7 +16,7 @@ num_dev = int(sys.argv[1])
 
 # start of customizable plot config #
 
-title = 'Downlink User and Listener'
+title = 'Downlink Performance'
 
 enable_trace_plot = True
 enable_stats_plot = True
@@ -24,19 +24,20 @@ xlims = [[] for i in range(num_dev)]
 
 if use_csv_logger:
   cols = [
-    ['Frame', 'DL-Pilot-SNR-0'],
-    ['Frame', 'EVM-SNR'],
-    ['Frame', 'Bit-Error-Rate'],
-    ['Frame', 'Symbol-Error-Rate']
+    ['Frame', 'UE0-DLP0-SNR'],
+    ['Frame', 'UE1-DLP0-SNR'],
+    ['Frame', 'UE0-EVM-SNR'],
+    ['Frame', 'UE1-EVM-SNR'],
+    ['Frame', 'UE0-BER'],
+    ['Frame', 'UE1-BER']
   ]
   files = [
-    'log-dlpsnr-ue',
-    'log-evmsnr-ue',
-    'log-berser-ue',
-    'log-berser-ue'
+    'log-dlpsnr-ue-119.csv',
+    'log-evmsnr-ue-119.csv',
+    'log-berser-ue-119.csv'
   ]
-  stats_zero_fill = [False, False, False, False]
-  ylims = [[], [], [0.0,0.5], [0.0,0.5]]
+  stats_zero_fill = [False for i in range(len(cols))]
+  ylims = [[] for i in range(len(cols))]
 
 else: # use log txt
   cols = [
@@ -59,12 +60,12 @@ else: # use log txt
 
 num_file = len(files)
 xlabel = cols[0][0]
-ylabels = [cols[i][1] for i in range(num_file)]
+ylabels = [cols[i][1] for i in range(len(cols))]
 
 dfs = []
 for i in range(num_file):
   for j in range(num_dev):
-    df = pd.read_csv(files[i] + f'-{j}.csv', usecols=cols[i]).dropna()
+    df = pd.read_csv(files[i], usecols=cols[i * num_dev + j]).dropna()
     dfs.append(df)
     xminmax = [min(df[xlabel]), max(df[xlabel])]
     if xlims[j] == []:
@@ -87,13 +88,13 @@ if enable_trace_plot:
   for i in range(num_file):
     for j in range(num_dev):
       idx = i * num_dev + j
-      axs[i].plot(dfs[idx][xlabel], str2float(dfs[idx][ylabels[i]].copy()),
+      axs[i].plot(dfs[idx][xlabel], str2float(dfs[idx][ylabels[idx]].copy()),
                   label = 'UE ' + str(j))
     axs[i].set_xlim(xlims[0])
-    if ylims[i] != []:
-      axs[i].set_ylim(ylims[i])
+    if ylims[idx] != []:
+      axs[i].set_ylim(ylims[idx])
     axs[i].set_xlabel(xlabel)
-    axs[i].set_ylabel(ylabels[i])
+    axs[i].set_ylabel(ylabels[idx])
     axs[i].legend()
 
 if enable_stats_plot:
@@ -102,10 +103,10 @@ if enable_stats_plot:
 for i in range(num_file):
   for j in range(num_dev):
     idx = i * num_dev + j
-    data_select = dfs[idx][ylabels[i]][dfs[idx][xlabel] \
+    data_select = dfs[idx][ylabels[idx]][dfs[idx][xlabel] \
                   .between(xlims[j][0], xlims[j][1])]
     str2float(data_select)
-    if stats_zero_fill[i]:
+    if stats_zero_fill[idx]:
       len_zeros = (xlims[j][1] - xlims[j][0] + 1) - len(data_select)
       if len_zeros > 0:
         data_select = pd.concat([data_select, pd.Series(np.zeros(len_zeros))])
@@ -113,12 +114,12 @@ for i in range(num_file):
       axs[i].plot(np.sort(data_select),
                   1. * np.arange(len(data_select)) / (len(data_select) - 1),
                   label = 'UE ' + str(j))
-    print(ylabels[i] + f': Mean[{j}] = %f, ' % sts.mean(data_select) +
+    print(ylabels[idx] + f': Mean[{j}] = %f, ' % sts.mean(data_select) +
           f'Median[{j}] = %f' % sts.median(data_select))
-    if ylabels[i] == 'Symbol-Errors':
+    if ylabels[idx] == 'Symbol-Errors':
       print(f'SER[{j}] = %f' % (sts.mean(data_select) / qam_symbols_per_frame))
   if enable_stats_plot:
-    axs[i].set_xlabel(ylabels[i])
+    axs[i].set_xlabel(ylabels[idx])
     axs[i].set_ylabel('CDF')
     axs[i].legend()
 if enable_trace_plot or enable_stats_plot:
