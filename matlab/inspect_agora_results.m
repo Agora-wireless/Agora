f25f39dfd7da2e0875aeca3c17d4d2d8ee2fe5b2
Andrew Sedlmayr <andrew.sedlmayr@rice.edu>
2022-07-22 12:29:38 -0500

    Changed to hdf5 input parsing

--- matlab/inspect_agora_results.m
+++ matlab/inspect_agora_results.m
@@ -1,111 +1,138 @@
-function inspect_agora_results_old(frame_id)
-    %close all;
-
-    symbol_size = 896;
-    fft_size = 512;
-    data_size = 304;
-    offset = 160;
-    cp = 32;
-    n_user = 1;
-    pig = [0];
-    dig = [1:2];
-    ue_serial = ['241';''];
-
-    nz_start_idx = (fft_size - data_size)/2;
-    nz_sc_idx = nz_start_idx+1:nz_start_idx+data_size;
-
-    plt_trk_sp = 16;
-    data_sc_idx = setdiff(1:data_size, 1:plt_trk_sp:data_size);
-
-    rx_pilot = zeros(symbol_size, n_user, length(pig));
-    tx_pilot = zeros(data_size, n_user, length(pig));
-    rx_data = zeros(symbol_size, n_user, length(dig));
-    tx_data = zeros(data_size, n_user, length(dig));
-
-    %% Load Data
-    % read rx and tx pilot files
-    for u=1:n_user
-      for p=1:length(pig)
-        filename_ext = strcat(int2str(frame_id), '_', int2str(pig(p)), '_', ...
-                              int2str(u-1), '_', ue_serial(u,:), '.bin');
-        filename = strcat('rxpilot_', filename_ext);
-        fileID = fopen(filename, 'r');
-        rx = fread(fileID, 'short');
-        fclose(fileID);
-        rx_pilot(:, u, p) = rx(1:2:end)/32768 + 1j*rx(2:2:end)/32768;
-
-        filename = strcat('txpilot_', filename_ext);
-        fileID = fopen(filename, 'r');
-        tx = fread(fileID, 'float');
-        fclose(fileID);
-        tx_pilot(:, u, p) = tx(1:2:end) + 1j*tx(2:2:end);
-      end  
+function inspect_agora_results(dataset_filename, inspect_frame, verbose)
+    %%Load data from the input file and inspect evm and snr
+    % -------- Fixed Values --------
+    group_id = '/Data';
+    %Display file / group attributes and datasets
+    if verbose == "true"
+        h5disp(dataset_filename,group_id);
     end
 
-    % read rx and tx data files
-    for u=1:n_user
-      for d=1:length(dig)
-        filename_ext = strcat(int2str(frame_id), '_', int2str(dig(d)), '_', ...
-                              int2str(u-1), '_', ue_serial(u,:), '.bin');
-        filename = strcat('rxdata_', filename_ext);
-        fileID = fopen(filename, 'r');
-        data = fread(fileID, 'short');
-        fclose(fileID);
-        rx_data(:, u, d) = data(1:2:end)/32768 + 1j*data(2:2:end)/32768;
+    samples_per_slot = double(h5readatt(dataset_filename, group_id, 'SLOT_SAMP_LEN'));
+    data_size = double(h5readatt(dataset_filename, group_id, 'OFDM_DATA_NUM'));
+    data_start = double(h5readatt(dataset_filename, group_id, 'OFDM_DATA_START'));
+    data_stop = double(h5readatt(dataset_filename, group_id, 'OFDM_DATA_STOP'));
+    ofdm_ca_size = double(h5readatt(dataset_filename, group_id, 'OFDM_CA_NUM'));
+    total_dl_symbols = double(h5readatt(dataset_filename, group_id, 'DL_SLOTS'));
+    total_users = 1;
+    dl_pilot_symbols = double(h5readatt(dataset_filename, group_id, 'DL_PILOT_SLOTS'));
+    dl_data_symbols = total_dl_symbols - dl_pilot_symbols;
+    cp_len = double(h5readatt(dataset_filename, group_id, 'CP_LEN'));
+    tx_zero_prefix_len = double(h5readatt(dataset_filename, group_id, 'TX_ZERO_PREFIX_LEN'));
+    fft_size = double(h5readatt(dataset_filename, group_id, 'OFDM_CA_NUM'));
 
-        filename = strcat('txdata_', filename_ext);
-        fileID = fopen(filename, 'r');
-        data = fread(fileID, 'float');
-        fclose(fileID);
-        txdata_tmp = data(1:2:end) + 1j*data(2:2:end);
-        tx_data(:, u, d) = txdata_tmp(nz_sc_idx);
-      end  
+    %Choose the downlink data
+    dataset_id = '/DownlinkData';
+
+    % Dimensions  [Samples, Ant, Symbol, Cells, Frame]
+    start = [1 1 1 1 inspect_frame];
+    count = [(samples_per_slot * 2) total_users total_dl_symbols 1 1];
+    %Display Info
+    if verbose == "true"
+        h5disp(dataset_filename,strcat(group_id,dataset_id));
     end
+    %Generate a int16 array
+    rx_syms_hdf5 = h5read(dataset_filename, strcat(group_id,dataset_id), start, count);
+    %Convert to double and scale
+    rx_syms_scaled_double = double(rx_syms_hdf5) ./ double(intmax('int16'));
+    clear rx_syms_hdf5;
+    %Convert to complex double
+    % Samples x User x Symbol
+    rx_syms_cxdouble = complex(rx_syms_scaled_double(1:2:end,:,:), rx_syms_scaled_double(2:2:end,:, :));
+    clear rx_syms_scaled_double;
+    % Split off pilots and data
+    rx_pilot_cxdouble = rx_syms_cxdouble(:,:,1:dl_pilot_symbols);
+    rx_data_cxdouble = rx_syms_cxdouble(:,:,1+dl_pilot_symbols:end);
+    clear start count rx_syms_cxdouble;
+
+    dataset_id = '/TxPilot';
+    %*2 for complex type (native float)
+    total_samples = data_size * 2;
+    if verbose == "true"
+        h5disp(dataset_filename,strcat(group_id,dataset_id));
+    end 
+    start = [1 1 1 1 1];
+    count = [total_samples total_users 1 1 1];
+    tx_pilot_hdf5 = double(h5read(dataset_filename, strcat(group_id,dataset_id), start, count));
+    %Convert to complex
+    tx_pilot_cxdouble = complex(tx_pilot_hdf5(1:2:end,:), tx_pilot_hdf5(2:2:end,:));
+    clear tx_pilot_hdf5 dataset_id start count total_samples;
+
+    dataset_id = '/TxData';
+    %Compare the pilot data
+    total_samples = ofdm_ca_size * 2;
+    if verbose == "true"
+        h5disp(dataset_filename,strcat(group_id,dataset_id));
+    end
+    start = [1 1 1 1 1];
+    count = [total_samples total_users total_dl_symbols 1 1];
+    tx_data_hdf5 = double(h5read(dataset_filename, strcat(group_id,dataset_id), start, count));
+    %Trim to data only
+    tx_data_hdf5 = tx_data_hdf5(1+(data_start * 2):(data_stop * 2),1, :);
+    %Convert to complex type
+    tx_data_cxdouble = complex(tx_data_hdf5(1:2:end,:,:), tx_data_hdf5(2:2:end,:,:));
+    % Samples (complex) x User Ant x Downlink Symbol Id
+    % removing the pilot for now because it doesn't check out?
+    %tx_pilot_bad = tx_data_cxdouble(:,:,1);
+    tx_data_cxdouble = tx_data_cxdouble(:,:,2:end);
+    clear start count total_samples tx_data_hdf5 dataset_id;
+
+    clear dataset_filename group_id;
+
 
     %% Process Loaded Files (Channel Estimation and Equalization)
     % Plot Constellations and print EVMs and SNRs
-    start_id = cp + offset;
-    snr = zeros(1, n_user);
-    evm = zeros(1, n_user);
+    start_id = cp_len + tx_zero_prefix_len;
+    snr = zeros(1, total_users);
+    evm = zeros(1, total_users);
     cl = 0;
-    for u=1:n_user
-        ch_est = zeros(data_size, length(pig));
-        for p=1:length(pig)
-          rx_pilot_f_tmp = fft(rx_pilot(start_id + 1:start_id + fft_size, u, p));
-          ch_est(:, p) = rx_pilot_f_tmp(nz_sc_idx) ./ tx_pilot(:, u, p);
+    nz_start_idx = (fft_size - data_size)/2;
+    nz_sc_idx = nz_start_idx+1:nz_start_idx+data_size;
+    clear nz_start_idx;
+    plt_trk_sp = 16;
+    data_sc_idx = setdiff(1:data_size, 1:plt_trk_sp:data_size);
+    for u=1:total_users
+        % Process pilots
+        ch_est = zeros(data_size, dl_pilot_symbols);
+        for p=1:dl_pilot_symbols
+          rx_pilot_f_tmp = fft(rx_pilot_cxdouble(start_id + 1:start_id + fft_size, u, p));
+          ch_est(:, p) = rx_pilot_f_tmp(nz_sc_idx) ./ tx_pilot_cxdouble(:, u, p);
         end
-        if length(pig) == 1
+        clear p
+        if dl_pilot_symbols == 1
             ch_est_mean = ch_est;
         else
             ch_est_mean = mean(ch_est, 2);
         end
-        data_phase_corr = zeros(data_size, length(dig));
-        aevms = zeros(u, length(dig));
-        for d=1:length(dig)
-          rx_data_f_tmp = fft(rx_data(start_id + 1:start_id + fft_size, u, d));
+
+        %Process data symbols
+        data_phase_corr = zeros(data_size, dl_data_symbols);
+        aevms = zeros(u, dl_data_symbols);
+        for d=1:dl_data_symbols
+          rx_data_f_tmp = fft(rx_data_cxdouble(start_id + 1:start_id + fft_size, u, d));
           data_eq = rx_data_f_tmp(nz_sc_idx) ./ ch_est_mean;
 
           % pilot tracking
-          phase_err = angle(mean((data_eq(1:plt_trk_sp:end) .* conj(tx_pilot(1:plt_trk_sp:end, u)))));
+          phase_err = angle(mean((data_eq(1:plt_trk_sp:end) .* conj(tx_pilot_cxdouble(1:plt_trk_sp:end, u)))));
           data_phase_corr(data_sc_idx, d) = data_eq(data_sc_idx) .* exp(-1j*phase_err);
 
-          evm_mat = abs(data_phase_corr(data_sc_idx, d) - tx_data(data_sc_idx, u, d)).^2;
+          evm_mat = abs(data_phase_corr(data_sc_idx, d) - tx_data_cxdouble(data_sc_idx, u, d)).^2;
           aevms(u, d) = mean(evm_mat(:)); % needs to be a scalar
 
           cl = cl + 1;
-          figure(cl+2);
+          figure(cl);
           scatter(real(data_phase_corr(data_sc_idx, d)), imag(data_phase_corr(data_sc_idx, d)),'r')
           hold on
-          scatter(real(tx_data(data_sc_idx)), imag(tx_data(data_sc_idx)),'b')
+          scatter(real(tx_data_cxdouble(data_sc_idx)), imag(tx_data_cxdouble(data_sc_idx)),'b')
           title(['Constellation [User ', num2str(u), ', Symbol ', num2str(d), ']'])
         end
+        clear d
 
         snr(u) = 10*log10(1./mean(aevms(u, :))); % calculate in dB scale.
         evm(u) = mean(aevms(u, :)) * 100;
-
     end
-    
-    disp(['Frame Inspect: ', num2str(frame_id)]);
+    clear u cl start_id rx_pilot_f_tmp rx_data_f_tmp plt_trk_sp nz_sc_idx;
+
+    disp(['Frame Inspect: ', num2str(inspect_frame)]);
     disp(['SNR: ', num2str(snr)]);
     disp(['EVM: ', num2str(evm)]);
 end
