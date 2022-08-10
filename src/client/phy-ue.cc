d8aa8b9015c9aa561e4d28842c3821458152cbde
Andrew Sedlmayr <andrew.sedlmayr@rice.edu>
2022-08-10 15:03:27 -0500

    change to simd

--- src/client/phy-ue.cc
+++ src/client/phy-ue.cc
@@ -65,14 +65,15 @@ PhyUe::PhyUe(Config* config)
 
   ue_pilot_vec_.resize(config_->UeAntNum());
   for (size_t i = 0; i < config_->UeAntNum(); i++) {
-    for (size_t j = config->OfdmTxZeroPrefix();
-         j < config_->SampsPerSymbol() - config->OfdmTxZeroPostfix(); j++) {
-      ue_pilot_vec_.at(i).push_back(std::complex<float>(
-          static_cast<float>(config_->UeSpecificPilotT()[i][j].real()) /
-              kShrtFltConvFactor,
-          static_cast<float>(config_->UeSpecificPilotT()[i][j].imag()) /
-              kShrtFltConvFactor));
-    }
+    const size_t pilot_len_samples =
+        config_->SampsPerSymbol() -
+        (config->OfdmTxZeroPostfix() + config->OfdmTxZeroPrefix());
+    auto& ue_pilot_f = ue_pilot_vec_.at(i);
+    ue_pilot_f.resize(pilot_len_samples);
+    SimdConvertShortToFloat(
+        reinterpret_cast<const short*>(
+            &config_->UeSpecificPilotT()[i][config->OfdmTxZeroPrefix()]),
+        reinterpret_cast<float*>(ue_pilot_f.data()), pilot_len_samples * 2);
   }
 
   complete_queue_ = moodycamel::ConcurrentQueue<EventData>(
