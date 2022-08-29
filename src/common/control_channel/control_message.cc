66837a0b200e06eef9b403fbd1edfde77be06fec
Andrew Sedlmayr <andrew.sedlmayr@rice.edu>
2022-08-29 13:44:34 -0500

    added messages
    Former-commit-id: ebdf7915eee3b4829e7d2302109c634bdf58204b

--- src/common/control_channel/control_message.cc
+++ src/common/control_channel/control_message.cc
@@ -4,11 +4,23 @@
  */
 #include "control_message.h"
 
+#include <cstring>
+
 std::unique_ptr<ControlMessage> ControlMessage::Construct(
     const std::byte* raw_data, size_t data_size) {
   switch (static_cast<ControlMessageType>(raw_data[0u])) {
     case ControlMessageType::kAssociation: {
       auto rx_message = std::make_unique<AssociationMessage>();
+      rx_message->Deserialize(raw_data, data_size);
+      return rx_message;
+    }
+    case ControlMessageType::kConnectAccept: {
+      auto rx_message = std::make_unique<ConnectionAcceptMessage>();
+      rx_message->Deserialize(raw_data, data_size);
+      return rx_message;
+    }
+    case ControlMessageType::kPowerControl: {
+      auto rx_message = std::make_unique<PowerControlMessage>();
       rx_message->Deserialize(raw_data, data_size);
       return rx_message;
     }
@@ -19,9 +31,51 @@ std::unique_ptr<ControlMessage> ControlMessage::Construct(
 
 void AssociationMessage::Deserialize(const std::byte* raw_data,
                                      size_t data_size) {
-  ((void)(raw_data));
-  ((void)(data_size));
-  client_id_ = 4;
-  //Null terminated c style string?
-  client_serial_ = {'0', '0', '\0'};
+  if (static_cast<uint8_t>(raw_data[0u]) != Type()) {
+    throw std::runtime_error("Invalid type");
+  }
+  client_serial_.append(reinterpret_cast<const char*>(&raw_data[1u]),
+                        data_size - 1);
+}
+
+size_t AssociationMessage::Serialize(std::byte* raw_data) {
+  raw_data[0u] = static_cast<std::byte>(Type());
+  std::memcpy(&raw_data[1u], client_serial_.data(), client_serial_.length());
+  return client_serial_.length() + 1;
+}
+
+void ConnectionAcceptMessage::Deserialize(const std::byte* raw_data,
+                                          size_t data_size) {
+  if (static_cast<uint8_t>(raw_data[0u]) != Type()) {
+    throw std::runtime_error("Invalid type");
+  }
+  client_id_ = static_cast<uint8_t>(raw_data[1u]);
+  client_serial_.append(reinterpret_cast<const char*>(&raw_data[2u]),
+                        data_size - 2);
+}
+
+size_t ConnectionAcceptMessage::Serialize(std::byte* raw_data) {
+  raw_data[0u] = static_cast<std::byte>(Type());
+  raw_data[1u] = static_cast<std::byte>(client_id_);
+  std::memcpy(&raw_data[2u], client_serial_.data(), client_serial_.length());
+  return client_serial_.length() + 2;
+}
+
+void PowerControlMessage::Deserialize(const std::byte* raw_data,
+                                      size_t data_size) {
+  if (static_cast<uint8_t>(raw_data[0u]) != Type()) {
+    throw std::runtime_error("Invalid type");
+  }
+  if (data_size != 3) {
+    throw std::runtime_error("Invalid Size of Power Control Message");
+  }
+  client_id_ = static_cast<uint8_t>(raw_data[1u]);
+  power_adjust_ = static_cast<int8_t>(raw_data[2u]);
+}
+
+size_t PowerControlMessage::Serialize(std::byte* raw_data) {
+  raw_data[0u] = static_cast<std::byte>(Type());
+  raw_data[1u] = static_cast<std::byte>(client_id_);
+  raw_data[2u] = static_cast<std::byte>(power_adjust_);
+  return 3;
 }
--- src/common/control_channel/control_message.h
+++ src/common/control_channel/control_message.h
@@ -17,19 +17,22 @@ class ControlMessage {
   enum ControlMessageType : uint8_t {
     kInvalidMessage = 0x0,
     kAssociation = 0x01,
-    kPowerControl = 0x02
+    kConnectAccept = 0x02,
+    kPowerControl = 0x03,
   };
   ControlMessage(ControlMessageType type) : type_(type) {}
   virtual ~ControlMessage() = default;
 
-  ControlMessageType Type() const { return type_; }
+  inline ControlMessageType Type() const { return type_; }
   virtual void Print() const = 0;
   static std::unique_ptr<ControlMessage> Construct(const std::byte* raw_data,
                                                    size_t data_size);
 
+  virtual void Deserialize(const std::byte* raw_data, size_t data_size) = 0;
+  virtual size_t Serialize(std::byte* raw_data) = 0;
+
  private:
   ControlMessageType type_;
-  virtual void Deserialize(const std::byte* raw_data, size_t data_size) = 0;
 };
 
 class AssociationMessage : public ControlMessage {
@@ -38,26 +41,49 @@ class AssociationMessage : public ControlMessage {
   virtual ~AssociationMessage() final = default;
 
   virtual void Print() const final {
-    std::printf("Association Message from client %d, with serial %s\n",
-                client_id_, client_serial_.data());
+    std::printf("Association Message from client with serial %s\n",
+                client_serial_.c_str());
   }
   virtual void Deserialize(const std::byte* raw_data, size_t data_size) final;
+  virtual size_t Serialize(std::byte* raw_data) final;
+
+ private:
+  std::string client_serial_;
+};
+
+class ConnectionAcceptMessage : public ControlMessage {
+ public:
+  ConnectionAcceptMessage()
+      : ControlMessage(ControlMessageType::kConnectAccept) {}
+  virtual ~ConnectionAcceptMessage() final = default;
+
+  virtual void Print() const final {
+    std::printf("Connection Accept Message to client %d, with serial %s\n",
+                client_id_, client_serial_.c_str());
+  }
+  virtual void Deserialize(const std::byte* raw_data, size_t data_size) final;
+  virtual size_t Serialize(std::byte* raw_data) final;
 
  private:
   uint8_t client_id_;
-  //Null terminated c style string?
-  std::array<char, kClientIdCharCnt> client_serial_;
+  std::string client_serial_;
 };
 
-/*
 class PowerControlMessage : public ControlMessage {
  public:
-  PowerControlMessage() { type_ = ControlMessageType::kPowerControl; }
-  //void Serialize();
+  PowerControlMessage() : ControlMessage(ControlMessageType::kPowerControl) {}
+  virtual ~PowerControlMessage() final = default;
+
+  virtual void Print() const final {
+    std::printf("Power Control Message to client %d, adjust power %d\n",
+                client_id_, power_adjust_);
+  }
+  virtual void Deserialize(const std::byte* raw_data, size_t data_size) final;
+  virtual size_t Serialize(std::byte* raw_data) final;
+
  private:
-  int8_t power_adjust_;
   uint8_t client_id_;
+  int8_t power_adjust_;
 };
-*/
 
 #endif /* CONTROL_MESSAGE_H_ */
