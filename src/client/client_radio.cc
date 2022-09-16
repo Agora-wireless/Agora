b8e4df28769e179d66bf3d3403a435cef3811d7b
Tom <82247342+Shadowcaster77@users.noreply.github.com>
2022-09-16 13:27:43 -0400

    Update client_radio.cc

--- src/client/client_radio.cc
+++ src/client/client_radio.cc
@@ -10,14 +10,24 @@
 ClientRadioConfig::ClientRadioConfig(const Config* const cfg,
                                      Radio::RadioType radio_type)
     : cfg_(cfg) {
+#if defined(USE_PURE_UHD)
+  total_radios_ = 1;
+#else
   total_radios_ = cfg_->UeNum();
+#endif
+
   total_antennas_ = cfg_->UeAntNum();
   std::cout << "Total Number of Client Radios " << total_radios_ << " with "
             << total_antennas_ << " antennas" << std::endl;
 
+// could be further optimized with the previous ifdef, but for debugging purpose, will seperate for now
+#if defined(USE_PURE_UHD)
+  radios_ = Radio::Create(radio_type);
+#else
   for (size_t i = 0; i < total_radios_; i++) {
     radios_.emplace_back(Radio::Create(radio_type));
   }
+#endif
 
   std::vector<std::thread> radio_threads;
   num_client_radios_initialized_ = 0;
@@ -50,16 +60,26 @@ ClientRadioConfig::ClientRadioConfig(const Config* const cfg,
   }
 #endif
 
+#if defined(USE_PURE_UHD)
+  radios_->PrintSettings();
+#else
   for (const auto& radio : radios_) {
     radio->PrintSettings();
   }
+#endif
   AGORA_LOG_INFO("ClientRadioConfig: Radio init complete\n");
 }
 
 void ClientRadioConfig::InitClientRadio(size_t radio_id) {
+  #if defined(USE_PURE_UHD)
+  radios_->Init(cfg_, radio_id, cfg_->UeRadioId().at(radio_id),
+                             Utils::StrToChannels(cfg_->UeChannel()),
+                             cfg_->UeHwFramer());
+  #else
   radios_.at(radio_id)->Init(cfg_, radio_id, cfg_->UeRadioId().at(radio_id),
                              Utils::StrToChannels(cfg_->UeChannel()),
                              cfg_->UeHwFramer());
+  #endif
 
   std::vector<double> tx_gains;
   tx_gains.emplace_back(cfg_->ClientTxGainA(radio_id));
@@ -69,67 +89,110 @@ void ClientRadioConfig::InitClientRadio(size_t radio_id) {
   rx_gains.emplace_back(cfg_->ClientRxGainA(radio_id));
   rx_gains.emplace_back(cfg_->ClientRxGainB(radio_id));
 
+  #if defined(USE_PURE_UHD)
+  radios_->Setup(tx_gains, rx_gains);
+  #else
   radios_.at(radio_id)->Setup(tx_gains, rx_gains);
+  #endif
   this->num_client_radios_initialized_.fetch_add(1);
 }
 
 bool ClientRadioConfig::RadioStart() {
   // send through the first radio for now
+#if defined(USE_PURE_UHD)
+  if (cfg_->UeHwFramer()) {
+      radios_->ConfigureTddModeUe();
+      }
+  radios_->Activate(Radio::ActivationTypes::kActivateWaitTrigger);
+#else
   for (size_t i = 0; i < total_radios_; i++) {
     if (cfg_->UeHwFramer()) {
       radios_.at(i)->ConfigureTddModeUe();
     }
     radios_.at(i)->Activate(Radio::ActivationTypes::kActivateWaitTrigger);
   }
+#endif
   AGORA_LOG_INFO("ClientRadioConfig: Radio start complete!\n");
   return true;
 }
 
-void ClientRadioConfig::Go() const {
-  if (kUseUHD == false) {
+void ClientRadioConfig::Go() {
+  #if defined(USE_PURE_UHD)
+  #else
+  if ((kUseUHD == false) || (kUsePureUHD == false)) {
     for (size_t i = 0; i < total_radios_; i++) {
       radios_.at(i)->Trigger();
     }
   }
+  #endif
 }
 
 int ClientRadioConfig::RadioTx(size_t radio_id, void** buffs, size_t num_samps,
                                Radio::TxFlags flags, long long& tx_time) {
+#if defined(USE_PURE_UHD)
+  return radios_->Tx(buffs, num_samps, flags, tx_time);
+#else
   return radios_.at(radio_id)->Tx(buffs, num_samps, flags, tx_time);
+#endif
 }
 
 int ClientRadioConfig::RadioRx(
     size_t radio_id, std::vector<std::vector<std::complex<int16_t>>>& rx_data,
     size_t rx_size, Radio::RxFlags& out_flags, long long& rx_time_ns) {
+#if defined(USE_PURE_UHD)
+  return radios_->Rx(rx_data, rx_size, out_flags, rx_time_ns);
+#else
   return radios_.at(radio_id)->Rx(rx_data, rx_size, out_flags, rx_time_ns);
+#endif
 }
 
 int ClientRadioConfig::RadioRx(
     size_t radio_id, std::vector<std::vector<std::complex<int16_t>>*>& rx_buffs,
     size_t rx_size, Radio::RxFlags& out_flags, long long& rx_time_ns) {
+#if defined(USE_PURE_UHD)
+  return radios_->Rx(rx_buffs, rx_size, out_flags, rx_time_ns);
+#else
   return radios_.at(radio_id)->Rx(rx_buffs, rx_size, out_flags, rx_time_ns);
+#endif
 }
 
 int ClientRadioConfig::RadioRx(size_t radio_id, std::vector<void*>& rx_locs,
                                size_t rx_size, Radio::RxFlags& out_flags,
                                long long& rx_time_ns) {
+#if defined(USE_PURE_UHD)
+  return radios_->Rx(rx_locs, rx_size, out_flags, rx_time_ns);
+#else
   return radios_.at(radio_id)->Rx(rx_locs, rx_size, out_flags, rx_time_ns);
+#endif
 }
 
 void ClientRadioConfig::ReadSensors() {
+#if defined(USE_PURE_UHD)
+  // radios_->ReadSensors();
+#else
   for (const auto& radio : radios_) {
     radio->ReadSensor();
   }
+#endif
 }
 
 void ClientRadioConfig::RadioStop() {
+#if defined(USE_PURE_UHD)
+  radios_->Deactivate();
+#else
   for (auto& radio : radios_) {
     radio->Deactivate();
   }
+#endif
 }
 
 ClientRadioConfig::~ClientRadioConfig() {
+
+#if defined(USE_PURE_UHD)
+  radios_->Close();
+#else
   for (auto& radio : radios_) {
     radio->Close();
   }
+#endif
 }
