da53d430f9f314fc053cc61c32349fa3752793c0
Tom <82247342+Shadowcaster77@users.noreply.github.com>
2022-10-04 11:42:09 -0400

    Update client_radio.cc
    Former-commit-id: 80eb80983007d3c9608f45357c77486291ebd3cd

--- src/client/client_radio.cc
+++ src/client/client_radio.cc
@@ -70,14 +70,15 @@ ClientRadioConfig::ClientRadioConfig(const Config* const cfg,
 }
 
 void ClientRadioConfig::InitClientRadio(size_t radio_id) {
-#if defined(USE_PURE_UHD)
+  #if defined(USE_PURE_UHD)
   radios_->Init(cfg_, radio_id, cfg_->UeRadioId().at(radio_id),
-                Utils::StrToChannels(cfg_->UeChannel()), cfg_->UeHwFramer());
-#else
+                             Utils::StrToChannels(cfg_->UeChannel()),
+                             cfg_->UeHwFramer());
+  #else
   radios_.at(radio_id)->Init(cfg_, radio_id, cfg_->UeRadioId().at(radio_id),
                              Utils::StrToChannels(cfg_->UeChannel()),
                              cfg_->UeHwFramer());
-#endif
+  #endif
 
   std::vector<double> tx_gains;
   tx_gains.emplace_back(cfg_->ClientTxGainA(radio_id));
@@ -87,11 +88,11 @@ void ClientRadioConfig::InitClientRadio(size_t radio_id) {
   rx_gains.emplace_back(cfg_->ClientRxGainA(radio_id));
   rx_gains.emplace_back(cfg_->ClientRxGainB(radio_id));
 
-#if defined(USE_PURE_UHD)
+  #if defined(USE_PURE_UHD)
   radios_->Setup(tx_gains, rx_gains);
-#else
+  #else
   radios_.at(radio_id)->Setup(tx_gains, rx_gains);
-#endif
+  #endif
   this->num_client_radios_initialized_.fetch_add(1);
 }
 
@@ -99,8 +100,8 @@ bool ClientRadioConfig::RadioStart() {
   // send through the first radio for now
 #if defined(USE_PURE_UHD)
   if (cfg_->UeHwFramer()) {
-    radios_->ConfigureTddModeUe();
-  }
+      radios_->ConfigureTddModeUe();
+      }
   radios_->Activate(Radio::ActivationTypes::kActivateWaitTrigger);
 #else
   for (size_t i = 0; i < total_radios_; i++) {
@@ -115,14 +116,14 @@ bool ClientRadioConfig::RadioStart() {
 }
 
 void ClientRadioConfig::Go() const {
-#if defined(USE_PURE_UHD)
-#else
+  #if defined(USE_PURE_UHD)
+  #else
   if ((kUseUHD == false) || (kUsePureUHD == false)) {
     for (size_t i = 0; i < total_radios_; i++) {
       radios_.at(i)->Trigger();
     }
   }
-#endif
+  #endif
 }
 
 int ClientRadioConfig::RadioTx(size_t radio_id, void** buffs, size_t num_samps,
