bf5afc4dfc60c5851ea14e2f4171d5a51a403da4
Tom <82247342+Shadowcaster77@users.noreply.github.com>
2022-10-04 11:16:36 -0400

    Update radio_uhdsdr.h
    Update to make 1*1 work

--- src/agora/radio/radio_uhdsdr.h
+++ src/agora/radio/radio_uhdsdr.h
@@ -15,7 +15,7 @@
 
 class RadioUHDSdr : public Radio {
  public:
-  explicit RadioUHDSdr();  // NO need for DataPlaneType for UHD Radios,
+  explicit RadioUHDSdr(); // NO need for DataPlaneType for UHD Radios, 
   ~RadioUHDSdr() final;
 
   void Init(const Config* cfg, size_t id, const std::string& serial,
@@ -23,14 +23,14 @@ class RadioUHDSdr : public Radio {
   void Setup(const std::vector<double>& tx_gains,
              const std::vector<double>& rx_gains) final;
 
-  // change to more generic version of Radio::ActivateTypes, if needed, add new act types
+// change to more generic version of Radio::ActivateTypes, if needed, add new act types
   void Activate(Radio::ActivationTypes type = Radio::ActivationTypes::kActivate,
                 long long act_time_ns = 0, size_t samples = 0);
   void Deactivate() final;
   void Close() final;
-  void Flush() final;
+  void Flush() final; 
 
-  // change to Radio::TxFlags, if new flags are needed, add them to Radio
+// change to Radio::TxFlags, if new flags are needed, add them to Radio
   int Tx(const void* const* tx_buffs, size_t tx_size, Radio::TxFlags tx_flags,
          long long& tx_time_ns);
 
@@ -55,30 +55,29 @@ class RadioUHDSdr : public Radio {
   void ReadSensor() const final;
   // void AdjustDelay(const std::string& delay) {};
 
-  //   // Calibration helper functions -
+//   // Calibration helper functions - 
 
-  //   //Sets both tx and rx, is this ok?
-  //   void SetFreqBb(size_t channel, double freq);
-  //   //End Calibration Routines
-  //   std::vector<std::complex<float>> SnoopSamples(size_t channel,
-  //                                                 size_t read_size);
 
-  inline uhd::usrp::multi_usrp::sptr UHDDevice() const {
-    return dev_;
-  }  // TODO: check which higher layer functions needs this?
+//   //Sets both tx and rx, is this ok?
+//   void SetFreqBb(size_t channel, double freq);
+//   //End Calibration Routines
+//   std::vector<std::complex<float>> SnoopSamples(size_t channel,
+//                                                 size_t read_size);
+
+  inline uhd::usrp::multi_usrp::sptr UHDDevice() const { return dev_; } // TODO: check which higher layer functions needs this? 
   inline const std::string& IpAddress() const { return ip_address_; }
 
  private:
-  //   void Correlator(bool enable);
+//   void Correlator(bool enable);
 
   uhd::usrp::multi_usrp::sptr dev_;
   std::string ip_address_;
 
-  //   std::unique_ptr<RadioDataPlaneUHD> rxp_;
+//   std::unique_ptr<RadioDataPlaneUHD> rxp_;
 
-  uhd::rx_streamer::sptr rxs_;  // init in RadioUHDSdr::Init()
+  uhd::rx_streamer::sptr rxs_; // init in RadioUHDSdr::Init()
   uhd::tx_streamer::sptr txs_;
-  //   bool correlator_enabled_;
+//   bool correlator_enabled_;
 };
 
 #endif  // RADIO_UHDSDR_H_
