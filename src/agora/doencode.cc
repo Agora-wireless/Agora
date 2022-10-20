8c7cd77cad05fff78e858d5bdd7aefaec7c6fc80
Di Mu <di.mu@rice.edu>
2022-10-20 16:17:55 -0500

    fix buffer name

--- src/agora/doencode.cc
+++ src/agora/doencode.cc
@@ -129,7 +129,7 @@ EventData DoEncode::Launch(size_t tag) {
     for (size_t i = 0; i < cfg_->NumBytesPerCb(dir_); i++) {
       dataprint << " " << std::setw(2)
                 << std::to_integer<int>(
-                       reinterpret_cast<std::byte*>(scrambler_buffer_)[i]);
+                       reinterpret_cast<std::byte*>(ldpc_input)[i]);
     }
     AGORA_LOG_INFO("ldpc input (%zu %zu %zu): %s\n", frame_id, symbol_idx,
                    ue_id, dataprint.str().c_str());
@@ -137,7 +137,7 @@ EventData DoEncode::Launch(size_t tag) {
 
   LdpcEncodeHelper(ldpc_config.BaseGraph(), ldpc_config.ExpansionFactor(),
                    ldpc_config.NumRows(), encoded_buffer_temp_, parity_buffer_,
-                   scrambler_buffer_);
+                   ldpc_input);
   if (kDebugTxData) {
     std::stringstream dataprint;
     dataprint << std::setfill('0') << std::hex;
