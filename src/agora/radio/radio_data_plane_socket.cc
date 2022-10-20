7a724def4cdb1a7e76c2338135900faa39457324
Andrew Sedlmayr <andrew.sedlmayr@rice.edu>
2022-10-20 16:20:44 -0500

    Ue tx

--- src/agora/radio/radio_data_plane_socket.cc
+++ src/agora/radio/radio_data_plane_socket.cc
@@ -93,7 +93,7 @@ void RadioDataPlaneSocket::Setup() {
     rxstream_args["iris:ip6_dst"] = socket_.Address();
     rxstream_args["iris:udp_dst"] = socket_.Port();
     //Helps with the disable strea, errors (-2)
-    //rxstream_args["SYNC_ACTIVATE"] = "false";
+    rxstream_args["SYNC_ACTIVATE"] = "false";
 
     AGORA_LOG_FRAME(
         "Setting stream destination - iris:ip6_dst %s\n iris:udp_dst %s\n",
