09c24aff059bd6c5cfa722309cff43961559093e
Di Mu <di.mu@rice.edu>
2022-10-25 12:07:45 -0500

    add max frame to plotting script

--- tools/python/plot_csv.py
+++ tools/python/plot_csv.py
@@ -2,23 +2,30 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+max_rows = 0
+if len(sys.argv) > 1:
+  max_rows = int(sys.argv[1])
+
 rows = []
 for line in sys.stdin:
   row = []
   for elem in line.split(','):
     row.append(float(elem))
   rows.append(row)
+  if len(rows) == max_rows:
+    break
 arr = np.array(rows)
 n_rows, n_cols = arr.shape
 
 plt.subplot(2, 1, 1)
 plt.plot(arr[:,0], arr[:,1:])
-if len(sys.argv) == 4:
-  plt.xlabel(sys.argv[1])
-  plt.ylabel(sys.argv[2])
+if len(sys.argv) > 3:
+  plt.xlabel(sys.argv[2])
+  plt.ylabel(sys.argv[3])
+if len(sys.argv) > 4:
   leg = []
   for i in range(n_cols):
-    leg.append(sys.argv[3] + ' ' + str(i))
+    leg.append(sys.argv[4] + ' ' + str(i))
   plt.legend(leg)
 
 plt.subplot(2, 1, 2)
