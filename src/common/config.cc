147675c4be84bba62167975c4d5f86adbd2a26b2
Di Mu <di.mu@rice.edu>
2022-10-25 09:52:13 -0500

    fix bad EVM when scrambler off (#380)
    * fix bad EVM when scrambler off
    
    Former-commit-id: 5c12954b4c242906f94ebc316e2960c3491814a9

--- src/common/config.cc
+++ src/common/config.cc
@@ -19,6 +19,7 @@
 #include "message.h"
 #include "modulation.h"
 #include "scrambler.h"
+#include "simd_types.h"
 #include "utils_ldpc.h"
 
 using json = nlohmann::json;
@@ -615,6 +616,8 @@ Config::Config(std::string jsonfilename)
   }
 
   ul_num_bytes_per_cb_ = ul_ldpc_config_.NumCbLen() / 8;
+  ul_num_padding_bytes_per_cb_ =
+      Roundup<64>(ul_num_bytes_per_cb_) - ul_num_bytes_per_cb_;
   ul_data_bytes_num_persymbol_ =
       ul_num_bytes_per_cb_ * ul_ldpc_config_.NumBlocksInSymbol();
   ul_mac_packet_length_ = ul_data_bytes_num_persymbol_;
@@ -628,6 +631,8 @@ Config::Config(std::string jsonfilename)
   ul_mac_bytes_num_perframe_ = ul_mac_packet_length_ * ul_mac_packets_perframe_;
 
   dl_num_bytes_per_cb_ = dl_ldpc_config_.NumCbLen() / 8;
+  dl_num_padding_bytes_per_cb_ =
+      Roundup<64>(dl_num_bytes_per_cb_) - dl_num_bytes_per_cb_;
   dl_data_bytes_num_persymbol_ =
       dl_num_bytes_per_cb_ * dl_ldpc_config_.NumBlocksInSymbol();
   dl_mac_packet_length_ = dl_data_bytes_num_persymbol_;
@@ -1027,7 +1032,7 @@ void Config::GenData() {
   const size_t dl_num_bytes_per_ue_pad =
       Roundup<64>(this->dl_num_bytes_per_cb_) *
       this->dl_ldpc_config_.NumBlocksInSymbol();
-  dl_bits_.Malloc(this->frame_.NumDLSyms(),
+  dl_bits_.Calloc(this->frame_.NumDLSyms(),
                   dl_num_bytes_per_ue_pad * this->ue_ant_num_,
                   Agora_memory::Alignment_t::kAlign64);
   dl_iq_f_.Calloc(this->frame_.NumDLSyms(), ofdm_data_num_ * ue_ant_num_,
@@ -1039,7 +1044,7 @@ void Config::GenData() {
   const size_t ul_num_bytes_per_ue_pad =
       Roundup<64>(this->ul_num_bytes_per_cb_) *
       this->ul_ldpc_config_.NumBlocksInSymbol();
-  ul_bits_.Malloc(this->frame_.NumULSyms(),
+  ul_bits_.Calloc(this->frame_.NumULSyms(),
                   ul_num_bytes_per_ue_pad * this->ue_ant_num_,
                   Agora_memory::Alignment_t::kAlign64);
   ul_iq_f_.Calloc(this->frame_.NumULSyms(),
@@ -1148,12 +1153,10 @@ void Config::GenData() {
   const size_t ul_num_blocks_per_symbol =
       this->ul_ldpc_config_.NumBlocksInSymbol() * this->ue_ant_num_;
 
-  // Used as an input ptr to
-  auto* ul_scramble_buffer =
-      new int8_t[ul_num_bytes_per_cb_ +
-                 kLdpcHelperFunctionInputBufferSizePaddingBytes]();
-  int8_t* ldpc_input = nullptr;
+  SimdAlignByteVector ul_scramble_buffer(
+      ul_num_bytes_per_cb_ + ul_num_padding_bytes_per_cb_, std::byte(0));
 
+  int8_t* ldpc_input = nullptr;
   // Encode uplink bits
   Table<int8_t> ul_encoded_bits;
   ul_encoded_bits.Malloc(this->frame_.NumULSyms() * ul_num_blocks_per_symbol,
@@ -1174,17 +1177,19 @@ void Config::GenData() {
                             j * ul_ldpc_config_.NumBlocksInSymbol() + k];
 
         if (scramble_enabled_) {
-          std::memcpy(ul_scramble_buffer,
-                      GetInfoBits(ul_bits_, Direction::kUplink, i, j, k),
-                      ul_num_bytes_per_cb_);
-          scrambler->Scramble(ul_scramble_buffer, ul_num_bytes_per_cb_);
-          std::memset(&ul_scramble_buffer[ul_num_bytes_per_cb_], 0u,
-                      kLdpcHelperFunctionInputBufferSizePaddingBytes);
-          ldpc_input = ul_scramble_buffer;
+          scrambler->Scramble(
+              ul_scramble_buffer.data(),
+              GetInfoBits(ul_bits_, Direction::kUplink, i, j, k),
+              ul_num_bytes_per_cb_);
+          ldpc_input = reinterpret_cast<int8_t*>(ul_scramble_buffer.data());
         } else {
           ldpc_input = GetInfoBits(ul_bits_, Direction::kUplink, i, j, k);
         }
-
+        //Clean padding
+        if (ul_num_bytes_per_cb_ > 0) {
+          std::memset(&ldpc_input[ul_num_bytes_per_cb_], 0u,
+                      ul_num_padding_bytes_per_cb_);
+        }
         LdpcEncodeHelper(ul_ldpc_config_.BaseGraph(),
                          ul_ldpc_config_.ExpansionFactor(),
                          ul_ldpc_config_.NumRows(), coded_bits_ptr,
@@ -1266,9 +1271,8 @@ void Config::GenData() {
   const size_t dl_num_blocks_per_symbol =
       this->dl_ldpc_config_.NumBlocksInSymbol() * this->ue_ant_num_;
 
-  auto* dl_scramble_buffer =
-      new int8_t[dl_num_bytes_per_cb_ +
-                 kLdpcHelperFunctionInputBufferSizePaddingBytes]();
+  SimdAlignByteVector dl_scramble_buffer(
+      dl_num_bytes_per_cb_ + dl_num_padding_bytes_per_cb_, std::byte(0));
 
   Table<int8_t> dl_encoded_bits;
   dl_encoded_bits.Malloc(this->frame_.NumDLSyms() * dl_num_blocks_per_symbol,
@@ -1289,15 +1293,17 @@ void Config::GenData() {
                             j * dl_ldpc_config_.NumBlocksInSymbol() + k];
 
         if (scramble_enabled_) {
-          std::memcpy(dl_scramble_buffer,
-                      GetInfoBits(dl_bits_, Direction::kDownlink, i, j, k),
-                      dl_num_bytes_per_cb_);
-          scrambler->Scramble(dl_scramble_buffer, dl_num_bytes_per_cb_);
-          std::memset(&dl_scramble_buffer[dl_num_bytes_per_cb_], 0u,
-                      kLdpcHelperFunctionInputBufferSizePaddingBytes);
-          ldpc_input = dl_scramble_buffer;
+          scrambler->Scramble(
+              dl_scramble_buffer.data(),
+              GetInfoBits(dl_bits_, Direction::kDownlink, i, j, k),
+              dl_num_bytes_per_cb_);
+          ldpc_input = reinterpret_cast<int8_t*>(dl_scramble_buffer.data());
         } else {
           ldpc_input = GetInfoBits(dl_bits_, Direction::kDownlink, i, j, k);
+        }
+        if (dl_num_padding_bytes_per_cb_ > 0) {
+          std::memset(&ldpc_input[dl_num_bytes_per_cb_], 0u,
+                      dl_num_padding_bytes_per_cb_);
         }
 
         LdpcEncodeHelper(dl_ldpc_config_.BaseGraph(),
@@ -1448,8 +1454,6 @@ void Config::GenData() {
   dl_encoded_bits.Free();
   ul_encoded_bits.Free();
   FreeBuffer1d(&pilot_ifft);
-  delete[] ul_scramble_buffer;
-  delete[] dl_scramble_buffer;
 }
 
 Config::~Config() {
