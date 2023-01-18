7ccf5c2289b93a5f8e8b4a1f593680216a9dc183
Di Mu <di.mu@rice.edu>
2023-01-18 15:02:41 -0700

    update vnc script
    Former-commit-id: 2657e26a6fc962ebe5a9bf925b274b03f9a9660a

--- scripts/vnc_setup.sh
+++ scripts/vnc_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir ~/.vnc
-curl https://raw.githubusercontent.com/Agora-wireless/Agora/develop/scripts/vnc_xstartup.sh > ~/.vnc/xstartup
-chmod +x ~/.vnc/xstartup
+cp ./scripts/vnc_xstartup.sh ~/.vnc/xstartup
 vncserver
