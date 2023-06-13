62c1dc7d7ed7714ed0c40e6bfe92f629ad984126
Tom Qi <zq40@tc319-srv3.egr.duke.edu>
2023-06-13 19:41:44 -0400

    real-time constellation plot
    Former-commit-id: 7c46e4a18db8db6c4be25a8caa2e6a1d002c145f

---
+++ files/log/csv/cubic_conste.py
@@ -0,0 +1,71 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import struct
+
+MODULATION_TYPE = '16QAM'
+
+def read_binary_file(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            data = file.read()
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    except IOError as e:
+        print(f"Error reading file: {e}")
+    return data
+
+def convert_to_float(binary_data):
+    float_values = struct.unpack('f' * (len(binary_data) // 4), binary_data)
+    return float_values
+
+def parse_coordinates(data):
+    coordinates = []
+    for i in range(0, len(data), 2):
+        real = data[i]
+        imag = data[i + 1]
+        coordinates.append((real, imag))
+    return coordinates
+
+def plot_coordinates(coordinates, angle=np.pi/12, ax=None):
+    i = np.array([coord[0] for coord in coordinates])
+    q = np.array([coord[1] for coord in coordinates])
+    ax.scatter(i * np.cos(angle) + q * np.sin(angle), - i * np.sin(angle) + q * np.cos(angle), s=1)
+    ax.set_xlabel('I')
+    ax.set_ylabel('Q')
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
+    ax.set_title('Coordinates Plot - ' + MODULATION_TYPE)
+
+
+def update_plot(file_path):
+    plt.ion()  # Turn on interactive mode
+    fig, ax = plt.subplots()
+
+    while True:
+        binary_data = read_binary_file(file_path)
+        float_values = convert_to_float(binary_data)
+        coordinates = parse_coordinates(float_values)
+
+        ax.clear()  # Clear the previous plot
+        plot_coordinates(coordinates, ax=ax)
+        plt.pause(0.05)  # Pause for 0.05 seconds
+
+
+def main():
+    # Remove the file if it exists
+    if os.path.isfile("equal_data.bin"):
+        os.system("rm equal_data.bin")
+
+    while True:
+        if os.path.isfile("equal_data.bin"):
+            # Call the update_plot function
+            update_plot("equal_data.bin")
+            break
+        else:
+            print("File 'equal_data.bin' not found. Waiting...")
+            time.sleep(1)  # Wait for 1 second before checking again
+
+if __name__ == "__main__":
+    main()
