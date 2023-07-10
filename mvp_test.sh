0c2ca89332e29c544360498eb45eb74743ccd8f4
cstandy <cst.andy@gmail.com>
2023-07-10 10:01:37 -0400

    Test: Enable mvp_test.sh to read latest log
    Former-commit-id: c04796618247f9ccf39218476f7d05b7512675b3

--- mvp_test.sh
+++ mvp_test.sh
@@ -16,7 +16,8 @@
 
 exe=./build/agora
 config=./files/config/examples/ul-usrp.json
-logfile=./log/$(date +"%Y-%m-%d_%H-%M-%S").log
+logpath=./log
+logfile=$logpath/$(date +"%Y-%m-%d_%H-%M-%S").log
 
 ################
 # Functions
@@ -26,10 +27,11 @@ logfile=./log/$(date +"%Y-%m-%d_%H-%M-%S").log
 function display_help {
     echo "Usage: ./mvp_test.sh [option]"
     echo "Options:"
-    echo "  -b | --build   - Build the project"
-    echo "  -r | --run     - Run the project"
-    echo "  -c | --clean   - Clean the project"
-    echo "  -h | --help    - Display this help message"
+    echo "  -b, --build   - Build the project"
+    echo "  -x, --execute - Run the project"
+    echo "  -r, --read    - Read the log of the latest test run"
+    echo "  -c, --clean   - Clean the project"
+    echo "  -h, --help    - Display this help message"
 }
 
 # Function to build the project
@@ -41,10 +43,25 @@ function build_project {
 }
 
 # Function to run the project
-function run_project {
+function exe_project {
     echo "Running the project..."
     script -q -c "$exe --conf_file $config" $logfile
     # Use `cat log/2023-06-23_11-32-22.log | less -R` to read colored log file
+}
+
+# Function to read the log
+function read_log {
+    # Find the latest log file
+    latest_log=$(ls -t "$logpath"/*.log | head -1)
+
+    # Read the contents
+    if [ -f "$latest_log" ]; then
+        echo "Reading the latest log: $latest_log"
+        # tail "$latest_log"
+        cat $latest_log | less -R
+    else
+        echo "No log files found in the directory"
+    fi
 }
 
 # Function to clean the project
@@ -70,8 +87,11 @@ case "$1" in
     "-b" | "--build")
         build_project
         ;;
-    "-r" | "--run")
-        run_project
+    "-x" | "--exe")
+        exe_project
+        ;;
+    "-r" | "--read")
+        read_log
         ;;
     "-c" | "--clean")
         clean_project
