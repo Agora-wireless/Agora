f018e4a583519ec8dc4572fb67c748a12de457b0
SergioLavao <s.lavaoo@javeriana.edu.co>
2023-07-20 15:49:05 -0500

    Rebase from chsim-dataset

--- src/client/ue_worker.cc
+++ src/client/ue_worker.cc
@@ -634,7 +634,8 @@ void UeWorker::DoIfft(size_t tag) {
   const size_t ant_id = gen_tag_t(tag).ue_id_;
   const size_t frame_slot = (frame_id % kFrameWnd);
 
-  const bool bypass_iFFT = config_.FreqDomainChannel();
+  //const bool bypass_iFFT = config_.FreqDomainChannel();//TODO
+  const bool bypass_iFFT = false;
 
   const size_t ul_symbol_perframe = config_.Frame().NumULSyms();
   const size_t ul_symbol_idx = config_.Frame().GetULSymbolIdx(symbol_id);
