eedda5dd41529bf5f960d088b13f2bc686904a4f
zhecun <zl83@rice.edu>
2023-11-09 16:32:23 -0600

    fixed single UE (I+N) power calculation, removing inf
    Former-commit-id: 38ac1d77df57f25efc5857c95a240015dd156c87

--- src/common/phy_stats.cc
+++ src/common/phy_stats.cc
@@ -496,19 +496,21 @@ void PhyStats::UpdateDlPilotSnr(size_t frame_id, size_t symbol_id,
   arma::fvec fft_IplusN_mag (fft_abs_mag.n_elem, arma::fill::zeros);
   arma::fvec fft_Signal_mag (fft_abs_mag.n_elem, arma::fill::zeros);
   for (size_t i=0; i < fft_abs_mag.size(); i++) {
-    if (i % config_->UeAntNum() != ant_id) {  // checked in shedRow_test
+    if (i % config_->UeAntNum() != ant_id) { 
       fft_IplusN_mag[i] = fft_abs_mag[i];
     } else {
       fft_Signal_mag[i] = fft_abs_mag[i];
     }
   }
    
-  const float iplusn_per_sc = arma::mean(fft_IplusN_mag); //avg power of (I+N)
-  //const float rssi_per_sc = arma::mean(fft_abs_mag);
+  float iplusn_per_sc = arma::mean(fft_IplusN_mag); //avg power of (I+N)
   const float rssi_per_sc = arma::mean(fft_Signal_mag);  // signal strength at un-blanked SCs
   fft_abs_mag.shed_rows(config_->OfdmDataStart(), config_->OfdmDataStop() - 1);
   const float noise_per_sc = arma::mean(fft_abs_mag); 
   const float interf_per_sc = iplusn_per_sc - noise_per_sc; // can be printed later
+  if (config_->UeAntNum() ==1) {
+    iplusn_per_sc = noise_per_sc;
+  }
 
   // const float snr = (rssi_per_sc - noise_per_sc) / noise_per_sc;
   // const float snr = (rssi_per_sc - iplusn_per_sc) / (iplusn_per_sc); // now the S/(I+N)
