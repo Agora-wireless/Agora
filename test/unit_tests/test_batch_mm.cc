93ba66a0d3f841a99b801f4a428913dab8d2f289
cstandy <cst.andy@gmail.com>
2023-11-24 18:00:48 -0500

    Test: Add (4x4)*(4x1) case to batch mat mult tests
    * Allows the dimension to be 4 in the previous commit.
    
    
    Former-commit-id: 6f2aca65cec01a6d90ba0e9e3e8bc2fed1826104

--- test/unit_tests/test_batch_mm.cc
+++ test/unit_tests/test_batch_mm.cc
@@ -14,8 +14,10 @@
 
 /*
  * Test 2x2xN cube slice-wise multiplication with 2x1xN matrix with a loop.
+ * OR
+ * Test 4x4xN cube slice-wise multiplication with 4x1xN matrix with a loop.
  */
-double time_batch_mm_2x2xN_arma_loop_slices(size_t vec_len, int dim,
+double time_batch_mm_arma_loop_slices(size_t vec_len, int dim,
                                             double freq_ghz) {
   size_t tsc_start, tsc_end;
   double duration_ms;
@@ -38,25 +40,71 @@ double time_batch_mm_2x2xN_arma_loop_slices(size_t vec_len, int dim,
 /*
  * Transform 2x2xN cube to 4 1x1xN vectors, 2x1xN matrix to 2 1x1xN vectors, and
  * perform element-wise multiplication to simulate slice-wise multiplication.
+ * 
+ * OR
+ * Transform 4x4xN cube to 16 1x1xN vectors, 4x1xN matrix to 4 1x1xN vectors,
+ * and perform element-wise multiplication to simulate slice-wise
+ * multiplication.
  */
-double time_batch_mm_2x2xN_arma_decomp_vec(size_t vec_len,
+double time_batch_mm_arma_decomp_vec(size_t vec_len,
                                       int dim, double freq_ghz) {
   size_t tsc_start, tsc_end;
   double duration_ms;
 
-  arma::cx_vec vec_a_1_1(vec_len, arma::fill::randu);
-  arma::cx_vec vec_a_1_2(vec_len, arma::fill::randu);
-  arma::cx_vec vec_a_2_1(vec_len, arma::fill::randu);
-  arma::cx_vec vec_a_2_2(vec_len, arma::fill::randu);
-  arma::cx_vec vec_b_1(vec_len, arma::fill::randu);
-  arma::cx_vec vec_b_2(vec_len, arma::fill::randu);
-  arma::cx_vec vec_c_1(vec_len, arma::fill::zeros);
-  arma::cx_vec vec_c_2(vec_len, arma::fill::zeros);
+  if (dim == 2) {
+    arma::cx_vec vec_a_1_1(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_1_2(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_2_1(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_2_2(vec_len, arma::fill::randu);
+    arma::cx_vec vec_b_1(vec_len, arma::fill::randu);
+    arma::cx_vec vec_b_2(vec_len, arma::fill::randu);
+    arma::cx_vec vec_c_1(vec_len, arma::fill::zeros);
+    arma::cx_vec vec_c_2(vec_len, arma::fill::zeros);
 
-  tsc_start = GetTime::Rdtsc();
-  vec_c_1 = vec_a_1_1 % vec_b_1 + vec_a_1_2 % vec_b_2;
-  vec_c_2 = vec_a_2_1 % vec_b_1 + vec_a_2_2 % vec_b_2;
-  tsc_end = GetTime::Rdtsc();
+    tsc_start = GetTime::Rdtsc();
+    vec_c_1 = vec_a_1_1 % vec_b_1 + vec_a_1_2 % vec_b_2;
+    vec_c_2 = vec_a_2_1 % vec_b_1 + vec_a_2_2 % vec_b_2;
+    tsc_end = GetTime::Rdtsc();
+  } else if (dim == 4) {
+    arma::cx_vec vec_a_1_1(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_1_2(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_1_3(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_1_4(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_2_1(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_2_2(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_2_3(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_2_4(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_3_1(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_3_2(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_3_3(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_3_4(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_4_1(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_4_2(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_4_3(vec_len, arma::fill::randu);
+    arma::cx_vec vec_a_4_4(vec_len, arma::fill::randu);
+    arma::cx_vec vec_b_1(vec_len, arma::fill::randu);
+    arma::cx_vec vec_b_2(vec_len, arma::fill::randu);
+    arma::cx_vec vec_b_3(vec_len, arma::fill::randu);
+    arma::cx_vec vec_b_4(vec_len, arma::fill::randu);
+    arma::cx_vec vec_c_1(vec_len, arma::fill::zeros);
+    arma::cx_vec vec_c_2(vec_len, arma::fill::zeros);
+    arma::cx_vec vec_c_3(vec_len, arma::fill::zeros);
+    arma::cx_vec vec_c_4(vec_len, arma::fill::zeros);
+
+    tsc_start = GetTime::Rdtsc();
+    vec_c_1 = vec_a_1_1 % vec_b_1 + vec_a_1_2 % vec_b_2 +
+              vec_a_1_3 % vec_b_3 + vec_a_1_4 % vec_b_4;
+    vec_c_2 = vec_a_2_1 % vec_b_1 + vec_a_2_2 % vec_b_2 +
+              vec_a_2_3 % vec_b_3 + vec_a_2_4 % vec_b_4;
+    vec_c_3 = vec_a_3_1 % vec_b_1 + vec_a_3_2 % vec_b_2 +
+              vec_a_3_3 % vec_b_3 + vec_a_3_4 % vec_b_4;
+    vec_c_4 = vec_a_4_1 % vec_b_1 + vec_a_4_2 % vec_b_2 +
+              vec_a_4_3 % vec_b_3 + vec_a_4_4 % vec_b_4;
+    tsc_end = GetTime::Rdtsc();
+  } else {
+    printf("Error: dim = %d not supported! Expected value: 2 or 4.\n", dim);
+    return -1;
+  }
 
   duration_ms = GetTime::CyclesToMs(tsc_end - tsc_start, freq_ghz);
   // printf("Time measured = %.2f ms\n", duration_ms);
@@ -68,8 +116,13 @@ double time_batch_mm_2x2xN_arma_decomp_vec(size_t vec_len,
  * perform element-wise multiplication to simulate slice-wise multiplication.
  * 
  * This version extracts the vectors from the cube.
+ * 
+ * OR
+ * Transform 4x4xN cube to 16 1x1xN vectors, 4x1xN matrix to 4 1x1xN vectors,
+ * and perform element-wise multiplication to simulate slice-wise
+ * multiplication.
  */
-double time_batch_mm_2x2xN_arma_decomp_vec_from_cube(size_t vec_len, int dim,
+double time_batch_mm_arma_decomp_vec_from_cube(size_t vec_len, int dim,
                                                      double freq_ghz) {
   size_t tsc_start, tsc_end;
   double duration_ms;
@@ -78,23 +131,63 @@ double time_batch_mm_2x2xN_arma_decomp_vec_from_cube(size_t vec_len, int dim,
   arma::cx_cube cub_b(dim, 1, vec_len, arma::fill::randu);
   arma::cx_cube cub_c(dim, 1, vec_len, arma::fill::zeros);
 
-  tsc_start = GetTime::Rdtsc();
-  cub_c.tube(0, 0) =
-    cub_a.tube(0, 0) % cub_b.tube(0, 0) + cub_a.tube(0, 1) % cub_b.tube(1, 0);
-  cub_c.tube(1, 0) =
-    cub_a.tube(1, 0) % cub_b.tube(0, 0) + cub_a.tube(1, 1) % cub_b.tube(1, 0);
+  if (dim == 2) {
+    tsc_start = GetTime::Rdtsc();
+    cub_c.tube(0, 0) =
+      cub_a.tube(0, 0) % cub_b.tube(0, 0) + cub_a.tube(0, 1) % cub_b.tube(1, 0);
+    cub_c.tube(1, 0) =
+      cub_a.tube(1, 0) % cub_b.tube(0, 0) + cub_a.tube(1, 1) % cub_b.tube(1, 0);
+    // arma::cx_vec vec_a_1_1 = cub_a.tube(0, 0);
+    // arma::cx_vec vec_a_1_2 = cub_a.tube(0, 1);
+    // arma::cx_vec vec_a_2_1 = cub_a.tube(1, 0);
+    // arma::cx_vec vec_a_2_2 = cub_a.tube(1, 1);
+    // arma::cx_vec vec_b_1 = cub_b.tube(0, 0);
+    // arma::cx_vec vec_b_2 = cub_b.tube(1, 0);
+    // arma::cx_vec vec_c_1 = vec_a_1_1 % vec_b_1 + vec_a_1_2 % vec_b_2;
+    // arma::cx_vec vec_c_2 = vec_a_2_1 % vec_b_1 + vec_a_2_2 % vec_b_2;
+    // cub_c.tube(0, 0) = vec_c_1;
+    // cub_c.tube(1, 0) = vec_c_2;
+    tsc_end = GetTime::Rdtsc();
+  } else if (dim == 4) {
+    tsc_start = GetTime::Rdtsc();
+    cub_c.tube(0, 0) =
+      cub_a.tube(0, 0) % cub_b.tube(0, 0) +
+      cub_a.tube(0, 1) % cub_b.tube(1, 0) +
+      cub_a.tube(0, 2) % cub_b.tube(2, 0) +
+      cub_a.tube(0, 3) % cub_b.tube(3, 0);
+    cub_c.tube(1, 0) =
+      cub_a.tube(1, 0) % cub_b.tube(0, 0) +
+      cub_a.tube(1, 1) % cub_b.tube(1, 0) +
+      cub_a.tube(1, 2) % cub_b.tube(2, 0) +
+      cub_a.tube(1, 3) % cub_b.tube(3, 0);
+    cub_c.tube(2, 0) =
+      cub_a.tube(2, 0) % cub_b.tube(0, 0) +
+      cub_a.tube(2, 1) % cub_b.tube(1, 0) +
+      cub_a.tube(2, 2) % cub_b.tube(2, 0) +
+      cub_a.tube(2, 3) % cub_b.tube(3, 0);
+    cub_c.tube(3, 0) =
+      cub_a.tube(3, 0) % cub_b.tube(0, 0) +
+      cub_a.tube(3, 1) % cub_b.tube(1, 0) +
+      cub_a.tube(3, 2) % cub_b.tube(2, 0) +
+      cub_a.tube(3, 3) % cub_b.tube(3, 0);
 
-  // arma::cx_vec vec_a_1_1 = cub_a.tube(0, 0);
-  // arma::cx_vec vec_a_1_2 = cub_a.tube(0, 1);
-  // arma::cx_vec vec_a_2_1 = cub_a.tube(1, 0);
-  // arma::cx_vec vec_a_2_2 = cub_a.tube(1, 1);
-  // arma::cx_vec vec_b_1 = cub_b.tube(0, 0);
-  // arma::cx_vec vec_b_2 = cub_b.tube(1, 0);
-  // arma::cx_vec vec_c_1 = vec_a_1_1 % vec_b_1 + vec_a_1_2 % vec_b_2;
-  // arma::cx_vec vec_c_2 = vec_a_2_1 % vec_b_1 + vec_a_2_2 % vec_b_2;
-  // cub_c.tube(0, 0) = vec_c_1;
-  // cub_c.tube(1, 0) = vec_c_2;
-  tsc_end = GetTime::Rdtsc();
+    // TODO: idea is interesting, but the syntax is not correct
+    // arma::cx_mat temp_c_mat(dim, vec_len, arma::fill::zeros);
+  
+    // temp_c_mat += cub_a.col_as_mat(0).each_row() % cub_b.row_as_mat(0);
+    // temp_c_mat += cub_a.col_as_mat(1).each_row() % cub_b.row_as_mat(1);
+    // temp_c_mat += cub_a.col_as_mat(2).each_row() % cub_b.row_as_mat(2);
+    // temp_c_mat += cub_a.col_as_mat(3).each_row() % cub_b.row_as_mat(3);
+
+    // for (int i = 0; i < dim; ++i) {
+    //   cub_c.tube(i, 0) = temp_c_mat.row(i);
+    // }
+
+    tsc_end = GetTime::Rdtsc();
+  } else {
+    printf("Error: dim = %d not supported! Expected value: 2 or 4.\n", dim);
+    return -1;
+  }
 
   duration_ms = GetTime::CyclesToMs(tsc_end - tsc_start, freq_ghz);
   // printf("Time measured = %.2f ms\n", duration_ms);
@@ -103,8 +196,10 @@ double time_batch_mm_2x2xN_arma_decomp_vec_from_cube(size_t vec_len, int dim,
 
 /*
  * Test loop-based 2x2xN slice-wise multiplication with 2x1xN matrix with MKL.
+ * OR
+ * Test loop-based 4x4xN slice-wise multiplication with 4x1xN matrix with MKL.
  */
-double time_batch_mm_2x2xN_mkl_cblas_cgemm_loop(size_t vec_len, int dim,
+double time_batch_mm_mkl_cblas_cgemm_loop(size_t vec_len, int dim,
                                                 double freq_ghz) {
   size_t tsc_start, tsc_end;
   double duration_ms;
@@ -164,8 +259,12 @@ double time_batch_mm_2x2xN_mkl_cblas_cgemm_loop(size_t vec_len, int dim,
 /*
  * Test 2x2xN cube slice-wise multiplication with 2x1xN matrix with MKL
  * (clbas interface).
+ * 
+ * OR
+ * Test 4x4xN cube slice-wise multiplication with 4x1xN matrix with MKL
+ * (clbas interface).
  */
-double time_batch_mm_2x2xN_mkl_cblas_cgemm_batch(size_t vec_len, int dim,
+double time_batch_mm_mkl_cblas_cgemm_batch(size_t vec_len, int dim,
                                                  double freq_ghz) {
   size_t tsc_start, tsc_end;
   double duration_ms;
@@ -243,7 +342,7 @@ double time_batch_mm_2x2xN_mkl_cblas_cgemm_batch(size_t vec_len, int dim,
 TEST(TestArmaMKL, CubeMMSlicewise) {
   int iter = 1000;
   size_t vec_len = 768; // number of subcarriers in this case
-  int dim = 2;
+  int dim = 4;
   double time_ms_loop = 0.0;
   double time_ms_vectors = 0.0;
   double time_ms_vectors_extract = 0.0;
@@ -251,16 +350,20 @@ TEST(TestArmaMKL, CubeMMSlicewise) {
   double time_ms_MKL_batch = 0.0;
   double freq_ghz = GetTime::MeasureRdtscFreq();
 
+  printf("Running %d times of %ld (%dx%d) x (%dx1) matrix mult...\n",
+         iter, vec_len, dim, dim, dim);
+
   for (int i = 0; i < iter; ++i) {
-    time_ms_loop += time_batch_mm_2x2xN_arma_loop_slices(vec_len, dim, freq_ghz);
+    time_ms_loop +=
+      time_batch_mm_arma_loop_slices(vec_len, dim, freq_ghz);
     time_ms_vectors +=
-      time_batch_mm_2x2xN_arma_decomp_vec(vec_len, dim, freq_ghz);
+      time_batch_mm_arma_decomp_vec(vec_len, dim, freq_ghz);
     time_ms_vectors_extract +=
-      time_batch_mm_2x2xN_arma_decomp_vec_from_cube(vec_len, dim, freq_ghz);
+      time_batch_mm_arma_decomp_vec_from_cube(vec_len, dim, freq_ghz);
     time_ms_MKL +=
-      time_batch_mm_2x2xN_mkl_cblas_cgemm_loop(vec_len, dim, freq_ghz);
+      time_batch_mm_mkl_cblas_cgemm_loop(vec_len, dim, freq_ghz);
     time_ms_MKL_batch +=
-      time_batch_mm_2x2xN_mkl_cblas_cgemm_batch(vec_len, dim, freq_ghz);
+      time_batch_mm_mkl_cblas_cgemm_batch(vec_len, dim, freq_ghz);
   }
   printf("[arma] Time for %dx loops of slices = %.2f ms\n", iter, time_ms_loop);
   printf("[arma] Time for %dx vector decomposition (vec) = %.2f ms\n",
