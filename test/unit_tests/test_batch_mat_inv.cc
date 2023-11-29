e102b5f4621e3b99acb46f5a6cb22970433fb4af
cstandy <cst.andy@gmail.com>
2023-11-29 15:02:03 -0500

    Test: (mat_inv) Fix vec decomp with correct .t()
    The transpose of complex matrices in Armadillo using .t() includes a conjugate,
    which was not considered in the previous vector-based decomposition
    implementation. Now the correctness is verified.
    
    Also change the iteration to 16 to match the system-level test config.
    
    
    Former-commit-id: 2e647f6757603a9f91eead8992cc9267c838b43e

--- test/unit_tests/test_batch_mat_inv.cc
+++ test/unit_tests/test_batch_mat_inv.cc
@@ -14,7 +14,7 @@
  */
 void batch_mat_inv_sympd_arma_loop_slices(size_t vec_len, int dim,
                                           double freq_ghz, double& duration_ms,
-                                          arma::cx_fcube& cub_a,
+                                          arma::cx_fcube cub_a,
                                           arma::cx_fcube& cub_b) {
   size_t tsc_start, tsc_end;
   
@@ -25,8 +25,14 @@ void batch_mat_inv_sympd_arma_loop_slices(size_t vec_len, int dim,
   for (size_t i = 0; i < vec_len; ++i) {
     arma::cx_fmat mat_csi = cub_a.slice(i);
     cub_b.slice(i) = arma::inv_sympd(mat_csi.t() * mat_csi) * mat_csi.t();
+    // cub_b.slice(i) = mat_csi.t();
+    // cub_b.slice(i) = cub_b.slice(i) * mat_csi;
+    // cub_b.slice(i) = arma::inv_sympd(cub_b.slice(i));
+    // cub_b.slice(i) = cub_b.slice(i) * mat_csi.t();
   }
   tsc_end = GetTime::Rdtsc();
+
+  // cub_b.print("cub_b");
 
   duration_ms = GetTime::CyclesToMs(tsc_end - tsc_start, freq_ghz);
   // printf("Time measured = %.2f ms\n", duration_ms);
@@ -37,37 +43,48 @@ void batch_mat_inv_sympd_arma_loop_slices(size_t vec_len, int dim,
  */
 void batch_mat_inv_sympd_arma_decomp_vec(size_t vec_len, int dim,
                                          double freq_ghz, double& duration_ms,
-                                         arma::cx_fcube& cub_a,
+                                         arma::cx_fcube cub_a,
                                          arma::cx_fcube& cub_b) {
-  size_t tsc_start, tsc_end;
-
   RtAssert(dim == 2, "Only support 2x2 matrix inversion");
+
+  size_t tsc_start, tsc_end;
   
-  // A = [ a b ], B = [a''' b''']
-  //     [ c d ]      [c''' d''']
+  // A = [ a b ], B = [a''' b'''], A^T = [conj(a) conj(c)]
+  //     [ c d ]      [c''' d''']        [conj(b) conj(d)]
   // arma::cx_fcube cub_a(dim, dim, vec_len, arma::fill::randu);
   // arma::cx_fcube cub_b(dim, dim, vec_len, arma::fill::zeros);
 
   // temporary storages
-  // Product of A^T and A = A', A' = [a^2 + c^2, a*b + c*d] = [a' b']
-  //                                 [a*b + c*d, b^2 + d^2]   [c' d']
+  // Product of A^T and A = A', A' =
+  //   [a*conj(a)+c*conj(c) conj(a)*b+conj(c)*d] = [a' b']
+  //   [a*conj(b)+c*conj(d) b*conj(b)+d*conj(d)]   [c' d']
   // Inversion of A' = A'' = [a'' b''] = [d'  -b'] / (a'*d' - b'*c')
   //                         [c'' d'']   [-c'  a']
-  // Multiplication of A'' and A = [a''' b'''] = [a''*a + b''*c  a''*b + b''*d]
-  //                               [c''' d''']   [c''*a + d''*c  c''*b + d''*d]
+  // Multiplication of A'' and A^T = 
+  //   [a''' b'''] = [a''*conj(a) + b''*conj(b)  a''*conj(c) + b''*conj(d)]
+  //   [c''' d''']   [c''*conj(a) + d''*conj(b)  c''*conj(c) + d''*conj(d)]
+  arma::cx_fcube cub_a_conj(dim, dim, vec_len, arma::fill::zeros);
   arma::cx_fcube cub_a_prod(dim, dim, vec_len, arma::fill::zeros);
   arma::cx_fcube cub_a_inv(dim, dim, vec_len, arma::fill::zeros);
   arma::cx_fcube cub_a_det(1, 1, vec_len, arma::fill::zeros);
 
   tsc_start = GetTime::Rdtsc();
+  cub_a_conj = arma::conj(cub_a);
   // a' = a^2 + c^2, b' = a*b + c*d, c' = a*b + c*d, d' = b^2 + d^2
   cub_a_prod.tube(0, 0) =
-    arma::square(cub_a.tube(0, 0)) + arma::square(cub_a.tube(0, 0));
+    cub_a.tube(0, 0) % cub_a_conj.tube(0, 0) +
+    cub_a.tube(1, 0) % cub_a_conj.tube(1, 0);
+    // arma::square(cub_a.tube(0, 0)) + arma::square(cub_a.tube(0, 0));
   cub_a_prod.tube(0, 1) =
-    cub_a.tube(0, 0) % cub_a.tube(0, 1) + cub_a.tube(1, 0) % cub_a.tube(1, 1);
-  cub_a_prod.tube(1, 0) = cub_a_prod.tube(0, 1);
+    cub_a_conj.tube(0, 0) % cub_a.tube(0, 1) +
+    cub_a_conj.tube(1, 0) % cub_a.tube(1, 1);
+  cub_a_prod.tube(1, 0) =
+    cub_a.tube(0, 0) % cub_a_conj.tube(0, 1) +
+    cub_a.tube(1, 0) % cub_a_conj.tube(1, 1);
   cub_a_prod.tube(1, 1) =
-    arma::square(cub_a.tube(0, 1)) + arma::square(cub_a.tube(1, 1));
+    cub_a_conj.tube(0, 1) % cub_a.tube(0, 1) +
+    cub_a_conj.tube(1, 1) % cub_a.tube(1, 1);
+    // arma::square(cub_a.tube(0, 1)) + arma::square(cub_a.tube(1, 1));
 
   // a_det = a'*d' - b'*c'
   cub_a_det.tube(0, 0) =
@@ -84,21 +101,23 @@ void batch_mat_inv_sympd_arma_decomp_vec(size_t vec_len, int dim,
   cub_a_inv.tube(1, 1) =
     cub_a_prod.tube(0, 0) / cub_a_det.tube(0, 0);
 
-  // a''' = a''*a + b''*c, b''' = a''*b + b''*d
-  // c''' = c''*a + d''*c, d''' = c''*b + d''*d
+  // a''' = a''*a + b''*b, b''' = a''*c + b''*d
+  // c''' = c''*a + d''*b, d''' = c''*c + d''*d
   cub_b.tube(0, 0) =
-    cub_a_inv.tube(0, 0) % cub_a.tube(0, 0) +
-    cub_a_inv.tube(0, 1) % cub_a.tube(1, 0);
+    cub_a_inv.tube(0, 0) % cub_a_conj.tube(0, 0) +
+    cub_a_inv.tube(0, 1) % cub_a_conj.tube(0, 1);
   cub_b.tube(0, 1) =
-    cub_a_inv.tube(0, 0) % cub_a.tube(0, 1) +
-    cub_a_inv.tube(0, 1) % cub_a.tube(1, 1);
+    cub_a_inv.tube(0, 0) % cub_a_conj.tube(1, 0) +
+    cub_a_inv.tube(0, 1) % cub_a_conj.tube(1, 1);
   cub_b.tube(1, 0) =
-    cub_a_inv.tube(1, 0) % cub_a.tube(0, 0) +
-    cub_a_inv.tube(1, 1) % cub_a.tube(1, 0);
+    cub_a_inv.tube(1, 0) % cub_a_conj.tube(0, 0) +
+    cub_a_inv.tube(1, 1) % cub_a_conj.tube(0, 1);
   cub_b.tube(1, 1) =
-    cub_a_inv.tube(1, 0) % cub_a.tube(0, 1) +
-    cub_a_inv.tube(1, 1) % cub_a.tube(1, 1);
+    cub_a_inv.tube(1, 0) % cub_a_conj.tube(1, 0) +
+    cub_a_inv.tube(1, 1) % cub_a_conj.tube(1, 1);
   tsc_end = GetTime::Rdtsc();
+
+  // cub_b = cub_a_prod;
 
   duration_ms = GetTime::CyclesToMs(tsc_end - tsc_start, freq_ghz);
   // printf("Time measured = %.2f ms\n", duration_ms);
@@ -111,8 +130,10 @@ void batch_mat_inv_sympd_arma_decomp_vec(size_t vec_len, int dim,
 void batch_mat_inv_sympd_arma_decomp_vec_simp(size_t vec_len, int dim,
                                               double freq_ghz,
                                               double& duration_ms,
-                                              arma::cx_fcube& cub_a,
+                                              arma::cx_fcube cub_a,
                                               arma::cx_fcube& cub_b) {
+  RtAssert(dim == 2, "Only support 2x2 matrix inversion");
+
   size_t tsc_start, tsc_end;
   
   // A = [ a b ], B = [a''' b''']
@@ -127,19 +148,29 @@ void batch_mat_inv_sympd_arma_decomp_vec_simp(size_t vec_len, int dim,
   //                         [c'' d'']   [-c'  a']
   // Multiplication of A'' and A = [a''' b'''] = [a''*a + b''*c  a''*b + b''*d]
   //                               [c''' d''']   [c''*a + d''*c  c''*b + d''*d]
+  arma::cx_fcube cub_a_conj(dim, dim, vec_len, arma::fill::zeros);
   arma::cx_fcube cub_a_prod(dim, dim, vec_len, arma::fill::zeros);
   arma::cx_fcube cub_a_inv(dim, dim, vec_len, arma::fill::zeros);
   arma::cx_fcube cub_a_det(1, 1, vec_len, arma::fill::zeros);
 
   tsc_start = GetTime::Rdtsc();
   // a' = a^2 + c^2, b' = a*b + c*d, c' = a*b + c*d, d' = b^2 + d^2
+  cub_a_conj = arma::conj(cub_a);
+  // a' = a^2 + c^2, b' = a*b + c*d, c' = a*b + c*d, d' = b^2 + d^2
   cub_a_prod.tube(0, 0) =
-    arma::square(cub_a.tube(0, 0)) + arma::square(cub_a.tube(0, 0));
+    cub_a.tube(0, 0) % cub_a_conj.tube(0, 0) +
+    cub_a.tube(1, 0) % cub_a_conj.tube(1, 0);
+    // arma::square(cub_a.tube(0, 0)) + arma::square(cub_a.tube(0, 0));
   cub_a_prod.tube(0, 1) =
-    cub_a.tube(0, 0) % cub_a.tube(0, 1) + cub_a.tube(1, 0) % cub_a.tube(1, 1);
-  cub_a_prod.tube(1, 0) = cub_a_prod.tube(0, 1);
+    cub_a_conj.tube(0, 0) % cub_a.tube(0, 1) +
+    cub_a_conj.tube(1, 0) % cub_a.tube(1, 1);
+  cub_a_prod.tube(1, 0) =
+    cub_a.tube(0, 0) % cub_a_conj.tube(0, 1) +
+    cub_a.tube(1, 0) % cub_a_conj.tube(1, 1);
   cub_a_prod.tube(1, 1) =
-    arma::square(cub_a.tube(0, 1)) + arma::square(cub_a.tube(1, 1));
+    cub_a_conj.tube(0, 1) % cub_a.tube(0, 1) +
+    cub_a_conj.tube(1, 1) % cub_a.tube(1, 1);
+    // arma::square(cub_a.tube(0, 1)) + arma::square(cub_a.tube(1, 1));
 
   // basically move the scalar det to the end, and substitute inversed matrix
   // to the operand of the second multiplication
@@ -149,28 +180,30 @@ void batch_mat_inv_sympd_arma_decomp_vec_simp(size_t vec_len, int dim,
     cub_a_prod.tube(0, 0) % cub_a_prod.tube(1, 1) -
     cub_a_prod.tube(0, 1) % cub_a_prod.tube(1, 0);
 
-  // a''' = a''*a + b''*c, b''' = a''*b + b''*d
-  // c''' = c''*a + d''*c, d''' = c''*b + d''*d
+  // a''' = a''*a + b''*b, b''' = a''*c + b''*d
+  // c''' = c''*a + d''*b, d''' = c''*c + d''*d
   cub_b.tube(0, 0) =
-    ( cub_a_prod.tube(1, 1) % cub_a.tube(0, 0) +
-     -cub_a_prod.tube(0, 1) % cub_a.tube(1, 0)) / cub_a_det.tube(0, 0);
+    ( cub_a_prod.tube(1, 1) % cub_a_conj.tube(0, 0) +
+     -cub_a_prod.tube(0, 1) % cub_a_conj.tube(0, 1)) / cub_a_det.tube(0, 0);
   cub_b.tube(0, 1) =
-    ( cub_a_prod.tube(1, 1) % cub_a.tube(0, 1) +
-     -cub_a_prod.tube(0, 1) % cub_a.tube(1, 1)) / cub_a_det.tube(0, 0);
+    ( cub_a_prod.tube(1, 1) % cub_a_conj.tube(1, 0) +
+     -cub_a_prod.tube(0, 1) % cub_a_conj.tube(1, 1)) / cub_a_det.tube(0, 0);
   cub_b.tube(1, 0) =
-    (-cub_a_prod.tube(1, 0) % cub_a.tube(0, 0) +
-      cub_a_prod.tube(0, 0) % cub_a.tube(1, 0)) / cub_a_det.tube(0, 0);
+    (-cub_a_prod.tube(1, 0) % cub_a_conj.tube(0, 0) +
+      cub_a_prod.tube(0, 0) % cub_a_conj.tube(0, 1)) / cub_a_det.tube(0, 0);
   cub_b.tube(1, 1) =
-    (-cub_a_prod.tube(1, 0) % cub_a.tube(0, 1) +
-      cub_a_prod.tube(0, 0) % cub_a.tube(1, 1)) / cub_a_det.tube(0, 0);
+    (-cub_a_prod.tube(1, 0) % cub_a_conj.tube(1, 0) +
+      cub_a_prod.tube(0, 0) % cub_a_conj.tube(1, 1)) / cub_a_det.tube(0, 0);
   tsc_end = GetTime::Rdtsc();
+
+  // cub_b = cub_a_prod;
 
   duration_ms = GetTime::CyclesToMs(tsc_end - tsc_start, freq_ghz);
   // printf("Time measured = %.2f ms\n", duration_ms);
 }
 
 TEST(TestBatchMatInv, TimingAnalysis) {
-  int iter = 1000;
+  int iter = 16;
   size_t vec_len = 768; // number of subcarriers in this case
   int dim = 2;
   double time_ms_loop = 0.0;
@@ -214,29 +247,30 @@ TEST(TestBatchMatInv, Correctness) {
   double time_ms_dummy = 0.0;
   double freq_ghz = GetTime::MeasureRdtscFreq();
 
+  arma::arma_rng::set_seed_random();
   arma::cx_fcube cub_a(dim, dim, vec_len, arma::fill::randu);
 
   // Method 1: Armadillo's inv_sympd + loop (default)
-  arma::cx_fcube cub_b_1(dim, dim, vec_len, arma::fill::zeros);
+  arma::cx_fcube cub_b(dim, dim, vec_len, arma::fill::zeros);
 
   batch_mat_inv_sympd_arma_loop_slices(vec_len, dim, freq_ghz, time_ms_dummy,
-                                       cub_a, cub_b_1);
+                                       cub_a, cub_b);
 
   // Method 2: vector decomposition
-  arma::cx_fcube cub_b_2(dim, dim, vec_len, arma::fill::zeros);
+  arma::cx_fcube cub_b_1(dim, dim, vec_len, arma::fill::zeros);
 
   batch_mat_inv_sympd_arma_decomp_vec(vec_len, dim, freq_ghz, time_ms_dummy,
-                                      cub_a, cub_b_2);
+                                      cub_a, cub_b_1);
   
   // Method 3: vector decomposition (simplified)
-  arma::cx_fcube cub_b_3(dim, dim, vec_len, arma::fill::zeros);
+  arma::cx_fcube cub_b_2(dim, dim, vec_len, arma::fill::zeros);
 
   batch_mat_inv_sympd_arma_decomp_vec_simp(vec_len, dim, freq_ghz,
-                                           time_ms_dummy, cub_a, cub_b_3);
+                                           time_ms_dummy, cub_a, cub_b_2);
   
   // Check the results
-  EXPECT_TRUE(arma::approx_equal(cub_a, cub_b_1, "absdiff", 1e-3));
-  EXPECT_TRUE(arma::approx_equal(cub_a, cub_b_2, "absdiff", 1e-3));
+  EXPECT_TRUE(arma::approx_equal(cub_b, cub_b_1, "reldiff", 1e-2));
+  EXPECT_TRUE(arma::approx_equal(cub_b, cub_b_2, "reldiff", 1e-2));
 }
 
 int main(int argc, char** argv) {
