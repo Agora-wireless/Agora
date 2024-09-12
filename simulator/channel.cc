3d20bf8ca1470c516ac3e7137ccfab42751deada
rdoost <doost@rice.edu>
2024-09-12 17:32:32 -0500

    bypass reading null subcarriers in frequency orthogonal

--- simulator/channel.cc
+++ simulator/channel.cc
@@ -47,8 +47,21 @@ void Channel::ApplyChan(const arma::cx_fmat& fmat_src, arma::cx_fmat& fmat_dst,
 
       fmat_h.zeros(n_rows, n_cols);
       for (size_t h_index = 0; h_index < n_rows; h_index++) {
-        fmat_h.row(h_index) = fmat_src.row(h_index) *
-                              channel_model_->GetMatrix(is_downlink, h_index);
+        arma::cx_fmat H;
+        if (cfg_->FreqDomainChannel()) {
+          if (h_index < cfg_->OfdmDataStart() ||
+              h_index >= cfg_->OfdmDataStop()) {
+            const size_t new_dim =
+                (is_downlink) ? cfg_->BsAntNum() : cfg_->UeAntNum();
+            H = arma::zeros<arma::cx_fmat>(new_dim, n_cols);
+          } else {
+            H = channel_model_->GetMatrix(is_downlink,
+                                          h_index - cfg_->OfdmDataStart());
+          }
+        } else {
+          H = channel_model_->GetMatrix(is_downlink, h_index);
+        }
+        fmat_h.row(h_index) = fmat_src.row(h_index) * H;
       }
       break;
     }
